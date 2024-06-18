7fb32a4eb414df3bef6864328a18233f5ac37163
Francis Rodriguez <francisngg@gmail.com>
2024-06-18 11:39:23 -0400

    DAO-411 Added colors and font to project.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next"
-import { Inter } from "next/font/google"
+import { Open_Sans } from "next/font/google"
 import "./globals.css"
 
-const inter = Inter({ subsets: ["latin"] })
+const openSans = Open_Sans({
+  variable: '--font-open-sans',
+  subsets: ['latin']
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" data-theme="default">
-      <body className={inter.className}>{children}</body>
+      <body className={`${openSans.variable} font-sans`}>{children}</body>
     </html>
   )
 }
