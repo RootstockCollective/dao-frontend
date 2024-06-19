7dcc4501c3f975fc312dcee3a0e4548724956656
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2024-06-19 14:29:05 -0300

    change background

--- src/components/BalanceRow/BalanceRow.tsx
+++ src/components/BalanceRow/BalanceRow.tsx
@@ -13,18 +13,18 @@ interface Props {
 }
 
 export const BalanceRow: FC<Props> = ({ label, amount, icon, actions }) => (
-  <div className="flex items-center p-3 mb-3">
-    <div className="mr-3">{icon}</div>
-    <div className="flex-grow ">
-      <div className="text-white text-sm">{label}</div>
-      <div className="text-gray-500 text-xs">{amount}</div>
+  <div className="flex items-center p-3 mb-3 bg-foreground">
+    <div className="mr-3 bg-foreground">{icon}</div>
+    <div className="flex-grow">
+      <div className="text-white text-sm bg-foreground">{label}</div>
+      <div className="text-zinc-400 text-xs bg-foreground">{amount}</div>
     </div>
-    <div className="flex space-x-2">
+    <div className="flex space-x-2 bg-foreground">
       {actions?.map((action, index) => (
         <button
           key={index}
           onClick={action.onClick}
-          className="border-white border-[1px] text-white rounded-full px-3 py-1 hover:bg-gray-700"
+          className="border-white border-[1px] text-white rounded-full px-1 hover:bg-zinc-700"
         >
           {action.label}
         </button>
