306a180e0e62a9c10cf33141bfdedd8be6df49e1
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2024-07-04 21:35:08 -0300

    textinput

---
+++ src/components/TextInput/TextInput.stories.tsx
@@ -0,0 +1,87 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { TextInput } from '@/components/TextInput/TextInput'
+import { userEvent, within, expect } from '@storybook/test'
+
+const meta = {
+  title: 'Components/TextInput',
+  component: TextInput,
+} satisfies Meta<typeof TextInput>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    onChange: (val: string) => console.log(val),
+    placeholder: 'test',
+    name: 'test',
+  },
+}
+
+export const WithLabel: Story = {
+  args: {
+    onChange: (val: string) => console.log(val),
+    placeholder: 'name your proposal',
+    name: 'Proposal name',
+    label: 'Proposal name',
+    fullWidth: true,
+  },
+}
+
+export const WithError: Story = {
+  args: {
+    onChange: (val: string) => console.log(val),
+    placeholder: 'name your proposal',
+    name: 'Proposal name',
+    label: 'Proposal name',
+    fullWidth: true,
+    errorMessage: 'This is an error message',
+  },
+}
+
+export const WithHint: Story = {
+  args: {
+    onChange: (val: string) => console.log(val),
+    placeholder: 'name your proposal',
+    name: 'Proposal name',
+    label: 'Proposal name',
+    fullWidth: true,
+    hint: 'This is a hint',
+  },
+}
+
+export const UncontrolledInputTest: Story = {
+  args: {
+    onChange: (val: string) => console.log(val),
+    placeholder: 'name your proposal',
+    name: 'proposal',
+    label: 'Proposal name',
+    fullWidth: true,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    const input = canvas.getByTestId('proposal')
+
+    await userEvent.type(input, 'Hello')
+    await expect(input).toHaveValue('Hello')
+  },
+}
+
+export const ControlledInputTest: Story = {
+  args: {
+    onChange: (val: string) => console.log(val),
+    placeholder: 'name your proposal',
+    name: 'proposal',
+    label: 'Proposal name',
+    fullWidth: true,
+    value: 'Start',
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    const input = canvas.getByTestId('proposal')
+
+    await userEvent.type(input, 'Hello')
+    await expect(input).toHaveValue('Start')
+  },
+}
---
+++ src/components/TextInput/TextInput.tsx
@@ -0,0 +1,84 @@
+import { Label, Paragraph } from '@/components/Typography'
+import { cn } from '@/lib/utils'
+import { FC, JSX } from 'react'
+
+const DEFAULT_CLASSES = `
+px-[20px] py-[12px]
+text-black bg-input-bg
+text-text-primary
+rounded-[6px]
+border-[1px]
+border-[white]/[0.10]
+placeholder:text-input-placeholder
+focus:outline-none
+focus-visible:ring-1 focus-visible:ring-ring
+focus-visible:ring-white focus-visible:ring-opacity-50
+`
+
+interface Props {
+  onChange: (value: string) => void
+  name: string
+  fullWidth?: boolean
+  value?: string
+  placeholder?: string
+  label?: string
+  labelWrapperProps?: JSX.IntrinsicElements['div']
+  inputProps?: JSX.IntrinsicElements['input']
+  hint?: string
+  errorMessage?: string
+  className?: string
+}
+export const TextInput: FC<Props> = ({
+  onChange,
+  name,
+  fullWidth = false,
+  value,
+  placeholder,
+  label,
+  labelWrapperProps = {},
+  inputProps = {},
+  hint,
+  errorMessage,
+  className,
+}) => {
+  const handleOnChange = (e: { target: { value: string } }) => onChange(e.target.value)
+
+  const classes = cn({
+    [DEFAULT_CLASSES]: true,
+    'w-full': fullWidth,
+    'border-st-error': errorMessage,
+  })
+  return (
+    <div className={className}>
+      {label && (
+        <div className="pb-[10px]" {...labelWrapperProps}>
+          <Label variant="semibold">{label}</Label>
+        </div>
+      )}
+      <input
+        className={classes}
+        placeholder={placeholder}
+        type="text"
+        value={value}
+        onChange={handleOnChange}
+        name={name}
+        data-testid={name}
+        {...inputProps}
+      />
+      {hint && !errorMessage && (
+        <div className="text-st-hint mt-[5px]">
+          <Paragraph variant="light" className="text-[14px]">
+            {hint}
+          </Paragraph>
+        </div>
+      )}
+      {errorMessage && (
+        <div className="mt-[5px]">
+          <Paragraph variant="error" className="text-[14px]">
+            {errorMessage}
+          </Paragraph>
+        </div>
+      )}
+    </div>
+  )
+}
