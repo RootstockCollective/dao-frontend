ec9107c6be9230ffb80e0bf6a9ca6d69a92df485
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2024-07-07 01:46:23 -0300

    fix load as client component

--- src/app/proposals/[id]/page.tsx
+++ src/app/proposals/[id]/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -11,7 +12,7 @@ import { MainContainer } from '@/components/MainContainer/MainContainer'
 import { MetricsCard } from '@/components/MetricsCard'
 import { Header, Paragraph } from '@/components/Typography'
 import { shortAddress } from '@/lib/utils'
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 
 const getProposalData = async (id: string): Promise<any> => {
   return new Promise(resolve => {
@@ -39,92 +40,101 @@ const getProposalData = async (id: string): Promise<any> => {
   })
 }
 
-export default async function ProposalView({ params }: { params: { id: string } }) {
-  const proposal = await getProposalData(params.id)
+export default function ProposalView({ params }: { params: { id: string } }) {
+  const [proposal, setProposal] = useState<any>(null)
+
+  useEffect(() => {
+    getProposalData(params.id).then(data => setProposal(data))
+  }, [params.id])
+
   return (
     <MainContainer>
       <div className="pl-4 grid grid-rows-1 gap-[32px] mb-[100px]">
-        <BreadcrumbSection title={proposal.title} />
-        <Header className="text-2xl">{proposal.title}</Header>
-        <div className="flex flex-row">
-          <Paragraph className="text-sm text-gray-500">
-            Proposed by: <span className="text-primary">{proposal.proposedBy}</span>
-          </Paragraph>
-          <Paragraph className="text-sm text-gray-500 ml-4">
-            Created: <span className="text-primary">{proposal.created.toDateString()}</span>
-          </Paragraph>
-          <Paragraph className="text-sm text-primary ml-4">{params.id}</Paragraph>
-        </div>
-        <div className="flex flex-row justify-between">
-          <div className="flex flex-row gap-x-6">
-            <MetricsCard title="Threshold" amount={`${proposal.threshold} votes`} />
-            <MetricsCard title="Snapshot" amount={proposal.snapshotBlock} fiatAmount="Taken at block" />
-          </div>
-          <div>
-            {/* TODO: vote on chain */}
-            <Button onClick={() => console.log('clicked')}>Vote on chain</Button>
-          </div>
-        </div>
-        <div className="flex flex-row gap-x-6">
-          <div className="basis-2/3">
-            <Header variant="h1" className="text-[24px] mb-6">
-              Description
-            </Header>
-            <Paragraph variant="normal" className="text-[16px] text-justify">
-              {proposal.description}
-            </Paragraph>
-          </div>
-          <div className="basis-1/3 flex flex-col gap-y-2">
-            <Header variant="h1" className="text-[24px]">
-              Votes
-            </Header>
-            <div className="flex flex-row justify-between border border-white border-opacity-40 rounded-lg px-[16px] py-[11px]">
-              <Paragraph variant="semibold" className="text-[16px] text-st-success">
-                {proposal.votes.for}
+        {proposal && (
+          <>
+            <BreadcrumbSection title={proposal.title} />
+            <Header className="text-2xl">{proposal.title}</Header>
+            <div className="flex flex-row">
+              <Paragraph className="text-sm text-gray-500">
+                Proposed by: <span className="text-primary">{proposal.proposedBy}</span>
               </Paragraph>
-              <Paragraph variant="semibold" className="text-[16px] text-st-success">
-                For
+              <Paragraph className="text-sm text-gray-500 ml-4">
+                Created: <span className="text-primary">{proposal.created.toDateString()}</span>
               </Paragraph>
+              <Paragraph className="text-sm text-primary ml-4">{params.id}</Paragraph>
             </div>
-            <div className="flex flex-row justify-between border border-white border-opacity-40 rounded-lg px-[16px] py-[11px]">
-              <Paragraph variant="semibold" className="text-[16px] text-st-error">
-                {proposal.votes.against}
-              </Paragraph>
-              <Paragraph variant="semibold" className="text-[16px] text-st-error">
-                Against
-              </Paragraph>
-            </div>
-            <div className="flex flex-row justify-between border border-white border-opacity-40 rounded-lg px-[16px] py-[11px]">
-              <Paragraph variant="semibold" className="text-[16px] text-text-light">
-                {proposal.votes.abstain}
-              </Paragraph>
-              <Paragraph variant="semibold" className="text-[16px] text-text-light">
-                Abstain
-              </Paragraph>
-            </div>
-            <Header variant="h1" className="text-[24px]">
-              Actions
-            </Header>
-            <div className="flex flex-row justify-between border border-white border-opacity-40 rounded-lg px-[16px] py-[11px]">
-              <div className="flex flex-col">
-                <Paragraph variant="semibold" className="text-[16px]">
-                  Transfer
-                </Paragraph>
-                <Paragraph variant="semibold" className="text-[16px]">
-                  To
-                </Paragraph>
+            <div className="flex flex-row justify-between">
+              <div className="flex flex-row gap-x-6">
+                <MetricsCard title="Threshold" amount={`${proposal.threshold} votes`} />
+                <MetricsCard title="Snapshot" amount={proposal.snapshotBlock} fiatAmount="Taken at block" />
               </div>
               <div>
-                <Paragraph variant="semibold" className="text-[16px]">
-                  {proposal.actions.amount} {proposal.actions.tokenSymbol}
-                </Paragraph>
-                <Paragraph variant="semibold" className="text-[16px]">
-                  {shortAddress(proposal.actions.toAddress)}
-                </Paragraph>
+                {/* TODO: vote on chain */}
+                <Button onClick={() => console.log('clicked')}>Vote on chain</Button>
               </div>
             </div>
-          </div>
-        </div>
+            <div className="flex flex-row gap-x-6">
+              <div className="basis-2/3">
+                <Header variant="h1" className="text-[24px] mb-6">
+                  Description
+                </Header>
+                <Paragraph variant="normal" className="text-[16px] text-justify">
+                  {proposal.description}
+                </Paragraph>
+              </div>
+              <div className="basis-1/3 flex flex-col gap-y-2">
+                <Header variant="h1" className="text-[24px]">
+                  Votes
+                </Header>
+                <div className="flex flex-row justify-between border border-white border-opacity-40 rounded-lg px-[16px] py-[11px]">
+                  <Paragraph variant="semibold" className="text-[16px] text-st-success">
+                    {proposal.votes.for}
+                  </Paragraph>
+                  <Paragraph variant="semibold" className="text-[16px] text-st-success">
+                    For
+                  </Paragraph>
+                </div>
+                <div className="flex flex-row justify-between border border-white border-opacity-40 rounded-lg px-[16px] py-[11px]">
+                  <Paragraph variant="semibold" className="text-[16px] text-st-error">
+                    {proposal.votes.against}
+                  </Paragraph>
+                  <Paragraph variant="semibold" className="text-[16px] text-st-error">
+                    Against
+                  </Paragraph>
+                </div>
+                <div className="flex flex-row justify-between border border-white border-opacity-40 rounded-lg px-[16px] py-[11px]">
+                  <Paragraph variant="semibold" className="text-[16px] text-text-light">
+                    {proposal.votes.abstain}
+                  </Paragraph>
+                  <Paragraph variant="semibold" className="text-[16px] text-text-light">
+                    Abstain
+                  </Paragraph>
+                </div>
+                <Header variant="h1" className="text-[24px]">
+                  Actions
+                </Header>
+                <div className="flex flex-row justify-between border border-white border-opacity-40 rounded-lg px-[16px] py-[11px]">
+                  <div className="flex flex-col">
+                    <Paragraph variant="semibold" className="text-[16px]">
+                      Transfer
+                    </Paragraph>
+                    <Paragraph variant="semibold" className="text-[16px]">
+                      To
+                    </Paragraph>
+                  </div>
+                  <div>
+                    <Paragraph variant="semibold" className="text-[16px]">
+                      {proposal.actions.amount} {proposal.actions.tokenSymbol}
+                    </Paragraph>
+                    <Paragraph variant="semibold" className="text-[16px]">
+                      {shortAddress(proposal.actions.toAddress)}
+                    </Paragraph>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </MainContainer>
   )
