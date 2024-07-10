038575f3540b24473dfcd2e317b44f561af15ce3
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2024-07-10 12:08:24 -0300

    static dapp

--- src/pages/proposals/[id].tsx
+++ src/pages/proposals/[id].tsx
@@ -72,7 +72,6 @@ export default function ProposalView({ params }: { params: { id: string } }) {
     }
   }, [id])
 
-  if (!id) return null
   return (
     <MainContainer>
       <div className="pl-4 grid grid-rows-1 gap-[32px] mb-[100px]">
