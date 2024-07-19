3723733b9d43382a71d3d7900de2ded81e53d3ea
Francis Rodriguez <39339295+Freshenext@users.noreply.github.com>
2024-07-19 10:42:39 -0400

    DAO-561 Will now show transfer to info (#70)

--- src/app/proposals/shared/utils.ts
+++ src/app/proposals/shared/utils.ts
@@ -5,12 +5,14 @@ export interface EventArgumentsParameter {
     voteStart: bigint
     voteEnd: bigint
     proposer: string
+    targets: string[]
+    values: bigint[]
   }
   timeStamp: string
 }
 
 export const getEventArguments = ({
-  args: { description, proposalId, proposer },
+  args: { description, proposalId, proposer, targets, values },
   timeStamp,
 }: EventArgumentsParameter) => ({
   name: description.split(';')[0],
@@ -18,4 +20,6 @@ export const getEventArguments = ({
   description: description.split(';')[1],
   proposalId: proposalId.toString(),
   Starts: new Date(parseInt(timeStamp, 16) * 1000).toISOString().split('T')[0],
+  transferTo: targets[0] ?? '',
+  transferToValue: values[0] ?? '',
 })
