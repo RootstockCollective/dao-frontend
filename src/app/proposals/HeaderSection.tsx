995afbe0c8fbe7748b0edd244c30d51a68925eea
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2024-07-22 19:05:50 -0300

    add threshold popover in create proposal button

--- src/app/proposals/HeaderSection.tsx
+++ src/app/proposals/HeaderSection.tsx
@@ -2,21 +2,33 @@ import { useRouter } from 'next/navigation'
 import { Paragraph } from '@/components/Typography'
 import { Button } from '@/components/Button'
 import { FaPlus } from 'react-icons/fa6'
+import { Popover } from '@/components/Popover'
 
-export const HeaderSection = ({ createProposalDisabled = true }) => {
+export const HeaderSection = ({ createProposalDisabled = true, threshold = '' }) => {
   const router = useRouter()
 
   return (
     <div className="flex flex-row justify-between container pl-4">
       <Paragraph className="font-semibold text-[18px]">My Governance</Paragraph>
       <div className="flex flex-row gap-x-6">
-        <Button
-          startIcon={<FaPlus />}
-          onClick={() => router.push('/proposals/create')}
-          disabled={createProposalDisabled}
+        <Popover
+          content={
+            <Paragraph variant="normal" className="text-sm">
+              You don&apos;t have enough voting power: {threshold}
+            </Paragraph>
+          }
+          trigger="hover"
+          background="dark"
+          size="small"
         >
-          Create Proposal
-        </Button>
+          <Button
+            startIcon={<FaPlus />}
+            onClick={() => router.push('/proposals/create')}
+            disabled={createProposalDisabled}
+          >
+            Create Proposal
+          </Button>
+        </Popover>
         {/*<Button variant="secondary" disabled>*/}
         {/*  Delegate*/}
         {/*</Button>*/}
