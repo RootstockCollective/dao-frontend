b97c3e1d1b931b4607f950b46ce5aaba9e7d025d
Francis Rodriguez <francisngg@gmail.com>
2024-07-25 09:50:40 -0400

    DAO-556 Added links

--- src/components/LeftSidebar/UsefulLinks.tsx
+++ src/components/LeftSidebar/UsefulLinks.tsx
@@ -1,19 +1,20 @@
 import { Paragraph } from '@/components/Typography/Paragraph'
 import { Link } from '@/components/Link'
+import { currentLinks } from '@/components/LeftSidebar/links'
 
 export const UsefulLinks = () => (
   <div>
     <Paragraph className="mb-[24px] text-[16px]">Useful links</Paragraph>
-    <Link href="#" variant="menu" className="mb-[16px] text-[16px]">
+    <Link href={currentLinks.registerRns} variant="menu" className="mb-[16px] text-[16px]" target="_blank">
       Register RNS Domain
     </Link>
-    <Link href="#" variant="menu" className="mb-[16px] text-[16px]">
+    <Link href={currentLinks.tokenBridge} variant="menu" className="mb-[16px] text-[16px]" target="_blank">
       Token Bridge Dapp
     </Link>
-    <Link href="#" variant="menu" className="mb-[16px] text-[16px]">
+    <Link href={currentLinks.flyover} variant="menu" className="mb-[16px] text-[16px]" target="_blank">
       Flyover Dapp
     </Link>
-    <Link href="#" variant="menu" className="mb-[16px] text-[16px]">
+    <Link href={currentLinks.readMore} variant="menu" className="mb-[16px] text-[16px]" target="_blank">
       Read more about RIF
     </Link>
   </div>
---
+++ src/components/LeftSidebar/links.tsx
@@ -0,0 +1,20 @@
+const testnet = {
+  registerRns: 'https://testnet.manager.rns.rifos.org/',
+  tokenBridge: 'https://testnet.tokenbridge.rsk.co/',
+  flyover: 'https://dapp.testnet.flyover.rif.technology/',
+  readMore: 'https://dev.rootstock.io/',
+}
+
+const mainnet = {
+  registerRns: 'https://manager.rns.rifos.org/',
+  tokenBridge: 'https://tokenbridge.rsk.co/',
+  flyover: 'https://dapp.flyover.rif.technology/',
+  readMore: 'https://dev.rootstock.io/',
+}
+
+const environments = {
+  testnet,
+  mainnet,
+}
+// @ts-ignore
+export const currentLinks = environments[process.env.NEXT_PUBLIC_ENV] as typeof testnet
