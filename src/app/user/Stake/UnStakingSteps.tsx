83521f6051c20dc9fc6abe6bf46051ab1fe0d1ae
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2024-08-02 12:42:57 -0300

    fix build

--- src/app/user/Stake/UnStakingSteps.tsx
+++ src/app/user/Stake/UnStakingSteps.tsx
@@ -27,9 +27,9 @@ const UnStakingSteps = ({ onCloseModal }: StakingStepsProps) => {
       balance: balances.stRIF.balance,
       symbol: balances.stRIF.symbol,
       contract: currentEnvContracts.stRIF,
-      price: prices.stRIF.price.toString(),
+      price: prices.stRIF?.price.toString(),
     }),
-    [balances.stRIF.balance, balances.stRIF.symbol, prices.stRIF.price],
+    [balances.stRIF.balance, balances.stRIF.symbol, prices.stRIF?.price],
   )
 
   const tokenToReceive: StakingToken = useMemo(
@@ -37,9 +37,9 @@ const UnStakingSteps = ({ onCloseModal }: StakingStepsProps) => {
       balance: balances.RIF.balance,
       symbol: balances.RIF.symbol,
       contract: currentEnvContracts.RIF,
-      price: prices.RIF.price.toString(),
+      price: prices.RIF?.price.toString(),
     }),
-    [balances.RIF.balance, balances.RIF.symbol, prices.RIF.price],
+    [balances.RIF.balance, balances.RIF.symbol, prices.RIF?.price],
   )
 
   return (
