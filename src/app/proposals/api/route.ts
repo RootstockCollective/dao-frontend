70fe1f1d2910cb94b0be3b3f33fb513fae0cf258
Francis Rodriguez <francisngg@gmail.com>
2024-08-05 12:06:21 -0400

    PROPOSALS_OPTIMIZATION - Fixed caching

--- src/app/proposals/api/route.ts
+++ src/app/proposals/api/route.ts
@@ -4,6 +4,7 @@ let cachedProposals = {
   lastUpdated: Date.now(),
   data: [],
   isFetching: false,
+  error: '',
 }
 
 function fetchProposals() {
@@ -14,18 +15,30 @@ function fetchProposals() {
       console.log(14, 'Finished fetching proposals...')
       cachedProposals.data = data
       cachedProposals.lastUpdated = Date.now()
+      cachedProposals.error = ''
+    })
+    .catch(err => {
+      cachedProposals.error = err.toString()
     })
     .finally(() => (cachedProposals.isFetching = false))
 }
 
 const SECONDS_INTERVAL = 10
 
-export async function GET() {
+export async function GET(request: Request) {
+  const disableCache = request.url
   const currentTime = Date.now()
   const timeElapsed = (currentTime - cachedProposals.lastUpdated) / 1000 // Time elapsed in seconds
 
   if ((cachedProposals.data.length === 0 || timeElapsed > SECONDS_INTERVAL) && !cachedProposals.isFetching) {
     fetchProposals()
   }
-  return Response.json(cachedProposals.data)
+  return Response.json(cachedProposals.data, {
+    headers: {
+      'X-Error': cachedProposals.error,
+      'X-Fetching': cachedProposals.isFetching ? 'true' : 'false',
+      'X-LastUpdated': cachedProposals.lastUpdated.toString(),
+      'X-TimeElapsed': timeElapsed.toString(),
+    },
+  })
 }
