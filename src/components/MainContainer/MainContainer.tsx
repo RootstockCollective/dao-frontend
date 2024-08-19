5b10ab47e1554c02b2469261f9ff4aa19806e3e7
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2024-08-19 11:11:07 -0300

    clear message on route change

--- src/components/MainContainer/MainContainer.tsx
+++ src/components/MainContainer/MainContainer.tsx
@@ -5,7 +5,7 @@ import { Footer } from '@/components/Footer'
 import { ConnectButton, Header } from '@/components/Header'
 import { StatefulSidebar } from '@/components/MainContainer/StatefulSidebar'
 import { shortAddress } from '@/lib/utils'
-import { useRouter } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 import { FC, ReactNode, useEffect, useState } from 'react'
 import { useAccount, useDisconnect } from 'wagmi'
 import { Alert } from '../Alert'
@@ -21,9 +21,10 @@ interface Props {
 export const MainContainer: FC<Props> = ({ children, notProtected = false }) => {
   const { isConnected, address } = useAccount()
   const { disconnect } = useDisconnect()
+  const { message, setMessage } = useAlertContext()
   const router = useRouter()
   const modal = useModal()
-  const { message, setMessage } = useAlertContext()
+  const pathname = usePathname()
 
   const [hasMounted, setHasMounted] = useState(false)
 
@@ -31,6 +32,11 @@ export const MainContainer: FC<Props> = ({ children, notProtected = false }) => 
     router.push('/')
     disconnect()
   }
+
+  useEffect(() => {
+    // Clear message on route change
+    setMessage(null)
+  }, [pathname, setMessage])
 
   useEffect(() => {
     // This is to prevent Hydration error on client side
