cdbf5a5d10556df3bab1bbdb11433dcb7376fb47
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2024-08-21 12:21:54 -0300

    disable stake and unstake button if don't have enough balance

--- src/app/user/Balances/UnStakeRIFCell.tsx
+++ src/app/user/Balances/UnStakeRIFCell.tsx
@@ -1,12 +1,14 @@
 import { useBalancesContext } from '@/app/user/Balances/context/BalancesContext'
+import { cn } from '@/lib/utils'
 
 export const UnStakeRIFCell = () => {
-  const { unstakeModal } = useBalancesContext()
-
+  const { unstakeModal, balances } = useBalancesContext()
+  const { balance } = balances['stRIF']
+  const hasEnoughBalance = Number(balance) > 0
   return (
     <p
-      onClick={unstakeModal.openModal}
-      className="text-link underline cursor-pointer"
+      onClick={hasEnoughBalance ? unstakeModal.openModal : undefined}
+      className={cn('underline', hasEnoughBalance ? 'text-link cursor-pointer' : 'text-zinc-500')}
       data-testid="UnstakeRIFParagraph"
     >
       Unstake
