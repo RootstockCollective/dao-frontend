64f9dc5affad7d49c97f94f0be278e4a6f2e1aae
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2024-08-23 16:46:12 -0300

    fix treasury amount

--- src/app/treasury/TreasurySection.tsx
+++ src/app/treasury/TreasurySection.tsx
@@ -1,6 +1,7 @@
 import { useTreasuryContext } from '@/app/treasury/TreasuryContext'
 import { Paragraph } from '@/components/Typography'
 import { MetricsCard } from '@/components/MetricsCard'
+import { toFixed } from '@/lib/utils'
 
 export const TreasurySection = () => {
   const { buckets } = useTreasuryContext()
@@ -11,32 +12,32 @@ export const TreasurySection = () => {
       <div className="grid grid-cols-3 gap-[24px]">
         <MetricsCard
           title="Treasury 1 RIF Holdings"
-          amount={bucketOne.RIF.amount}
+          amount={toFixed(bucketOne.RIF.amount)}
           fiatAmount={`= USD ${bucketOne.RIF.fiatAmount}`}
         />
         <MetricsCard
           title="Treasury 2 RIF Holdings"
-          amount={bucketTwo.RIF.amount}
+          amount={toFixed(bucketTwo.RIF.amount)}
           fiatAmount={`= USD ${bucketTwo.RIF.fiatAmount}`}
         />
         <MetricsCard
           title="Treasury 3 RIF Holdings"
-          amount={bucketThree.RIF.amount}
+          amount={toFixed(bucketThree.RIF.amount)}
           fiatAmount={`= USD ${bucketThree.RIF.fiatAmount}`}
         />
         <MetricsCard
           title="Treasury 1 RBTC Holdings"
-          amount={bucketOne.RBTC.amount}
+          amount={toFixed(bucketOne.RBTC.amount)}
           fiatAmount={`= USD ${bucketOne.RBTC.fiatAmount}`}
         />
         <MetricsCard
           title="Treasury 2 RBTC Holdings"
-          amount={bucketTwo.RBTC.amount}
+          amount={toFixed(bucketTwo.RBTC.amount)}
           fiatAmount={`= USD ${bucketTwo.RBTC.fiatAmount}`}
         />
         <MetricsCard
           title="Treasury 3 RBTC Holdings"
-          amount={bucketThree.RBTC.amount}
+          amount={toFixed(bucketThree.RBTC.amount)}
           fiatAmount={`= USD ${bucketThree.RBTC.fiatAmount}`}
         />
       </div>
