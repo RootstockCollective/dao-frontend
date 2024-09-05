0aa78ebc79871036fe96e40adbdbcf18f3acb4b9
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2024-09-05 02:54:19 -0300

    transform returned data

--- src/app/proposals/hooks/useFetchLatestProposals.ts
+++ src/app/proposals/hooks/useFetchLatestProposals.ts
@@ -1,39 +1,28 @@
 import { fetchProposalsCreatedCached } from '@/app/user/Balances/actions'
 import { GovernorAbi } from '@/lib/abis/Governor'
 import { useQuery } from '@tanstack/react-query'
-import { useMemo } from 'react'
 import { parseEventLogs } from 'viem'
 
 export const useFetchLatestProposals = () => {
   const { data, isLoading } = useQuery({
     queryFn: fetchProposalsCreatedCached,
     queryKey: ['proposalsCreated'],
-    staleTime: 60000,
+    staleTime: 60000, // 60 seconds
     retry: 10,
     retryDelay: attemptIndex => Math.min(1000 * 2 ** attemptIndex, 30000), // Exponential backoff
-  })
-
-  const latestProposals = useMemo(() => {
-    if (data?.data) {
-      const proposals = parseEventLogs({
+    select: ({ data }) =>
+      parseEventLogs({
         abi: GovernorAbi,
-        logs: data.data,
+        logs: data,
         eventName: 'ProposalCreated',
       })
+        .filter(
+          (proposal, index, self) =>
+            self.findIndex(p => p.args.proposalId === proposal.args.proposalId) === index,
+        )
+        // @ts-ignore
+        .sort((a, b) => b.timeStamp - a.timeStamp),
+  })
 
-      // remove duplicates
-      return (
-        proposals
-          .filter(
-            (proposal, index, self) =>
-              self.findIndex(p => p.args.proposalId === proposal.args.proposalId) === index,
-          )
-          // @ts-ignore
-          .sort((a, b) => b.timeStamp - a.timeStamp)
-      )
-    }
-    return []
-  }, [data])
-
-  return { latestProposals, isLoading }
+  return { latestProposals: data || [], isLoading }
 }
