2d3a655d1ade3678743c19ff5da9d9c07fd74df1
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2024-09-11 18:39:21 -0300

    footer in landing page

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import { BUILD_ID, GITHUB_ORG } from '@/lib/constants'
+import { cn } from '@/lib/utils'
 import { FaGithub, FaSlack, FaXTwitter } from 'react-icons/fa6'
 
 interface Props {
@@ -8,7 +9,7 @@ interface Props {
 
 const DEFAULT_CONTAINER_CLASS = 'my-4'
 
-const DEFAULT_LOGIN_CLASS = 'absolute'
+const DEFAULT_LOGIN_CLASS = 'absolute left-8 w-2/5 '
 
 const VARIANTS = {
   container: DEFAULT_CONTAINER_CLASS,
@@ -18,15 +19,11 @@ const VARIANTS = {
 export const Footer = ({ brand = 'RootstockCollective', variant = 'login' }: Props) => {
   const year = new Date().getFullYear()
   return (
-    <footer className={`bottom-4 w-full flex justify-around text-sm ${VARIANTS[variant]}`}>
+    <footer className={cn('bottom-4 flex justify-around text-sm opacity-60', VARIANTS[variant])}>
       <div className="flex flex-col">
-        <p className="text-xs text-white">
-          Built by <span className="text-sm">{brand}</span>
-        </p>
-        <span className="text-[0.5rem] text-white opacity-50">
-          Copyright © {year} {brand}. All rights reserved.
-        </span>
-        <span className="text-[0.5rem] text-white opacity-50">
+        <p className="text-sm text-white">About RootstockCollective</p>
+        <span className="text-[0.5rem] text-white">Built by {brand}</span>
+        <span className="text-[0.5rem] text-white">
           <a
             href={`https://github.com/${GITHUB_ORG}/dao-frontend/commit/${BUILD_ID}`}
             className="hover:underline me-4 md:me-6"
@@ -36,36 +33,34 @@ export const Footer = ({ brand = 'RootstockCollective', variant = 'login' }: Pro
             {BUILD_ID ? BUILD_ID.slice(0, 7) : ''}
           </a>
         </span>
+        <span className="text-[0.5rem] text-white">
+          Copyright © {year} {brand}. All rights reserved.
+        </span>
       </div>
-      <div className="flex justify-center items-center flex-row">
-        <a href="#" className="hover:underline me-4 md:me-6">
-          About RootstockCollective
-        </a>
-        <a href="#" className="hover:underline me-4 md:me-6">
-          FAQs
-        </a>
-        <a href="https://wiki.rootstockcollective.xyz" className="hover:underline me-4 md:me-6">
-          Whitepaper
-        </a>
-        <a href="#" className="hover:underline">
-          Discourse
-        </a>
-      </div>
-      {variant === 'container' ? (
-        <div className="flex-1 items-center">
-          <a href="https://x.com/rootstockcoll">
-            <FaXTwitter className="mr-4" size={'1.5em'} />
+      {variant === 'container' && (
+        <div className="flex justify-center items-center flex-row">
+          <a href="#" className="hover:underline me-4 md:me-6">
+            FAQs
           </a>
-          <a href={`https://github.com/${GITHUB_ORG}`}>
-            <FaGithub className="mr-4" size={'1.5em'} />
+          <a href="https://wiki.rootstockcollective.xyz" className="hover:underline me-4 md:me-6">
+            Whitepaper
           </a>
-          <a href="#">
-            <FaSlack size={'1.5em'} />
+          <a href="#" className="hover:underline">
+            Discourse
           </a>
         </div>
-      ) : (
-        <div></div>
       )}
+      <div className="flex flex-1 items-start justify-end">
+        <a href="https://x.com/rootstockcoll">
+          <FaXTwitter className="mr-4" size={'1.5em'} />
+        </a>
+        <a href={`https://github.com/${GITHUB_ORG}`}>
+          <FaGithub className="mr-4" size={'1.5em'} />
+        </a>
+        <a href="#">
+          <FaSlack size={'1.5em'} />
+        </a>
+      </div>
     </footer>
   )
 }
