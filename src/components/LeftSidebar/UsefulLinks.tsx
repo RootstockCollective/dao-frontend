d54fd927560de92c9458e8a5a4d83e78c8bea829
Francis Rodriguez <francisngg@gmail.com>
2024-09-12 11:15:34 -0400

    fix: useful links fix as they were overlapping

--- src/components/LeftSidebar/UsefulLinks.tsx
+++ src/components/LeftSidebar/UsefulLinks.tsx
@@ -5,17 +5,19 @@ import { currentLinks } from '@/components/LeftSidebar/links'
 export const UsefulLinks = () => (
   <div className="mt-[4rem]">
     <Paragraph className="mb-[24px] text-[16px]">Useful links</Paragraph>
-    <Link href={currentLinks.rif} variant="menu" className="mb-[16px] text-[16px]" target="_blank">
-      RIF
-    </Link>
-    <Link href={currentLinks.rbtc} variant="menu" className="mb-[16px] text-[16px]" target="_blank">
-      Get RBTC
-    </Link>
-    <Link href={currentLinks.tokenBridge} variant="menu" className="mb-[16px] text-[16px]" target="_blank">
-      Bridge tokens
-    </Link>
-    <Link href={currentLinks.registerRns} variant="menu" className="mb-[16px] text-[16px]" target="_blank">
-      Get RNS Domain
-    </Link>
+    <div className="flex flex-col">
+      <Link href={currentLinks.rif} variant="menu" className="mb-[16px] text-[16px]" target="_blank">
+        RIF
+      </Link>
+      <Link href={currentLinks.rbtc} variant="menu" className="mb-[16px] text-[16px]" target="_blank">
+        Get RBTC
+      </Link>
+      <Link href={currentLinks.tokenBridge} variant="menu" className="mb-[16px] text-[16px]" target="_blank">
+        Bridge tokens
+      </Link>
+      <Link href={currentLinks.registerRns} variant="menu" className="mb-[16px] text-[16px]" target="_blank">
+        Get RNS Domain
+      </Link>
+    </div>
   </div>
 )
