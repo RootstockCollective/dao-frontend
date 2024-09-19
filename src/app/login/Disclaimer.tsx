7aa4f85a7ba7f5ad56fd75dba91cd15571c455ab
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2024-09-19 10:51:32 -0300

    add terms of use

--- src/app/login/Disclaimer.tsx
+++ src/app/login/Disclaimer.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/Button'
 import { ConnectButton } from '@/components/Header'
-import { Paragraph } from '@/components/Typography'
+import { Link } from '@/components/Link'
+import { Paragraph, Span } from '@/components/Typography'
 import { FC } from 'react'
 
 interface Props {
@@ -12,11 +13,18 @@ export const Disclaimer: FC<Props> = ({ onConnect, onCancel }) => (
   <div className="flex flex-col items-center mt-12 w-3/4">
     <Paragraph size="large" className="font-rootstock-sans">
       The RootstockCollective has taken actions in order to prevent access to any person located in the
-      prohibited jurisdictions, as mentioned in the Terms of Use, including any person located in the United
-      States of America. Please note that interaction with the dApp by any person or entity considered a
-      resident, or taxpayer in a prohibited jurisdiction, including without limitation the United States of
-      America, is forbidden. Please read the terms and conditions carefully before using the
-      RootstockCollective dApp.
+      prohibited jurisdictions, as mentioned in the{' '}
+      <a
+        href="https://wiki.rootstockcollective.xyz/Terms-of-Use-1041ca6b0b028041a0b1de60e2c16f3c"
+        target="_blank"
+        className="underline cursor-pointer"
+      >
+        Terms of Use
+      </a>
+      , including any person located in the United States of America. Please note that interaction with the
+      dApp by any person or entity considered a resident, or taxpayer in a prohibited jurisdiction, including
+      without limitation the United States of America, is forbidden. Please read the terms and conditions
+      carefully before using the RootstockCollective dApp.
     </Paragraph>
     <div className="flex mt-12">
       <ConnectButton onSuccess={onConnect} variant="white">
