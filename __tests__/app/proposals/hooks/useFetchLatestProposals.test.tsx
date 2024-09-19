209456f7efa270ec802064cb34999d5ccfa0ed8d
Juraj <juraj@iovlabs.org>
2024-09-19 23:11:50 +0100

    TOK-253: add bim proposal fetching (#18)
    * feat(bim): adds proposal fetching
    
    * feat(bim): adds proposal fetch test

---
+++ __tests__/app/proposals/hooks/useFetchLatestProposals.test.tsx
@@ -0,0 +1,262 @@
+import { SimplifiedRewardDistributorAbi } from '@/lib/abis/SimplifiedRewardDistributorAbi'
+import { expect } from '@jest/globals'
+import { QueryClient, QueryClientProvider, UseQueryResult } from '@tanstack/react-query'
+import { renderHook, waitFor } from '@testing-library/react'
+import { Interface } from 'ethers/abi'
+import { parseEventLogs } from 'viem'
+import { fetchProposalsCreatedCached } from '@/app/user/Balances/actions'
+import { useFetchLatestBIMProposals } from '@/app/proposals/hooks/useFetchLatestProposals'
+
+jest.mock('@/app/user/Balances/actions', () => ({
+  fetchProposalsCreatedCached: jest.fn(),
+}))
+
+jest.mock('viem', () => ({
+  parseEventLogs: jest.fn(),
+}))
+
+const BIM_WHITELIST_FUNCTION = 'whitelistBuilder' // TODO: refactor
+const BIM_WHITELIST_FUNCTION_SELECTOR = new Interface(SimplifiedRewardDistributorAbi).getFunction(
+  BIM_WHITELIST_FUNCTION,
+)?.selector
+
+const builder_1 = '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826'
+const builder_2 = '0x45418b3cc0CF56847A8A3C3004961c572E259142'
+
+// Create a new instance of QueryClient for each test
+const createTestQueryClient = () =>
+  new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: true, // Disable retries to simplify test logic
+      },
+    },
+  })
+
+// Reusable function to wrap renderHook with QueryClientProvider
+const renderHookWithQueryClient = <P, R>(hook: (initialProps: P) => R) => {
+  const queryClient = createTestQueryClient()
+
+  return renderHook(hook, {
+    wrapper: ({ children }) => <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>,
+  })
+}
+
+const renderAndWaitForHook = async <P, R>(hook: (initialProps: P) => UseQueryResult<R>) => {
+  const { result } = renderHookWithQueryClient(hook)
+
+  await waitFor(() => {
+    expect(result.current.isLoading).toBe(false)
+  })
+
+  return result.current.data
+}
+
+const mocFetchProposalsCreatedCached = fetchProposalsCreatedCached as jest.Mock
+const mockParseEventLogs = parseEventLogs as jest.Mock
+
+describe('useFetchLatestBIMProposals', () => {
+  let queryClient: QueryClient
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+    queryClient = createTestQueryClient()
+  })
+
+  it('should return an empty object when no data is fetched', async () => {
+    mocFetchProposalsCreatedCached.mockReturnValue({ data: null })
+    const expectedValue = {}
+
+    const actualValue = await renderAndWaitForHook(() => useFetchLatestBIMProposals())
+
+    expect(actualValue).toEqual(expectedValue)
+  })
+
+  it('should return proposals', async () => {
+    const mockProposalData = {
+      data: [
+        {
+          args: {
+            proposalId: 1,
+            calldatas: [`${BIM_WHITELIST_FUNCTION_SELECTOR}${builder_1.slice(2)}12345678890098765`],
+          },
+          timeStamp: 2000,
+        },
+      ],
+    }
+
+    const expectedValue = {
+      [builder_1]: {
+        [mockProposalData.data[0].args.proposalId.toString()]: mockProposalData.data[0],
+      },
+    }
+
+    mocFetchProposalsCreatedCached.mockReturnValue(mockProposalData)
+    mockParseEventLogs.mockReturnValue(mockProposalData.data)
+
+    let actualValue = await renderAndWaitForHook(() => useFetchLatestBIMProposals())
+
+    expect(actualValue).toEqual(expectedValue)
+  })
+
+  it('should handle proposals without calldatas', async () => {
+    const mockDataWithoutCalldatas = {
+      data: [
+        {
+          args: {
+            proposalId: 1,
+            calldatas: [],
+          },
+          timeStamp: 1000,
+        },
+      ],
+    }
+    mocFetchProposalsCreatedCached.mockReturnValue({ data: mockDataWithoutCalldatas })
+    mockParseEventLogs.mockReturnValue(mockDataWithoutCalldatas.data)
+    const expectedValue = {}
+
+    let actualValue = await renderAndWaitForHook(() => useFetchLatestBIMProposals())
+
+    expect(actualValue).toEqual(expectedValue)
+  })
+
+  it('should handle proposals with unrelated calldatas', async () => {
+    const mockDataWithoutCalldatas = {
+      data: [
+        {
+          args: {
+            proposalId: 1,
+            calldatas: ['0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef'],
+          },
+          timeStamp: 1000,
+        },
+      ],
+    }
+    mocFetchProposalsCreatedCached.mockReturnValue({ data: mockDataWithoutCalldatas })
+    mockParseEventLogs.mockReturnValue(mockDataWithoutCalldatas.data)
+    const expectedValue = {}
+
+    let actualValue = await renderAndWaitForHook(() => useFetchLatestBIMProposals())
+
+    expect(actualValue).toEqual(expectedValue)
+  })
+
+  it('should return unique proposals sorted by timestamp', async () => {
+    const mockData = {
+      data: [
+        {
+          args: {
+            proposalId: 1,
+            calldatas: [`${BIM_WHITELIST_FUNCTION_SELECTOR}${builder_1.slice(2)}12345678890098765`],
+          },
+          timeStamp: 1500,
+        },
+        {
+          args: {
+            proposalId: 3,
+            calldatas: [`${BIM_WHITELIST_FUNCTION_SELECTOR}${builder_1.slice(2)}12345678890098765`],
+          },
+          timeStamp: 3000,
+        },
+        {
+          args: {
+            proposalId: 1,
+            calldatas: [`${BIM_WHITELIST_FUNCTION_SELECTOR}${builder_1.slice(2)}12345678890098765`],
+          },
+          timeStamp: 1500,
+        },
+        {
+          args: {
+            proposalId: 2,
+            calldatas: [`${BIM_WHITELIST_FUNCTION_SELECTOR}${builder_1.slice(2)}12345678890098765`],
+          },
+          timeStamp: 2000,
+        },
+      ],
+    }
+    mocFetchProposalsCreatedCached.mockReturnValue(mockData)
+    mockParseEventLogs.mockReturnValue(mockData.data)
+
+    const expectedValue = {
+      [builder_1]: {
+        [mockData.data[0].args.proposalId.toString()]: mockData.data[0],
+        [mockData.data[1].args.proposalId.toString()]: mockData.data[1],
+        [mockData.data[3].args.proposalId.toString()]: mockData.data[3], // a Record does not preserve order so we can not guarantee the order of the proposals unless we use a Map.
+      },
+    }
+
+    let actualValue = await renderAndWaitForHook(() => useFetchLatestBIMProposals())
+
+    expect(actualValue).toEqual(expectedValue)
+  })
+
+  it('should handle multiple proposals with the same builder', async () => {
+    const mockData = {
+      data: [
+        {
+          args: {
+            proposalId: 1,
+            calldatas: [`${BIM_WHITELIST_FUNCTION_SELECTOR}${builder_1.slice(2)}12345678890098765`],
+          },
+          timeStamp: 1500,
+        },
+        {
+          args: {
+            proposalId: 2,
+            calldatas: [`${BIM_WHITELIST_FUNCTION_SELECTOR}${builder_1.slice(2)}12345678890098765`],
+          },
+          timeStamp: 2000,
+        },
+      ],
+    }
+    mocFetchProposalsCreatedCached.mockReturnValue(mockData)
+    mockParseEventLogs.mockReturnValue(mockData.data)
+
+    const expectedValue = {
+      [builder_1]: {
+        [mockData.data[0].args.proposalId.toString()]: mockData.data[0],
+        [mockData.data[1].args.proposalId.toString()]: mockData.data[1],
+      },
+    }
+
+    let actualValue = await renderAndWaitForHook(() => useFetchLatestBIMProposals())
+
+    expect(actualValue).toEqual(expectedValue)
+  })
+
+  it('should handle multiple builders', async () => {
+    const mockData = {
+      data: [
+        {
+          args: {
+            proposalId: 1,
+            calldatas: [`${BIM_WHITELIST_FUNCTION_SELECTOR}${builder_1.slice(2)}12345678890098765`],
+          },
+          timeStamp: 1500,
+        },
+        {
+          args: {
+            proposalId: 2,
+            calldatas: [`${BIM_WHITELIST_FUNCTION_SELECTOR}${builder_2.slice(2)}12345678890098765`],
+          },
+          timeStamp: 2000,
+        },
+      ],
+    }
+    mocFetchProposalsCreatedCached.mockReturnValue(mockData)
+    mockParseEventLogs.mockReturnValue(mockData.data)
+
+    const expectedValue = {
+      [builder_1]: {
+        [mockData.data[0].args.proposalId.toString()]: mockData.data[0],
+      },
+      [builder_2]: {
+        [mockData.data[1].args.proposalId.toString()]: mockData.data[1],
+      },
+    }
+
+    let actualValue = await renderAndWaitForHook(() => useFetchLatestBIMProposals())
+
+    expect(actualValue).toEqual(expectedValue)
+  })
+})
--- src/app/proposals/hooks/useFetchLatestProposals.ts
+++ src/app/proposals/hooks/useFetchLatestProposals.ts
@@ -1,8 +1,10 @@
-import { useMemo } from 'react'
-import { useQuery } from '@tanstack/react-query'
-import { parseEventLogs } from 'viem'
-import { GovernorAbi } from '@/lib/abis/Governor'
 import { fetchProposalsCreatedCached } from '@/app/user/Balances/actions'
+import { GovernorAbi } from '@/lib/abis/Governor'
+import { SimplifiedRewardDistributorAbi } from '@/lib/abis/SimplifiedRewardDistributorAbi'
+import { useQuery, UseQueryResult } from '@tanstack/react-query'
+import { Interface } from 'ethers'
+import { useMemo } from 'react'
+import { parseEventLogs } from 'viem'
 
 export const useFetchLatestProposals = () => {
   const { data } = useQuery({
@@ -34,4 +36,83 @@ export const useFetchLatestProposals = () => {
   }, [data])
 
   return { latestProposals }
+}
+
+const BIM_WHITELIST_FUNCTION = 'whitelistBuilder' // TODO: refactor
+const BIM_WHITELIST_FUNCTION_SELECTOR = new Interface(SimplifiedRewardDistributorAbi).getFunction(
+  BIM_WHITELIST_FUNCTION,
+)?.selector
+if (!BIM_WHITELIST_FUNCTION_SELECTOR) {
+  throw new Error(`Function ${BIM_WHITELIST_FUNCTION} not found in SimplifiedRewardDistributorAbi.`)
+}
+
+type ElementType<T> = T extends (infer U)[] ? U : never
+
+type EventLog = ElementType<ReturnType<typeof parseEventLogs<typeof GovernorAbi, true, 'ProposalCreated'>>>
+
+export type BimProposalCachedEvent = {
+  event: EventLog
+  builder: string
+}
+
+export type BimProposalMap = Record<
+  BimProposalCachedEvent['builder'],
+  Record<string, BimProposalCachedEvent['event']>
+>
+
+export const useFetchLatestBIMProposals = (): UseQueryResult<BimProposalMap> => {
+  const queryResult = useQuery({
+    queryFn: async () => {
+      const fetchedData = await fetchProposalsCreatedCached()
+
+      if (!fetchedData?.data) {
+        return {} as BimProposalMap
+      }
+
+      const events = parseEventLogs({
+        abi: GovernorAbi,
+        logs: fetchedData.data,
+        eventName: 'ProposalCreated',
+      })
+
+      // deduplicate
+      const eventsMap = new Map(events.map(eventItem => [eventItem.args.proposalId, eventItem])).values()
+
+      // why can't we use block number instead of timestamp which is not supported by the type?
+      const sortedEvents = Array.from(eventsMap).sort(
+        (a, b) =>
+          (b as unknown as { timeStamp: number }).timeStamp -
+          (a as unknown as { timeStamp: number }).timeStamp,
+      )
+
+      const bimProposalMap = sortedEvents.reduce<BimProposalMap>((acc, event) => {
+        const bimEventCalldatas = event.args.calldatas.find(calldata =>
+          calldata.startsWith(BIM_WHITELIST_FUNCTION_SELECTOR),
+        )
+
+        if (bimEventCalldatas) {
+          const addressStart = BIM_WHITELIST_FUNCTION_SELECTOR.length
+          const addressEnd = addressStart + 40
+          const builder = `0x${bimEventCalldatas.slice(addressStart, addressEnd)}`
+          const existingBuilder = acc[builder] || {}
+
+          acc = {
+            ...acc,
+            [builder]: {
+              ...existingBuilder,
+              [event.args.proposalId.toString()]: event,
+            },
+          }
+        }
+
+        return acc
+      }, {})
+
+      return bimProposalMap
+    },
+    queryKey: ['bimProposalCreated'],
+    refetchInterval: 30_000,
+  })
+
+  return queryResult as UseQueryResult<BimProposalMap>
 }
