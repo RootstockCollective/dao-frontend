a49e8552a4fd75dc4766b053205d132f6c276ef4
Francis Rodriguez <francisngg@gmail.com>
2024-10-01 20:53:56 -0400

    DAO-705 Improved loading times in dev env. This is caused by react-icons. It's importing all icons. Check https://github.com/vercel/next.js/issues/45529

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  modularizeImports: {
+    'react-icons': {
+      transform: 'react-icons/{{member}}',
+    },
+  },
 }
 
 export default nextConfig
