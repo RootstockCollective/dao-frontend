0ccda8dac4528a377d2279cdbbdd6e3e486f7fb4
Francis Rodriguez <francisngg@gmail.com>
2024-10-03 17:40:04 -0400

    DAO-710 feat: added HeaderTitle.tsx which is the typography that will be used for section titles. BREAKING CHANGE: changed background color and foreground color fix: removed plus icon from create proposal button. fix: will now use HeaderTitle in the HeaderSection for proposals page fix: will use HeaderTitle in the proposals page where latest proposals is fix: edited columns name fix: edited date output format feat: edited MetricsCard.tsx to account for new style changes. fix: My voting power in proposals is not a metric card anymore; this component was created there. fix: adjusted width of metrics card of proposals created to 272 pixels fix: camel case my voting power fix: proposal name column will not wrap, and the text will be white due to new changes in the table design. BREAKING CHANGE: table header will now contain new style changes. BREAKING CHANGE: the table will have no rounded borders anymore due to design change. BREAKING CHANGE: table head color has changed to the foreground per new design changes fix: exported Typography type so it can be used.

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -53,7 +53,11 @@ export const Table: FC<TableProps> = ({ data, equalColumns = true, tbodyProps, t
       </TableHead>
       <TableBody {...tbodyProps}>
         {data.map((record, i) => (
-          <TableRow key={i} className="text-[14px] border-hidden" style={{ borderTopStyle: 'solid' }}>
+          <TableRow
+            key={i}
+            className="text-[14px] border-hidden text-[rgba(255,255,255,0.6)]" // @TODO this color here move it
+            style={{ borderTopStyle: 'solid' }}
+          >
             {Object.values(record).map((val, j) => (
               <TableCell style={{ width }} key={j}>
                 <Span className="text-[14px]" variant="light">
