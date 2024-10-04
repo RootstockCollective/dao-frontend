3916db5d701b26e77ccf1feb6caae6e7f680412d
Francis Rodriguez <39339295+Freshenext@users.noreply.github.com>
2024-10-04 06:53:52 -0400

    DAO-708 Reduced number of JS files when building. (#237)

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  webpack: (
+    config
+  ) => {
+    config.optimization.splitChunks = {
+      chunks: 'all',
+      maxInitialRequests: 2, // Limit the number of requests on initial page load
+      maxAsyncRequests: 2, // Limit the number of requests when loading dynamically imported modules
+    }
+    return config
+  },
 }
 
 export default nextConfig
