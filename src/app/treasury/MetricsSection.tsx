2569f113fe8b1b5325233bd37c5b4cd72098297c
Francis Rodriguez <39339295+Freshenext@users.noreply.github.com>
2024-10-07 12:04:15 -0400

    DAO-XXX The treasury strif will show rif price as default (#253)

--- src/app/treasury/MetricsSection.tsx
+++ src/app/treasury/MetricsSection.tsx
@@ -1,12 +1,20 @@
 import { HeaderTitle } from '@/components/Typography'
 import { Table } from '@/components/Table'
 import { TokenHoldingsStRIF } from '@/app/treasury/TokenHoldingsStRIF'
+import { usePricesContext } from '@/shared/context/PricesContext'
+import { formatCurrency } from '@/lib/utils'
+
+const StRIFPrice = () => {
+  const { prices } = usePricesContext()
+  const symbol = 'stRIF'
+  return formatCurrency(prices[symbol]?.price || 0)
+}
 
 const tableData = [
   {
     token: 'Staked Rootstock Infrastructure Framework',
     symbol: 'stRIF',
-    price: '—',
+    price: <StRIFPrice />,
     holdings: <TokenHoldingsStRIF />,
   },
 ]
