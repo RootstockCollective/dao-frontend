69c33b750910b4a36a4dac09615514f41f1b514a
Francis Rodriguez <francisngg@gmail.com>
2024-10-11 10:52:19 -0400

    DAO-XXX-BG-FIX fix: background image not being built by tailwind css dynamically. Had to use another method so that the BG renders properly.

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,1 +1,3 @@
-export const BG_IMG_CLASSES = 'bg-[url(../../public/images/login-bg.svg)] bg-cover bg-no-repeat bg-right'
+import './utils.css'
+// Had to be done this way because tailwind css is not dynamically rendering the image on build
+export const BG_IMG_CLASSES = 'background-logo'
