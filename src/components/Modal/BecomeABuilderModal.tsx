a9c3b28943901cfe104aa28fc00c88c07aa1d4b3
franciscotobar <100875069+franciscotobar@users.noreply.github.com>
2024-10-14 04:13:21 -0600

    TOK-302: disclaimer modal (#46)
    * feat(bim): disclaimer modal
    
    * chore: add comment
    
    * feat: add kyc link
    
    ---------
    
    Co-authored-by: Antonio <antonio@iovlabs.org>

--- src/components/Modal/BecomeABuilderModal.tsx
+++ src/components/Modal/BecomeABuilderModal.tsx
@@ -1,16 +1,42 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { cn } from '@/lib/utils'
 import { Modal } from '@/components/Modal/Modal'
 import { Header, Typography } from '@/components/Typography'
 import { Button } from '@/components/Button'
 import { BsCardText } from 'react-icons/bs'
 import { RiPassportLine } from 'react-icons/ri'
-import { BuilderStatus } from '@/app/bim/types'
 import { useRouter } from 'next/navigation'
 import { SupportedActionAbiName, SupportedProposalActionName } from '@/app/proposals/shared/supportedABIs'
 
-interface BecomeABuilderModalProps {
-  onClose: () => void
+type DisclaimerProps = {
+  onAccept: () => void
+  onDecline: () => void
+}
+
+const Disclaimer: FC<DisclaimerProps> = ({ onAccept, onDecline }) => {
+  return (
+    <>
+      {/* TODO: we need to change it when we have access in dev mode to the design */}
+      <Header variant="h1" className="font-bold text-center py-[50px]">
+        Disclaimer
+      </Header>
+
+      <div className="flex flex-row pb-[50px] justify-center">
+        <Typography tagVariant="span" className="text-[16px] font-light w-9/12">
+          By submitting your request to become a whitelisted Builder in the Collective Rewards Program, you
+          hereby acknowledge and agree to be bound to the terms of the Collective Rewards Program and to
+          submit your KYC information to the RootstockCollective Foundation. You accept that the information
+          submitted will be treated pursuant to the Privacy Policy.
+        </Typography>
+      </div>
+      <div className="flex flex-row justify-center items-center text-center gap-x-20">
+        <Button onClick={onAccept}>I agree</Button>
+        <Button variant="secondary" onClick={onDecline}>
+          I Disagree
+        </Button>
+      </div>
+    </>
+  )
 }
 
 // TODO: check if this is a hardcoded list or if it should be dynamic
@@ -51,9 +77,46 @@ const steps: Array<Step> = [
   },
 ]
 
-export const BecomeABuilderModal: FC<BecomeABuilderModalProps> = ({ onClose }) => {
-  const router = useRouter()
+type BecomeABuilderProps = {
+  onStartKYC: () => void
+  onSubmitProposal: () => void
+}
 
+const BecomeABuilder: FC<BecomeABuilderProps> = ({ onStartKYC, onSubmitProposal }) => {
+  return (
+    <>
+      <Header variant="h1" className="font-bold text-center py-[50px]">
+        How to become a builder
+      </Header>
+
+      <div className="flex flex-row pb-[50px] justify-center">
+        <Typography tagVariant="span" className="text-[16px] font-light w-9/12">
+          <RecursiveOrderedList steps={steps} className="list-decimal" />
+        </Typography>
+      </div>
+      <div className="flex flex-row justify-center items-center text-center gap-x-20">
+        <div className="flex flex-col items-center gap-4">
+          <RiPassportLine size={48} />
+          Verify your <br /> profile persona
+          <Button onClick={onStartKYC}>Start KYC</Button>
+        </div>
+        <div className="flex flex-col items-center gap-4">
+          <BsCardText size={48} />
+          Create a <br /> proposal
+          <Button onClick={onSubmitProposal}>Submit proposal</Button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+type BecomeABuilderModalProps = {
+  onClose: () => void
+}
+export const BecomeABuilderModal: FC<BecomeABuilderModalProps> = ({ onClose }) => {
+  const [isDisclaimerAccepted, setIsDisclaimerAccepted] = useState(false)
+
+  const router = useRouter()
   const contractName: SupportedActionAbiName = 'SimplifiedRewardDistributorAbi'
   const action: SupportedProposalActionName = 'whitelistBuilder'
   const formLink =
@@ -62,30 +125,16 @@ export const BecomeABuilderModal: FC<BecomeABuilderModalProps> = ({ onClose }) =
   return (
     <Modal onClose={onClose} width={1016}>
       <div className="px-[50px] pt-[21px] pb-[42px] flex flex-col justify-center">
-        <Header variant="h1" className="font-bold text-center py-[50px]">
-          How to become a builder
-        </Header>
-        <div className="flex flex-row pb-[50px] justify-center">
-          <Typography tagVariant="span" className="text-[16px] font-light w-9/12">
-            <RecursiveOrderedList steps={steps} className="list-decimal" />
-          </Typography>
-        </div>
-        <div className="flex flex-row justify-center items-center text-center gap-x-20">
-          <div className="flex flex-col items-center gap-4">
-            <RiPassportLine size={48} />
-            Verify your <br /> profile persona
-            <Button onClick={() => window.open(formLink, '_blank')}>Start KYC</Button>
-          </div>
-          <div className="flex flex-col items-center gap-4">
-            <BsCardText size={48} />
-            Create a <br /> proposal
-            <Button
-              onClick={() => router.push(`/proposals/create?contract=${contractName}&action=${action}`)}
-            >
-              Submit proposal
-            </Button>
-          </div>
-        </div>
+        {isDisclaimerAccepted ? (
+          <BecomeABuilder
+            onSubmitProposal={() =>
+              router.push(`/proposals/create?contract=${contractName}&action=${action}`)
+            }
+            onStartKYC={() => window.open(formLink, '_blank')}
+          />
+        ) : (
+          <Disclaimer onAccept={() => setIsDisclaimerAccepted(true)} onDecline={onClose} />
+        )}
       </div>
     </Modal>
   )
