368b7661c7c12557aca01963e91492d5ab0d94e3
Jesse Clark <hello@developerjesse.com>
2024-10-29 19:40:33 +0200

    Add isMintable flag.

--- src/app/communities/communityUtils.tsx
+++ src/app/communities/communityUtils.tsx
@@ -19,6 +19,7 @@ export const firstCommunity = {
     'The Early Adopters collection features a vibrant array of digital pioneers, each uniquely crafted to embody the spirit of innovation and community in the blockchain world.',
   nftAddress: nftContracts.EA,
   numberOfMembers: 0,
+  isMintable: true,
   longDescription: (
     <>
       <p className="mb-4">
@@ -48,6 +49,7 @@ export const ogFounders = {
     'In the magical realm of Rootstock, a select group of visionary elves, known as the Founders, received the prestigious OG Badge. This badge was a testament to their foresight and pioneering spirit, marking them as the true architects of their era.',
   nftAddress: nftContracts.OG_FOUNDERS,
   numberOfMembers: 0,
+  isMintable: true,
 }
 
 export const ogFoundersEcosystemPartners = {
@@ -58,6 +60,7 @@ export const ogFoundersEcosystemPartners = {
     'In the magical realm of Rootstock, a new chapter unfolds as the elves of the RootstockCollective embark on an extraordinary journey. Equipped with the OG Badge, these visionary elves invite their esteemed external partners to join them in exploring and innovating within the Rootstock ecosystem.',
   nftAddress: nftContracts.OG_PARTNERS,
   numberOfMembers: 0,
+  isMintable: false,
 }
 
 export const ogFoundersExternalContributors = {
@@ -68,6 +71,7 @@ export const ogFoundersExternalContributors = {
     'In the enchanting realm of Rootstock, a thrilling new chapter unfolds as the elves of the RootstockCollective embark on an extraordinary journey. These visionary elves, recognized for their remarkable contributions, are bestowed with the prestigious OG Badge—a symbol of honor that grants them special powers.',
   nftAddress: nftContracts.OG_CONTRIBUTORS,
   numberOfMembers: 0,
+  isMintable: false,
 }
 
 export const communitiesToRender = [
