f2030c19cd875f82e1e3c6b83f2509ebff638a24
Francisco Tobar <francisco.tobar@iovlabs.org>
2024-10-31 11:31:38 -0600

    refactor: remove not use dependency

--- src/app/collective-rewards/actions.ts
+++ src/app/collective-rewards/actions.ts
@@ -1,15 +1,8 @@
-import { encodeEventTopics } from 'viem'
-import { SimplifiedRewardDistributorAbi } from '@/lib/abis/SimplifiedRewardDistributorAbi'
 import { SimplifiedRewardDistributorAddress } from '@/lib/contracts'
 import { axiosInstance } from '@/lib/utils'
 import { fetchRewardDistributedLogsByAddress } from '@/lib/endpoints'
 
 export const fetchRewardDistributedLogs = (fromBlock = 0) => {
-  const topic = encodeEventTopics({
-    abi: SimplifiedRewardDistributorAbi,
-    eventName: 'RewardDistributed',
-  })[0]
-
   return axiosInstance.get(
     fetchRewardDistributedLogsByAddress
       .replace('{{address}}', SimplifiedRewardDistributorAddress)
