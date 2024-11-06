5cf25024b55bc7e54f73c3a779c9ce8d54fc3f57
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2024-11-06 11:32:17 -0300

    fix communities banner image (#340)

--- src/app/communities/page.tsx
+++ src/app/communities/page.tsx
@@ -7,11 +7,17 @@ import { communitiesToRender } from '@/app/communities/communityUtils'
 export default function Communities() {
   return (
     <MainContainer notProtected>
-      <div className="ml-[24px]">
-        <div className="relative h-[407px]">
-          <Image src="/images/communities-header.png" alt="Communities" fill />
+      <div className="flex flex-col ml-[24px]">
+        <div>
+          <Image
+            src="/images/communities-header.svg"
+            alt="Communities"
+            width={0}
+            height={0}
+            style={{ width: '100%', height: 'auto' }}
+          />
         </div>
-        <div className="mt-[16px] flex flex-wrap gap-[24px]">
+        <div className="flex flex-wrap mt-[16px] gap-[24px]">
           {communitiesToRender.map(community => (
             <CommunityItem key={community.title} {...community} />
           ))}
