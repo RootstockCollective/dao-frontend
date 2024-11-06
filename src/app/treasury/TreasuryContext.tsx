ce8e274a09295ef0cf28a784cbbe1892a84e9f54
Talal <primezinadah@gmail.com>
2024-11-06 08:29:33 +0000

    Format to solve lint

--- src/app/treasury/TreasuryContext.tsx
+++ src/app/treasury/TreasuryContext.tsx
@@ -61,11 +61,11 @@ const getBucketBalance = (
 const TreasuryContextProvider = ({ children }: Props) => {
   const { prices } = usePricesContext()
 
-  const bucketOneBalance = useGetTreasuryBucketBalance(treasuryContracts["GRANTS"].address)
-  const bucketTwoBalance = useGetTreasuryBucketBalance(treasuryContracts["GRANTS_ACTIVE"].address)
-  const bucketThreeBalance = useGetTreasuryBucketBalance(treasuryContracts["GROWTH"].address)
-  const bucketFourBalance = useGetTreasuryBucketBalance(treasuryContracts["GROWTH_REWARDS"].address)
-  const bucketFiveBalance = useGetTreasuryBucketBalance(treasuryContracts["GENERAL"].address)
+  const bucketOneBalance = useGetTreasuryBucketBalance(treasuryContracts['GRANTS'].address)
+  const bucketTwoBalance = useGetTreasuryBucketBalance(treasuryContracts['GRANTS_ACTIVE'].address)
+  const bucketThreeBalance = useGetTreasuryBucketBalance(treasuryContracts['GROWTH'].address)
+  const bucketFourBalance = useGetTreasuryBucketBalance(treasuryContracts['GROWTH_REWARDS'].address)
+  const bucketFiveBalance = useGetTreasuryBucketBalance(treasuryContracts['GENERAL'].address)
 
   // Create the buckets array
   const buckets = useMemo(
