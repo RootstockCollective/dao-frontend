9d94374ecbdc6f6f413b2db7e10de1df95e3f468
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2024-11-12 12:32:13 -0300

    fix subtitle

--- src/app/user/Delegation/DelegationSection.tsx
+++ src/app/user/Delegation/DelegationSection.tsx
@@ -1,30 +1,28 @@
+import { HolderColumn } from '@/app/communities/HolderColumn'
 import { Table } from '@/components/Table'
 import { HeaderTitle } from '@/components/Typography'
+import { useAccount } from 'wagmi'
 import { RenderTotalBalance } from '../Balances/RenderTotalBalance'
 import { BalancesProvider } from '../Balances/context/BalancesContext'
 import { useDelegate } from './hooks/useDelegate'
-import { useAccount } from 'wagmi'
-import { HolderColumn } from '@/app/communities/HolderColumn'
 
 export const DelegationSection = () => {
   const { address } = useAccount()
   const { delegateeAddress } = useDelegate(address)
 
-  if (!delegateeAddress || address === delegateeAddress) {
+  if (!delegateeAddress || delegateeAddress === address) {
     return null
   }
 
-  const delegatees = [
-    {
-      'Voting Power Received': <HolderColumn address={delegateeAddress} />,
-      Amount: <RenderTotalBalance symbol="stRIF" />,
-    },
-  ]
+  const delegatee = {
+    'Voting Power Delegated': <HolderColumn address={delegateeAddress} />,
+    Amount: <RenderTotalBalance symbol="stRIF" />,
+  }
   return (
     <>
       <HeaderTitle className="mb-6">Delegation</HeaderTitle>
       <BalancesProvider>
-        <Table data={delegatees} />
+        <Table data={[delegatee]} />
       </BalancesProvider>
     </>
   )
