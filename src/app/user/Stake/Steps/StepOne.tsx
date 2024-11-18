6a70b20a73cb015394c46ccdd5d85d4659766315
Francis Rodriguez <francisngg@gmail.com>
2024-11-18 13:33:48 -0400

    DAO-842 Added unstake check when the user cannot withdraw due to collective reward allocations.

--- src/app/user/Stake/Steps/StepOne.tsx
+++ src/app/user/Stake/Steps/StepOne.tsx
@@ -2,10 +2,14 @@ import { StakeRIF } from '@/app/user/Stake/StakeRIF'
 import { useStakingContext } from '@/app/user/Stake/StakingContext'
 import { StepProps } from '@/app/user/Stake/types'
 import { formatCurrency, toFixed } from '@/lib/utils'
-import { useMemo } from 'react'
+import { useEffect, useMemo } from 'react'
+import { useCanAccountUnstakeAmount } from '@/shared/hooks/useCanAccountUnstakeAmount'
 
 export const StepOne = ({ onGoNext = () => {} }: StepProps) => {
   const { amount, onAmountChange, tokenToSend, actionName } = useStakingContext()
+
+  const { isCanAccountWithdrawLoading, canAccountWithdraw, refetchCanAccountWithdraw } =
+    useCanAccountUnstakeAmount(BigInt(amount))
 
   const balanceToCurrency = useMemo(
     () => Number(tokenToSend.price) * Number(tokenToSend.balance),
@@ -16,13 +20,32 @@ export const StepOne = ({ onGoNext = () => {} }: StepProps) => {
     onAmountChange((Number(tokenToSend.balance) * (percentage / 100)).toString())
   }
 
+  const shouldShowCannotWithdraw =
+    actionName === 'UNSTAKE' && !isCanAccountWithdrawLoading && !canAccountWithdraw
+
   const shouldEnableGoNext = useMemo(() => {
     if (Number(amount) <= 0) {
       return false
     }
+    // Extra unstake validation
+    if (actionName === 'UNSTAKE') {
+      if (isCanAccountWithdrawLoading) {
+        return false
+      }
+      if (shouldShowCannotWithdraw) {
+        return false
+      }
+    }
 
     return Number(amount) <= Number(tokenToSend.balance)
-  }, [amount, tokenToSend.balance])
+  }, [actionName, amount, isCanAccountWithdrawLoading, shouldShowCannotWithdraw, tokenToSend.balance])
+
+  useEffect(() => {
+    // Run extra validation when UNSTAKE
+    if (actionName === 'UNSTAKE') {
+      refetchCanAccountWithdraw()
+    }
+  }, [actionName, amount, refetchCanAccountWithdraw])
 
   return (
     <StakeRIF
@@ -35,6 +58,7 @@ export const StepOne = ({ onGoNext = () => {} }: StepProps) => {
       totalBalanceConverted={balanceToCurrency ? 'USD ' + formatCurrency(balanceToCurrency) : ''}
       symbol={tokenToSend.symbol}
       actionName={actionName}
+      shouldShowCannotWithdraw={shouldShowCannotWithdraw}
     />
   )
 }
