737bda0b87bb9fab393e8109ea5d35b1a3927ddb
Alex <shenshin@me.com>
2024-11-19 22:31:04 +0100

    feat(DebounceSearch): show spinner during debounce

--- src/components/DebounceSearch/DebounceSearch.tsx
+++ src/components/DebounceSearch/DebounceSearch.tsx
@@ -15,20 +15,44 @@ interface ProposalSearchProps {
   placeholder: string
 }
 
+const SPINNER_DEBOUNCE_MS = 300 // Delay after the user stops typing before showing the spinner
+const SEARCH_DEBOUNCE_MS = 700 // Additional delay before finalizing input and calling onSearchSubmit
+
 /**
- * DebounceSearch component allows delayed submission of search input to the parent.
- * It includes a "clear" button to reset the search field.
+ * DebounceSearch component that uses two levels of debouncing to control user experience:
+ *
+ * 1. The first debounce (300ms) detects when the user has stopped typing and shows a spinner
+ *    indicating that the application is "thinking."
+ * 2. The second debounce (700ms after the first debounce) finalizes the input, hides the spinner,
+ *    and triggers the onSearchSubmit function.
+ *
+ * This design ensures the spinner is only displayed after the user stops typing, creating a
+ * smooth and responsive UX. It prevents unnecessary actions or spinner flickering while typing.
  */
 export function DebounceSearch({ onSearchSubmit, placeholder = 'Search' }: ProposalSearchProps) {
   const [searchText, setSearchText] = useState('')
-  const [debouncedSearchText] = useDebounce(searchText, 1000)
+  // First debounce runs shortly after user stops typing. It launches spinner
+  const [isUserStoppedTyping] = useDebounce(searchText, SPINNER_DEBOUNCE_MS)
+  // Second debounce runs after the first one. When it finishes, it stops the spinner and calls on-search function
+  const [debouncedSearchText] = useDebounce(isUserStoppedTyping, SEARCH_DEBOUNCE_MS)
+  // shows/hides spinner
+  const [isLoading, setIsLoading] = useState(false)
+  useEffect(() => {
+    // Show the spinner as soon as the user stops typing
+    setIsLoading(true)
+  }, [isUserStoppedTyping])
+  useEffect(() => {
+    // Hide the spinner and trigger the search function after the second debounce
+    setIsLoading(false)
+    onSearchSubmit(debouncedSearchText)
+  }, [debouncedSearchText, onSearchSubmit])
+
   const handleClear = () => {
     setSearchText('')
   }
   const handleChange = (val: string) => {
     setSearchText(val)
   }
-  useEffect(() => onSearchSubmit(debouncedSearchText), [debouncedSearchText, onSearchSubmit])
   return (
     <Input
       value={searchText}
@@ -39,6 +63,7 @@ export function DebounceSearch({ onSearchSubmit, placeholder = 'Search' }: Propo
       type="search"
       className="my-5"
       onClear={handleClear}
+      loading={isLoading}
     />
   )
 }
