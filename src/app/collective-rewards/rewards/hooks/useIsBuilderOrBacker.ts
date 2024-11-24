df4e93a0a416ff0378392a16dc6babd3d1d74e9e
Antonio <antonio@iovlabs.org>
2024-11-24 17:59:04 +0100

    fix: keep into account backer allocation

--- src/app/collective-rewards/rewards/hooks/useIsBuilderOrBacker.ts
+++ src/app/collective-rewards/rewards/hooks/useIsBuilderOrBacker.ts
@@ -2,6 +2,7 @@ import { useMemo } from 'react'
 import { Address, zeroAddress } from 'viem'
 import { useGetBuilderToGauge } from '@/app/collective-rewards/user'
 import { useGetBackerRewardPerTokenPaid } from '@/app/collective-rewards/rewards/hooks'
+import { useBackerTotalAllocation } from '@/app/collective-rewards/allocations/hooks'
 
 export const useIsBuilderOrBacker = (address: Address) => {
   /*
@@ -12,23 +13,33 @@ export const useIsBuilderOrBacker = (address: Address) => {
   const { data: gauge, isLoading: isGaugeLoading, error: gaugeError } = useGetBuilderToGauge(address)
 
   const {
+    data: backerTotalAllocation,
+    isLoading: isBackerTotalAllocationLoading,
+    error: backerTotalAllocationError,
+  } = useBackerTotalAllocation(address)
+
+  const {
     data: backerRewardPerTokenPaid,
     isLoading: isBackerRewardPerTokenPaidLoading,
     error: backerRewardPerTokenPaidError,
   } = useGetBackerRewardPerTokenPaid(address)
 
   const data = useMemo(() => {
-    return (gauge && gauge !== zeroAddress) || backerRewardPerTokenPaid > 0n
-  }, [gauge, backerRewardPerTokenPaid])
+    return (
+      (gauge && gauge !== zeroAddress) ||
+      (backerTotalAllocation && backerTotalAllocation > 0n) ||
+      backerRewardPerTokenPaid > 0n
+    )
+  }, [gauge, backerRewardPerTokenPaid, backerTotalAllocation])
 
   const isLoading = useMemo(
-    () => isGaugeLoading || isBackerRewardPerTokenPaidLoading,
-    [isGaugeLoading, isBackerRewardPerTokenPaidLoading],
+    () => isGaugeLoading || isBackerRewardPerTokenPaidLoading || isBackerTotalAllocationLoading,
+    [isGaugeLoading, isBackerRewardPerTokenPaidLoading, isBackerTotalAllocationLoading],
   )
 
   const error = useMemo(
-    () => gaugeError ?? backerRewardPerTokenPaidError,
-    [gaugeError, backerRewardPerTokenPaidError],
+    () => gaugeError ?? backerRewardPerTokenPaidError ?? backerTotalAllocationError,
+    [gaugeError, backerRewardPerTokenPaidError, backerTotalAllocationError],
   )
 
   return {
