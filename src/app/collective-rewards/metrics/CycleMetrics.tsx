dae6b11d4ea0e93991f1f2118521f3cc25ff113d
franciscotobar <100875069+franciscotobar@users.noreply.github.com>
2024-12-05 10:51:08 -0600

    TOK-522: cycle metrics (#441)
    * fix: cycle metrics

--- src/app/collective-rewards/metrics/CycleMetrics.tsx
+++ src/app/collective-rewards/metrics/CycleMetrics.tsx
@@ -1,45 +1,53 @@
-import { useCycleContext } from '@/app/collective-rewards/metrics'
+import { useCycleContext, useGetTimeUntilNextCycle } from '@/app/collective-rewards/metrics'
 import { Duration, DateTime } from 'luxon'
 import { useEffect, useState } from 'react'
 import { MetricsCard, MetricsCardTitle, TokenMetricsCardRow } from '@/app/collective-rewards/rewards'
 import { withSpinner } from '@/components/LoadingSpinner/withLoadingSpinner'
 import { useHandleErrors } from '@/app/collective-rewards/utils'
-
-let timeout: NodeJS.Timeout
+import { AVERAGE_BLOCKTIME } from '@/lib/constants'
 
 export const CycleMetrics = () => {
   const [timeRemaining, setTimeRemaining] = useState<Duration>(Duration.fromObject({ minutes: 0 }))
-  let {
-    data: { cycleDuration, cycleNext },
-    isLoading,
-    error,
-  } = useCycleContext()
+  const [timestamp, setTimestamp] = useState(BigInt(DateTime.now().toUnixInteger()))
 
+  const {
+    data: { cycleDuration, cycleNext },
+    isLoading: cycleLoading,
+    error: cycleError,
+  } = useCycleContext()
+  const {
+    data: timeUntilNextCycle,
+    isLoading: timeUntilNextCycleLoading,
+    error: timeUntilNextCycleError,
+  } = useGetTimeUntilNextCycle(timestamp)
+
+  const error = cycleError ?? timeUntilNextCycleError
   useHandleErrors({ error, title: 'Error loading cycle metrics' })
 
   const duration =
     cycleDuration.as('days') < 1 ? cycleDuration.shiftTo('hours') : cycleDuration.shiftTo('days')
 
   useEffect(() => {
-    if (timeRemaining.as('minutes') > 0) {
-      timeout = setTimeout(() => {
-        setTimeRemaining(state => state.minus({ minutes: 1 }))
-      }, 60000) // every minute
-    }
+    const interval = setInterval(() => {
+      setTimestamp(BigInt(DateTime.now().toUnixInteger()))
+    }, AVERAGE_BLOCKTIME)
 
-    return () => clearTimeout(timeout)
-  }, [timeRemaining])
+    return () => clearInterval(interval) // Cleanup interval on component unmount
+  }, [])
 
   useEffect(() => {
-    const now = DateTime.now()
-    let diff = cycleNext.diff(now, ['days']).mapUnits(unit => Math.floor(unit))
+    const nextCycle = Duration.fromObject({ seconds: Number(timeUntilNextCycle) })
 
-    if (diff.as('days') < 1) {
-      diff = cycleNext.diff(now, ['hours', 'minutes']).mapUnits(unit => Math.floor(unit))
+    if (nextCycle.as('minutes') <= 60) {
+      setTimeRemaining(nextCycle.shiftTo('minutes').mapUnits(Math.floor))
+    } else if (nextCycle.as('hours') <= 24) {
+      setTimeRemaining(nextCycle.shiftTo('hours', 'minutes').mapUnits(Math.floor))
+    } else {
+      setTimeRemaining(nextCycle.shiftTo('days', 'hours').mapUnits(Math.floor))
     }
+  }, [timeUntilNextCycle])
 
-    setTimeRemaining(diff)
-  }, [cycleNext])
+  const isLoading = cycleLoading || timeUntilNextCycleLoading
 
   return (
     <MetricsCard borderless>
