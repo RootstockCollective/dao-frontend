cf207c6d5ede13aab343ef56fbe0211d2c726b0b
Antonio <antonio@iovlabs.org>
2024-12-10 13:26:29 +0100

    fix: cr whitepaper link

--- src/app/collective-rewards/rewards/MyRewards.tsx
+++ src/app/collective-rewards/rewards/MyRewards.tsx
@@ -21,9 +21,10 @@ const SubText = () => {
       <Link
         className="text-[#E56B1A]"
         href={
-          'https://wiki.rootstockcollective.xyz/2c6e3b87b49f4c1e9225b713e1b49538?v=819168fca4964319896c19e8299a8ea0'
+          'https://file.notion.so/f/f/5c274ed5-ccaa-43f3-9086-7ff7fed02299/2d5ab5ff-a08b-428f-b8ef-1fb642dcaf42/20241128_RootstockCollective_Rewards_Whitepaper_V1_0.pdf?table=block&id=14c1ca6b-0b02-8093-beba-c642396e4e09&spaceId=5c274ed5-ccaa-43f3-9086-7ff7fed02299&expirationTimestamp=1733918400000&signature=0fXpO_3zOse-KRipYdYjHuOFRMqBcbKy5gHR2T5yyZQ&downloadName=20241128_RootstockCollective_Rewards_Whitepaper_V1_0.pdf'
         }
         target="_blank"
+        rel="noopener noreferrer"
       >
         Whitepaper
       </Link>{' '}
