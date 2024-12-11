733aa2a392d45832dab534214bc60cc6a81e7928
Antonio <antonio@iovlabs.org>
2024-12-11 11:02:12 +0100

    feat: include text under rewards leaderboard

--- src/app/collective-rewards/rewards/MyRewards.tsx
+++ src/app/collective-rewards/rewards/MyRewards.tsx
@@ -15,22 +15,13 @@ import { useRouter } from 'next/navigation'
 import { Link } from '@/components/Link'
 import { Builder } from '../types'
 import { useCanManageAllocations } from '@/app/collective-rewards/allocations/hooks'
+import { CRWhitepaperLink } from '../shared'
 
 const SubText = () => {
   return (
     <>
       Track and claim the rewards you earn from Collective Rewards. For more information check the{' '}
-      <Link
-        className="text-[#E56B1A]"
-        href={
-          'https://file.notion.so/f/f/5c274ed5-ccaa-43f3-9086-7ff7fed02299/2d5ab5ff-a08b-428f-b8ef-1fb642dcaf42/20241128_RootstockCollective_Rewards_Whitepaper_V1_0.pdf?table=block&id=14c1ca6b-0b02-8093-beba-c642396e4e09&spaceId=5c274ed5-ccaa-43f3-9086-7ff7fed02299&expirationTimestamp=1733918400000&signature=0fXpO_3zOse-KRipYdYjHuOFRMqBcbKy5gHR2T5yyZQ&downloadName=20241128_RootstockCollective_Rewards_Whitepaper_V1_0.pdf'
-        }
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Whitepaper
-      </Link>{' '}
-      .
+      <CRWhitepaperLink />.
     </>
   )
 }
