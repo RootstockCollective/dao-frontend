137f67a30ff1a534b34c2263fada2366a632b791
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2025-01-09 11:56:24 -0300

    add locators for connect wallet modal (#514)

--- src/components/Header/ConnectButton.tsx
+++ src/components/Header/ConnectButton.tsx
@@ -24,7 +24,7 @@ export const ConnectButton: FC<Props> = ({ onSuccess, children = 'Connect wallet
   }
 
   return (
-    <Button onClick={handleConnectWallet} variant={variant}>
+    <Button onClick={handleConnectWallet} variant={variant} data-testid="ConnectWallet">
       {children}
     </Button>
   )
