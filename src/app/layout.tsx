eeb1ad776d3d7a7b035f8c058516be621652606a
LordCheta <chinaechetam.maduabuchi@iovlabs.org>
2025-01-10 07:27:37 +0100

    feat: add google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
 import './globals.css'
 import { ContextProviders } from './providers'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const openSans = Open_Sans({
   variable: '--font-open-sans',
@@ -20,7 +21,18 @@ interface Props {
 export default function RootLayout({ children }: Readonly<Props>) {
   return (
     <html lang="en" data-theme="default">
+      <GoogleTagManager gtmId="GTM-PTL6VZMT" />
       <body className={`${openSans.variable} font-sans`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PTL6VZMT"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <ContextProviders>{children}</ContextProviders>
       </body>
     </html>
