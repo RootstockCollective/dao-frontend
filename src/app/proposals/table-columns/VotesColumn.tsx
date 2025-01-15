a13bbdb7a1604b52c1019ee0aa85c963991ee982
Francis Rodriguez <francisngg@gmail.com>
2025-01-15 16:17:18 -0400

    DAO-961 Changed Number for Big.js in Latest Proposals Table

--- src/app/proposals/table-columns/VotesColumn.tsx
+++ src/app/proposals/table-columns/VotesColumn.tsx
@@ -1,37 +1,40 @@
 import { formatNumberWithCommas } from '@/lib/utils'
+import Big from '@/lib/big'
 
 interface VotesColumnProps {
-  forVotes: number
-  abstainVotes: number
-  quorumAtSnapshot: number
+  forVotes: Big
+  abstainVotes: Big
+  quorumAtSnapshot: Big
 }
 
 export const VotesColumn = ({ forVotes, abstainVotes, quorumAtSnapshot }: VotesColumnProps) => {
   // Calculate the total votes considered for the quorum
-  const quorumVotes = forVotes + abstainVotes
+  const quorumVotes = forVotes.add(abstainVotes)
 
   // Calculate the percentage relative to the quorumAtSnapshot
   // If quorumAtSnapshot is 0, percentage defaults to 0
-  const percentage = quorumAtSnapshot === 0 ? 0 : Math.round((quorumVotes / quorumAtSnapshot) * 100)
+  const percentage = quorumAtSnapshot.eq(0)
+    ? Big(0)
+    : quorumVotes.div(quorumAtSnapshot).mul(100).round(undefined, Big.roundHalfEven)
 
   // Determine the color class based on the percentage
-  const colorClass =
-    percentage >= 100
-      ? 'text-st-success' // Green for 100% or more
-      : percentage >= 50
-        ? 'text-st-info' // Orange for 50% to 99%
-        : 'text-st-error' // Red for less than 50%
+  const colorClass = percentage.gte(100)
+    ? 'text-st-success' // Green for 100% or more
+    : percentage.gte(50)
+      ? 'text-st-info' // Orange for 50% to 99%
+      : 'text-st-error' // Red for less than 50%
 
   // Prepare values to display
-  const quorumToShow = quorumAtSnapshot === 0 ? '-' : quorumAtSnapshot // Show '-' if quorum is 0
-  const percentageToShow = quorumAtSnapshot === 0 ? '-' : percentage // Show '-' if percentage can't be calculated
+  const quorumToShow = quorumAtSnapshot.eq(0) ? '-' : quorumAtSnapshot // Show '-' if quorum is 0
+  const percentageToShow = quorumAtSnapshot.eq(0) ? '-' : percentage // Show '-' if percentage can't be calculated
 
   return (
     <>
       <p className={colorClass}>
-        {formatNumberWithCommas(Math.floor(quorumVotes))} ({formatNumberWithCommas(percentageToShow)}%)
+        {formatNumberWithCommas(quorumVotes.round().toString())} (
+        {formatNumberWithCommas(percentageToShow.toString())}%)
       </p>
-      <p>Quorum: {formatNumberWithCommas(quorumToShow)}</p>
+      <p>Quorum: {formatNumberWithCommas(quorumToShow.toString())}</p>
     </>
   )
 }
