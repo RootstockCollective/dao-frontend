63fe2682c6d9c11de084422ffddc17d83a789254
Cybervoid <shenshin@me.com>
2025-01-18 02:27:30 +0100

    feat(cards page): first step of "choose your proposal" page design implementation

--- src/app/proposals/choose/page.tsx
+++ src/app/proposals/choose/page.tsx
@@ -1,16 +1,30 @@
-import { HeaderTitle, Header, Headline } from '@/components/Typography'
+'use client'
+
+import { useState } from 'react'
+import { HeaderTitle } from '@/components/Typography'
 import { BackButton } from '@/components/BackButton'
 import { Card } from './Card'
 import { cards } from './cards'
 
 export default function ChooseProposal() {
+  const [findMore, setFindMore] = useState<number>()
+
+  const handleFindMore = (id: number) => {
+    const selectedCard = cards.find(card => card.id === id)
+    setFindMore(id)
+    console.log('Find out more', selectedCard?.title)
+  }
+  const handleChoose = (id: number) => {
+    const selectedCard = cards.find(card => card.id === id)
+    console.log('card chosen', selectedCard?.title)
+  }
   return (
     <div className="xl:pl-3">
       <BackButton className="mb-4" />
       <HeaderTitle className="mb-24 text-5xl text-center">Choose Your Proposal</HeaderTitle>
-      <div className="flex flex-col xl:flex-row gap-12 justify-center items-center">
+      <div className="flex flex-row gap-6 xl:gap-[clamp(10px,2vw,46px)] justify-center items-center">
         {cards.map(card => (
-          <Card key={card.id} card={card} />
+          <Card key={card.id} card={card} handleChoose={handleChoose} handleFindMore={handleFindMore} />
         ))}
       </div>
     </div>
