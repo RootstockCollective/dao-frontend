9fc9032d3df6dfd1c986d3769244eb84ff3ca2c7
Cybervoid <shenshin@me.com>
2025-01-21 04:16:23 +0100

    feat(proposals): add proposal selection cards fade in-out animation

--- src/app/proposals/choose/page.tsx
+++ src/app/proposals/choose/page.tsx
@@ -1,32 +1,53 @@
 'use client'
 
-import { useState } from 'react'
+import { LazyExoticComponent, ReactElement, useState, Suspense, lazy } from 'react'
 import { HeaderTitle } from '@/components/Typography'
 import { BackButton } from '@/components/BackButton'
 import { Card } from './Card'
-import { cards } from './cards'
+import { cards, type ProposalType } from './cards'
+import { useRouter } from 'next/navigation'
 
 export default function ChooseProposal() {
-  const [findMore, setFindMore] = useState<number>()
+  const router = useRouter()
+  const [highlightIndex, setHighlightIndex] = useState<number | null>(null)
+  const [ActiveInfoPanel, setActiveInfoPanel] = useState<LazyExoticComponent<() => ReactElement> | null>(null)
 
-  const handleFindMore = (id: number) => {
-    const selectedCard = cards.find(card => card.id === id)
-    setFindMore(id)
-    console.log('Find out more', selectedCard?.title)
+  const navigateToCreateProposal = (index: number) => {
+    const { contract, action } = cards[index]
+    router.push(
+      `/proposals/create?${new URLSearchParams({
+        contract,
+        action,
+      })}`,
+    )
   }
-  const handleChoose = (id: number) => {
-    const selectedCard = cards.find(card => card.id === id)
-    console.log('card chosen', selectedCard?.title)
+
+  const showInfoPanel = (proposalType: ProposalType) => {
+    setActiveInfoPanel(lazy(() => import(`./info-panel/${proposalType}`)))
   }
+
   return (
     <div className="xl:pl-3">
       <BackButton className="mb-4" />
       <HeaderTitle className="mb-24 text-5xl text-center">Choose Your Proposal</HeaderTitle>
-      <div className="flex flex-row gap-6 xl:gap-[clamp(10px,2vw,46px)] justify-center items-center">
-        {cards.map(card => (
-          <Card key={card.id} card={card} handleChoose={handleChoose} handleFindMore={handleFindMore} />
+      <div className="mb-8 flex flex-row gap-6 xl:gap-[clamp(10px,2vw,46px)] justify-center items-center">
+        {cards.map((card, index) => (
+          <Card
+            key={card.id}
+            card={card}
+            isHighlighted={highlightIndex === null || highlightIndex === index}
+            onMouseEnter={() => setHighlightIndex(index)}
+            onMouseLeave={() => setHighlightIndex(null)}
+            navigateToCreateProposal={() => navigateToCreateProposal(index)}
+            showInfoPanel={() => showInfoPanel(card.proposalType)}
+          />
         ))}
       </div>
+      {ActiveInfoPanel && (
+        <Suspense fallback={<p>Loading...</p>}>
+          <ActiveInfoPanel />
+        </Suspense>
+      )}
     </div>
   )
 }
