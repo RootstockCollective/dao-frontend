6c375f34beb95e826cffdee9704084a5b4935cc0
Cybervoid <shenshin@me.com>
2025-01-25 02:16:24 +0100

    feat(choose proposal): enhance card and info panel structure with descriptions and improved layout

--- src/app/proposals/choose/components/example-card/example-card.tsx
+++ src/app/proposals/choose/components/example-card/example-card.tsx
@@ -1,5 +1,5 @@
 import { cn } from '@/lib/utils'
-import { cloneElement, HTMLAttributes, ReactElement } from 'react'
+import { cloneElement, HTMLAttributes, PropsWithChildren, ReactElement } from 'react'
 import type { ProposalType } from '../../types'
 import { exampleCardData } from './data'
 import { Header, Typography } from '@/components/Typography'
@@ -8,27 +8,50 @@ interface ProposalExampleProps extends HTMLAttributes<HTMLDivElement> {
   proposal: ProposalType
 }
 
+const Cell = ({ children }: PropsWithChildren) => (
+  <div className="h-[31px] px-4 flex justify-between items-center flex-row border-[1px] border-[#2D2D2D] rounded-lg">
+    {children}
+  </div>
+)
+
 export function ExampleCard({ proposal, className, style, ...props }: ProposalExampleProps) {
-  const { proposalId, proposer, text, title } = exampleCardData[proposal]
+  const { proposalId, proposer, text, title, description } = exampleCardData[proposal]
   return (
-    <div
-      {...props}
-      className={cn(className, 'p-4 w-[326px] rounded-lg')}
-      style={{ ...style, border: '0.2px solid rgba(255,255,255,0.3)' }}
-    >
-      <Header className="mb-4 text-base font-normal leading-tight text-center" fontFamily="kk-topo">
-        {title}
-      </Header>
-      <div className="mb-3 flex flex-row justify-around nowrap text-[10px] font-[700] leading-tight">
-        <Typography className="text-[rgba(255,255,255,0.6)]">Proposed by</Typography>
-        <Typography className="text-primary">{proposer}</Typography>
-        <Typography className="text-[rgba(255,255,255,0.6)]">Proposal ID</Typography>
-        <Typography className="text-primary">ID {proposalId}</Typography>
+    <div {...props} className={cn(className, 'flex flex-col items-center gap-3')}>
+      <div className="p-4 w-[326px] min-h-[491px] flex flex-col border-[0.2px] gap-4 border-[rgba(255,255,255,0.3)] rounded-lg">
+        <div className="flex-grow">
+          <Header className="mb-4 text-base font-normal leading-tight text-center" fontFamily="kk-topo">
+            {title}
+          </Header>
+          <div className="mb-3 flex flex-row justify-around nowrap text-[10px] font-[700] leading-tight">
+            <Typography className="text-[rgba(255,255,255,0.6)]">Proposed by</Typography>
+            <Typography className="text-primary">{proposer}</Typography>
+            <Typography className="text-[rgba(255,255,255,0.6)]">Proposal ID</Typography>
+            <Typography className="text-primary">ID {proposalId}</Typography>
+          </div>
+          {cloneElement<HTMLAttributes<HTMLDivElement>>(text, {
+            className: cn('text-[10px] leading-[15px] space-y-4'),
+          })}
+        </div>
+        <div className="flex flex-col gap-2">
+          <Header className="text-base font-normal leading-tight " fontFamily="kk-topo">
+            VOTES
+          </Header>
+          <Cell>
+            <Typography className="text-[12px] text-[#1BC47D]">123K</Typography>
+            <Typography className="text-[12px] text-[#1BC47D]">For</Typography>
+          </Cell>
+          <Cell>
+            <Typography className="text-[12px] text-[#F24822]">12</Typography>
+            <Typography className="text-[12px] text-[#F24822]">Against</Typography>
+          </Cell>
+          <Cell>
+            <Typography className="text-[12px] text-[rgba(255,255,255,0.6)]">0</Typography>
+            <Typography className="text-[12px] text-[rgba(255,255,255,0.6)]">Abstain</Typography>
+          </Cell>
+        </div>
       </div>
-      {/* <div className="text-primary space-y-4">{example.text}</div> */}
-      {cloneElement<HTMLAttributes<HTMLDivElement>>(text, {
-        className: cn('text-[10px] leading-[15px] space-y-4'),
-      })}
+      <Typography className="text-[10px] leading-tight">{description}</Typography>
     </div>
   )
 }
--- src/app/proposals/choose/components/info-panel/info-panel.tsx
+++ src/app/proposals/choose/components/info-panel/info-panel.tsx
@@ -17,33 +17,38 @@ export function InfoPanel({ proposal, className }: InfoPanelProps) {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 1 }}
-      className={cn(className, 'p-7 w-full bg-[#1A1A1A] overflow-hidden')}
+      className={cn(className, 'py-7 px-8 w-full bg-[#1A1A1A] overflow-hidden')}
     >
-      {/* <div className="ml-4 max-w-[308px] min-w-[100px] text-center float-right">
-        <Image placeholder="blur" src={exampleImage} alt="Example image" className="mb-2" />
-        <Typography className="text-[10px]">Example of a grant proposal</Typography>
-      </div> */}
-      <ExampleCard proposal={proposal} className="ml-4 float-right" />
-      <HeaderTitle className="text-[32px] uppercase leading-tight">{proposal} Must have</HeaderTitle>
-      <Typography className="mb-8 text-lg leading-tight">
-        A list of items you should have done to be able to submit a standard proposal
-      </Typography>
-      <ol className="p-0 space-y-6 list-none clearfix">
-        {infoPanelData[proposal].map(({ id, text, icon, linkText, linkUrl }, i) => (
-          <li key={id} className="">
-            {cloneElement<HTMLAttributes<HTMLDivElement>>(icon, { className: 'inline-block mr-2' })}
-            <Typography className="inline">
-              {i + 1}. {text}
-            </Typography>
-            <a className="mt-2 block" target="_blank" rel="noopener noreferrer" href={linkUrl}>
-              <Typography className="inline  text-lg font-[700] leading-tight text-primary">
-                {linkText}
-              </Typography>
-              <LinkArrow className="h-[22px] ml-1 inline" />
-            </a>
-          </li>
-        ))}
-      </ol>
+      <div className="flex flex-col lg:flex-row gap-4">
+        <div>
+          <HeaderTitle className="text-[32px] uppercase leading-tight">{proposal} Must have</HeaderTitle>
+          <Typography className="mb-4 lg:mb-8 text-lg leading-tight">
+            List of items you should have completed to submit {proposal.toLowerCase()} proposal
+          </Typography>
+          <ol className="p-0 space-y-6 list-none">
+            {infoPanelData[proposal].map(({ id, text, icon, linkText, linkUrl }, i) => (
+              <li key={id}>
+                <div className="flex flex-row gap-2">
+                  {/* Icon */}
+                  <div className="pt-1">{icon}</div>
+                  <Typography>{i + 1}.</Typography>
+                  {/* Description paragraph */}
+                  <Typography className="leading-tight">{text}</Typography>
+                </div>
+
+                {/* Orange link */}
+                <a className="mt-2 block" target="_blank" rel="noopener noreferrer" href={linkUrl}>
+                  <Typography className="inline text-lg font-[500] leading-tight text-primary">
+                    {linkText}
+                  </Typography>
+                  <LinkArrow className="h-[22px] ml-1 inline" />
+                </a>
+              </li>
+            ))}
+          </ol>
+        </div>
+        <ExampleCard className="mt-4 lg:mt-0" proposal={proposal} />
+      </div>
     </motion.div>
   )
 }
