70847aa603b24f96dd1f4dfa29f68ed89e2ec520
Alexander Evchenko <alexivchenko9@gmail.com>
2025-02-03 18:25:19 +0400

    refactor: text in Sidebar to be constants

---
+++ src/shared/contants/text/index.ts
@@ -0,0 +1,7 @@
+export const sidebar = {
+  my_collective: 'My Collective',
+  treasury: 'Treasury',
+  proposals: 'Proposals',
+  communities: 'Communities',
+  collective_rewards: 'Collective Rewards',
+}
