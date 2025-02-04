6e42aa0b25afa20b48439312f9020c2734a25b1b
Juraj Piar <juraj@iovlabs.org>
2025-02-04 12:00:09 +0000

    chore(cr): change backer est. reward tooltip text

--- src/app/collective-rewards/rewards/backers/BackerRewardsTable.tsx
+++ src/app/collective-rewards/rewards/backers/BackerRewardsTable.tsx
@@ -20,7 +20,7 @@ import { useBasicPaginationUi } from '@/shared/hooks/usePaginationUi'
 import { CycleContextProvider } from '@/app/collective-rewards/metrics'
 import Link from 'next/link'
 import Big from '@/lib/big'
-import { EstimatedRewardsTooltip } from './components/EstimatedRewardsTooltip'
+import { Typography } from '@/components/Typography/Typography'
 
 enum RewardsColumnKeyEnum {
   builder = 'builder',
@@ -39,7 +39,22 @@ const tableHeaders: TableHeader[] = [
     className: 'w-[20%]',
     sortKey: RewardsColumnKeyEnum.estimatedRewards,
     tooltip: {
-      text: <EstimatedRewardsTooltip />,
+      text: (
+        <>
+          <Typography>
+            An estimate of the remainder of this Cycle’s rewards from each Builder that will become fully
+            claimable by the end of the current Cycle. These rewards gradually transition into your ‘Claimable
+            Rewards’ as the cycle progresses.
+          </Typography>
+          <Typography marginTop="1rem" marginBottom="1rem">
+            To check the cycle`s completion, go to Collective Rewards → Current Cycle.
+          </Typography>
+          <Typography>
+            The displayed information is dynamic and may vary based on total rewards and user activity. This
+            data is for informational purposes only.
+          </Typography>
+        </>
+      ),
       popoverProps: { size: 'medium' },
     },
   },
--- src/app/collective-rewards/rewards/backers/Rewards.tsx
+++ src/app/collective-rewards/rewards/backers/Rewards.tsx
@@ -13,7 +13,7 @@ import { CycleContextProvider } from '@/app/collective-rewards/metrics'
 import { PricesContextProvider } from '@/shared/context/PricesContext'
 import { Button } from '@/components/Button'
 import { MetricContainer } from '@/app/collective-rewards/rewards/components/MetricContainer'
-import { EstimatedRewardsTooltip } from './components/EstimatedRewardsTooltip'
+import { Typography } from '@/components/Typography/Typography'
 
 type RewardsProps = RewardDetails
 
@@ -39,7 +39,22 @@ const RewardsContent: FC<RewardsProps> = ({ builder, gauges, tokens }) => {
           tokens={tokens}
           rewards={['estimated']}
           tooltip={{
-            text: <EstimatedRewardsTooltip />,
+            text: (
+              <>
+                <Typography>
+                  An estimate of the remainder of this Cycle’s rewards that will become fully claimable by the
+                  end of the current Cycle. These rewards gradually transition into your ‘Claimable Rewards’
+                  as the cycle progresses.
+                </Typography>
+                <Typography marginTop="1rem" marginBottom="1rem">
+                  To check the cycle`s completion, go to Collective Rewards → Current Cycle.
+                </Typography>
+                <Typography>
+                  The displayed information is dynamic and may vary based on total rewards and user activity.
+                  This data is for informational purposes only.
+                </Typography>
+              </>
+            ),
             popoverProps: { size: 'medium' },
           }}
         />
