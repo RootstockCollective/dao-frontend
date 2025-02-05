2df331ae99381dbb28fefbbb6f5900c4ef10a003
Alexander Evchenko <alexivchenko9@gmail.com>
2025-02-05 17:02:57 +0400

    feat: allocations link

--- src/components/LeftSidebar/links.tsx
+++ src/components/LeftSidebar/links.tsx
@@ -16,6 +16,7 @@ const testnet = {
   rbtc: 'https://rootstock.io/rbtc/#get-rbtc/',
   getRif: 'https://wiki.rootstockcollective.xyz/Token-Resources-e3f89008a96e4dcab3037ff7861d9d8a',
   forum: 'https://gov.rootstockcollective.xyz',
+  allocations: 'https://rootstockcollective.xyz/collective-rewards-how-to-become-a-backer/',
 }
 
 const mainnet = {
@@ -25,6 +26,7 @@ const mainnet = {
   rbtc: 'https://rootstock.io/rbtc/#get-rbtc/',
   getRif: 'https://wiki.rootstockcollective.xyz/Token-Resources-e3f89008a96e4dcab3037ff7861d9d8a',
   forum: 'https://gov.rootstockcollective.xyz',
+  allocations: 'https://rootstockcollective.xyz/collective-rewards-how-to-become-a-backer/',
 }
 
 const environments = {
