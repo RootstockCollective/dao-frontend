4a9a71a1362210198371f8cf49c8b3f7ffc7a436
Aleksandr Shenshin <shenshin@me.com>
2025-02-07 18:01:36 +0100

    fix: remove deprecated IPFS gateway URL (#655)

--- src/config/ipfsGateways.ts
+++ src/config/ipfsGateways.ts
@@ -2,5 +2,4 @@ export const ipfsGateways = [
   'https://red-legislative-meadowlark-461.mypinata.cloud/ipfs/',
   'https://ipfs.io/ipfs/',
   'https://gateway.pinata.cloud/ipfs/',
-  'https://dweb.link/ipfs/',
 ]
