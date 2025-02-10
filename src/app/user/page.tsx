f39e58b8787dd97d819ce0623fec4a28083b891a
Alexander Evchenko <alexivchenko9@gmail.com>
2025-02-10 13:52:49 +0400

    feat: Signposting on My Collective Dashboard

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -6,28 +6,26 @@ import { DelegationSection } from '@/app/user/Delegation'
 import { MainContainer } from '@/components/MainContainer/MainContainer'
 import { Tabs, TabsContent, TabsList, TabsTrigger, TabTitle } from '@/components/Tabs'
 import { TxStatusMessage } from '@/components/TxStatusMessage'
-import { useSearchParams } from 'next/navigation'
-import { Suspense } from 'react'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { Suspense, useCallback, useEffect, useMemo, useState } from 'react'
+import { useCookies } from 'next-client-cookies'
 import { zeroAddress } from 'viem'
 import { useAccount } from 'wagmi'
 import { useIsBuilderOrBacker } from '../collective-rewards/rewards/hooks/useIsBuilderOrBacker'
 import { useHandleErrors } from '../collective-rewards/utils'
 import { BecomeABuilderButton } from '../collective-rewards/user'
+import { Dropdown, DropdownItem, DropdownTopic, getStartedData } from '@/components/dropdown'
+import { dropdown } from '@/shared/contants'
+import { HeaderTitle, Typography } from '@/components/Typography'
+import { BalancesProvider, useBalancesContext } from './Balances/context/BalancesContext'
+import { TokenBalanceRecord } from './types'
+import { RBTC, RIF, stRIF } from '@/lib/constants'
+import Big from 'big.js'
 
-interface MyHoldingsProps {
-  showBuilderButton?: boolean
-}
-
-const MyHoldings = ({ showBuilderButton = false }: MyHoldingsProps) => (
-  <>
-    <TxStatusMessage messageType="staking" />
-    <BalancesSection showBuilderButton={showBuilderButton} />
-    <DelegationSection />
-    <CommunitiesSection />
-  </>
-)
-
+const getStartedSkipped = 'getStartedSkipped'
 const values = ['holdings', 'rewards'] as const
+const COMPLETED = 'COMPLETED'
+
 type TabValue = (typeof values)[number]
 type Tabs = {
   [key in TabValue]: {
@@ -46,7 +44,48 @@ const tabs: Tabs = {
   },
 }
 
+
+const checkSteps = (items: DropdownItem[],  balances: TokenBalanceRecord) => {
+  if (Big(balances[stRIF].balance).gt(0)) {
+    return [
+      {
+        items: items.slice(3),
+      },
+      {
+        topic: COMPLETED,
+        items: items.slice(0, 3),
+      },
+    ]
+  }
+
+  if (Big(balances[RIF].balance).gt(0)) {
+    return [
+      { items: items.slice(2) },
+      { topic: COMPLETED, items: items.slice(0, 2) },
+    ]
+  }
+
+  if (Big(balances[RBTC].balance).gt(0)) {
+    return [
+      { items: items.slice(1) },
+      {  topic: COMPLETED, items: items.filter(step => step.id === RBTC) },
+    ]
+  }
+
+  return [{ items }]
+}
+
+
+
 function User() {
+  const { balances } = useBalancesContext()
+
+  // Getting Started dropdown
+  const router = useRouter()
+  const coockies = useCookies()
+  const isGetStatedSkipped = useMemo(() => Boolean(coockies.get(getStartedSkipped)), [coockies])
+  const [dropdownData, setDropdownData] = useState<DropdownTopic[]>([])
+
   const { address } = useAccount()
   const searchParams = useSearchParams()
   const tabFromParams = searchParams?.get('tab') as TabValue
@@ -59,12 +98,24 @@ function User() {
     title: 'Error fetching user data',
   })
 
+  const skipGetStarted = useCallback(async () => {
+    coockies.set(getStartedSkipped, 'true')
+  }, [coockies])
+
+  useEffect(() => {
+    if (!isGetStatedSkipped) {
+      setDropdownData(checkSteps(getStartedData(router)[0].items, balances))
+    }
+  }, [balances, isGetStatedSkipped])
+
+  const showAdditionalContent = !isLoading && isBuilderOrBacker
+
   return (
     <MainContainer>
       {/* We don't show the tab if it's loading */}
-      {!isLoading && isBuilderOrBacker ? (
-        <Tabs defaultValue={defaultTabValue}>
-          <div className="row-container">
+      <Tabs defaultValue={defaultTabValue}>
+        <div className="row-container">
+          {showAdditionalContent ? (
             <TabsList>
               <TabsTrigger value={tabs.holdings.value}>
                 <TabTitle>{tabs.holdings.title}</TabTitle>
@@ -73,18 +124,43 @@ function User() {
                 <TabTitle>{tabs.rewards.title}</TabTitle>
               </TabsTrigger>
             </TabsList>
-            <BecomeABuilderButton address={address!} />
-          </div>
-          <TabsContent value={tabs.holdings.value}>
-            <MyHoldings />
-          </TabsContent>
+          ) : (
+            <HeaderTitle className="mb-6">Balances</HeaderTitle>
+          )}
+
+          {!isGetStatedSkipped ? (
+            <Dropdown
+              title={dropdown.steps}
+              subtitle={`(${dropdownData[1] ? dropdownData[1].items.length : '0'}/5 ${COMPLETED})`}
+              description={dropdown.influencerDesc}
+              data={dropdownData}
+              footer={
+                <Typography
+                  className="text-[12px] text-center leading-none text-[#171412] font-normal font-rootstock-sans hover:underline"
+                  cursor="pointer"
+                  onClick={skipGetStarted}
+                >
+                  {dropdown.already_familiar}
+                </Typography>
+              }
+            />
+          ) : null}
+
+          <BecomeABuilderButton address={address!} />
+        </div>
+
+        <TabsContent value={tabs.holdings.value}>
+          <TxStatusMessage messageType="staking" />
+          <BalancesSection showTitle={showAdditionalContent} />
+          <DelegationSection />
+          <CommunitiesSection />
+        </TabsContent>
+        {showAdditionalContent ? (
           <TabsContent value={tabs.rewards.value}>
             <Rewards builder={address!} />
           </TabsContent>
-        </Tabs>
-      ) : (
-        <MyHoldings showBuilderButton={true} />
-      )}
+        ) : null}
+      </Tabs>
     </MainContainer>
   )
 }
@@ -92,7 +168,9 @@ function User() {
 export default function UserPage() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <User />
+      <BalancesProvider>
+        <User />
+      </BalancesProvider>
     </Suspense>
   )
 }
