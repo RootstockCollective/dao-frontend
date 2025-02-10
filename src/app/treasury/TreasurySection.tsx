36b3b78850ec018072137645cfa22096e09ec263
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2025-02-10 11:56:56 -0300

    Fix UI amount on Treasures

--- src/app/treasury/TreasurySection.tsx
+++ src/app/treasury/TreasurySection.tsx
@@ -17,7 +17,7 @@ export const TreasurySection = () => {
             key={`${contract.name}-RIF`}
             title={`${contract.name} RIF`}
             // Show the RIF amount, rounded up to the nearest whole number. If not available, show 0 RIF.
-            amount={`${buckets[index]?.RIF?.amount ? formatNumberWithCommas(Big(buckets[index].RIF.amount).ceil().toString()) : 0} RIF`}
+            amount={`${buckets[index]?.RIF?.amount ? formatNumberWithCommas(Big(buckets[index].RIF.amount).ceil()) : 0} RIF`}
             // Display the fiat amount in USD, or show 0 if not available
             fiatAmount={`= USD ${buckets[index]?.RIF?.fiatAmount ? buckets[index].RIF.fiatAmount : 0}`}
             contractAddress={contract.address}
@@ -30,7 +30,7 @@ export const TreasurySection = () => {
           <MetricsCard
             key={`${contract.name}-RBTC`}
             title={`${contract.name} RBTC`}
-            amount={`${Big(buckets[index]?.RBTC?.amount).toFixedNoTrailing(8)}`}
+            amount={`${formatNumberWithCommas(Big(buckets[index]?.RBTC?.amount).toFixedNoTrailing(8))} RBTC`}
             fiatAmount={`= USD ${buckets[index]?.RBTC?.fiatAmount ? buckets[index].RBTC.fiatAmount : 0}`}
             data-testid={`${contract.name}-RBTC`}
             borderless
