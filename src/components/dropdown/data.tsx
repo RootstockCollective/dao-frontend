4439994f0ae94fcf2e2ab4e9803df80a9eb0227f
Alexander Evchenko <alexivchenko9@gmail.com>
2025-02-10 11:01:32 +0400

    refactor: Dropdown to allow for display of topic

--- src/components/dropdown/data.tsx
+++ src/components/dropdown/data.tsx
@@ -21,68 +21,81 @@ export interface DropdownItem {
   TitleIcon?: (props: TokenImageWithPreconfigutedSymbol) => JSX.Element
 }
 
+export interface DropdownTopic {
+  items: DropdownItem[]
+  topic?: string
+}
+
 const onExternal = (linkUrl: string) => window.open(linkUrl, '_blank', 'noopener,noreferrer')
 
-export const prepareProposalsData: DropdownItem[] = [
+export const prepareProposalsData: DropdownTopic[] = [
   {
-    id: '1',
-    Icon: BulbIcon,
-    title: 'Discuss on Discourse',
-    text: 'Clarify your project՚s purpose on Discourse',
-    onClick: () => onExternal('https://gov.rootstockcollective.xyz/'),
-  },
-  {
-    id: '2',
-    Icon: KycIcon,
-    title: 'Verify KYC',
-    text: 'Complete your KYC to ensure eligibility. (Apply for Grants)',
-    onClick: () =>
-      onExternal(
-        'https://docs.google.com/forms/d/e/1FAIpQLSd4HklyTFPFAo2I0l_N5fy_di01WZ27e4uFDG1KVy8ZIOSiow/viewform',
-      ),
+    items: [
+      {
+        id: '1',
+        Icon: BulbIcon,
+        title: 'Discuss on Discourse',
+        text: 'Clarify your project՚s purpose on Discourse',
+        onClick: () => onExternal('https://gov.rootstockcollective.xyz/'),
+      },
+      {
+        id: '2',
+        Icon: KycIcon,
+        title: 'Verify KYC',
+        text: 'Complete your KYC to ensure eligibility. (Apply for Grants)',
+        onClick: () =>
+          onExternal(
+            'https://docs.google.com/forms/d/e/1FAIpQLSd4HklyTFPFAo2I0l_N5fy_di01WZ27e4uFDG1KVy8ZIOSiow/viewform',
+          ),
+      },
+    ],
   },
 ]
 
-export const getStartedData = (router: AppRouterInstance): DropdownItem[] => [
+export const getStartedData = (router: AppRouterInstance): DropdownTopic[] => [
   {
-    id: RBTC,
-    Icon: props => <NumberIcon number="1" {...props} />,
-    title: 'GET RBTC',
-    text: 'Learn more about rBTC',
-    onClick: () => onExternal(currentLinks.rbtc),
-    TitleIcon: (props: TokenImageWithPreconfigutedSymbol) => (
-      <TokenImage symbol={RBTC} size={props.size} className={props.className} />
-    ),
-  },
-  {
-    id: RIF,
-    Icon: props => <NumberIcon number="2" {...props} />,
-    title: 'GET RIF',
-    text: 'Learn more about RIF',
-    onClick: () => onExternal(currentLinks.getRif),
-    TitleIcon: (props: TokenImageWithPreconfigutedSymbol) => (
-      <TokenImage symbol={RIF} size={props.size} className={props.className} />
-    ),
-  },
-  {
-    id: stRIF,
-    Icon: props => <NumberIcon number="3" {...props} />,
-    title: 'Stake RIF',
-    text: 'Learn more about staking',
-    onClick: () => onExternal(''),
-  },
-  {
-    id: 'VOTE',
-    Icon: props => <NumberIcon number="4" {...props} />,
-    title: 'VOTE ON YOUR FIRST PROPOSAL',
-    text: '',
-    onClick: () => router.push('/proposals'),
-  },
-  {
-    id: 'ALLOCATIONS',
-    Icon: props => <NumberIcon number="5" {...props} />,
-    title: 'MAKE YOUR FIRST ALLOCATIONS OF RIF',
-    text: 'Learn more about allocations',
-    onClick: () => onExternal(currentLinks.allocations),
+    items: [
+      {
+        id: RBTC,
+        Icon: props => <NumberIcon number="1" {...props} />,
+        title: 'GET RBTC',
+        text: 'Learn more about rBTC',
+        onClick: () => onExternal(currentLinks.rbtc),
+        TitleIcon: (props: TokenImageWithPreconfigutedSymbol) => (
+          <TokenImage symbol={RBTC} size={props.size} className={props.className} />
+        ),
+      },
+      {
+        id: RIF,
+        Icon: props => <NumberIcon number="2" {...props} />,
+        title: 'GET RIF',
+        text: 'Learn more about RIF',
+        onClick: () => onExternal(currentLinks.getRif),
+        TitleIcon: (props: TokenImageWithPreconfigutedSymbol) => (
+          <TokenImage symbol={RIF} size={props.size} className={props.className} />
+        ),
+      },
+      {
+        id: stRIF,
+        Icon: props => <NumberIcon number="3" {...props} />,
+        title: 'Stake RIF',
+        text: 'Learn more about staking',
+        onClick: () => onExternal(''),
+      },
+      {
+        id: 'VOTE',
+        Icon: props => <NumberIcon number="4" {...props} />,
+        title: 'VOTE ON YOUR FIRST PROPOSAL',
+        text: '',
+        onClick: () => router.push('/proposals'),
+      },
+      {
+        id: 'ALLOCATIONS',
+        Icon: props => <NumberIcon number="5" {...props} />,
+        title: 'MAKE YOUR FIRST ALLOCATIONS OF RIF',
+        text: 'Learn more about allocations',
+        onClick: () => onExternal(currentLinks.allocations),
+      },
+    ],
   },
 ]
