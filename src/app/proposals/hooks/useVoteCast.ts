9d75cd99eaa7ae7f199c4cd5bf34f3644ce6b552
Alexander Evchenko <alexivchenko9@gmail.com>
2025-02-11 10:10:49 +0400

    refator: query function

--- src/app/proposals/hooks/useVoteCast.ts
+++ src/app/proposals/hooks/useVoteCast.ts
@@ -4,21 +4,23 @@ import { Address, parseEventLogs } from 'viem'
 import { useBlockNumber } from 'wagmi'
 import { fetchVoteCastByAddress } from '../api/events'
 
+const parseVoteCastEvents = (address: Address, latestBlockNumber: bigint) => async () => {
+  const logs = await fetchVoteCastByAddress(address, latestBlockNumber)
+
+  const events = parseEventLogs({
+    abi: governor.abi,
+    logs,
+    eventName: 'VoteCast',
+  })
+
+  return events
+}
+
 export const useVoteCastEvent = (address: Address) => {
   const { data: latestBlockNumber } = useBlockNumber()
 
   return useQuery({
-    queryFn: async () => {
-      const logs = await fetchVoteCastByAddress(address, latestBlockNumber!)
-
-      const events = parseEventLogs({
-        abi: governor.abi,
-        logs,
-        eventName: 'VoteCast',
-      })
-
-      return events
-    },
+    queryFn: parseVoteCastEvents(address, latestBlockNumber!),
     queryKey: ['VoteCast'],
   })
 }
