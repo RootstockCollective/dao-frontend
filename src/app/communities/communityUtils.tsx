bd61776aeb8f8a855d052dbe106310b0ae09901b
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2025-02-11 14:28:57 -0300

    Change description of OG Contributors

--- src/app/communities/communityUtils.tsx
+++ src/app/communities/communityUtils.tsx
@@ -113,7 +113,7 @@ export const ogFoundersExternalContributors: CommunityItem = {
   title: 'OG Contributors',
   subtitle: 'DeFi',
   description:
-    'In the enchanting realm of Rootstock, a thrilling new chapter unfolds as this dedicated and specialised club in the community embark on an extraordinary journey making outstanding contributions to the Collective starting with the Shepherds. These visionaries, recognized for their remarkable contributions, are bestowed with the prestigious OG Contributors Badge - a symbol of honor that highlights their passion and engagement with Governance process and community needs.',
+    'In the enchanting realm of Rootstock, a thrilling new chapter unfolds as this dedicated and specialised club in the community embark on an extraordinary journey making outstanding contributions to the Collective starting with the Shepherds.',
   nftAddress: nftContracts.OG_CONTRIBUTORS,
   numberOfMembers: 0,
   cover: '/images/nfts/contributors-cover.png',
