99295b4b2db9a533a21dfe3d4f3ab0053c6a1b37
Juraj <juraj@iovlabs.org>
2025-02-13 15:00:07 +0000

    TOK-617: add nft boost status card (#678)
    * feat(cr_nft): add nft boost status card
    
    * fix: fix lint errors and add data-testid
    
    * fix: use justify end to show the loading button at the end of the row
    
    ---------
    
    Co-authored-by: Antonio <antonio@iovlabs.org>

--- src/app/collective-rewards/user/components/Button/WithBuilderButton.tsx
+++ src/app/collective-rewards/user/components/Button/WithBuilderButton.tsx
@@ -1,6 +1,7 @@
 import { ComponentType, FC } from 'react'
 import { BecomeABuilderButton } from './BecomeABuilderButton'
 import { useAccount } from 'wagmi'
+import { NFTBoosterCard } from '@/app/shared/components'
 
 export const withBuilderButton = <P extends {}>(Component: ComponentType<P>): FC<P> => {
   const WrappedComponent = ({ ...props }: P) => {
@@ -8,7 +9,11 @@ export const withBuilderButton = <P extends {}>(Component: ComponentType<P>): FC
     return (
       <div className="flex justify-between items-center self-stretch mb-6">
         <Component {...(props as P)} />
-        <BecomeABuilderButton address={address!} />
+        <div className="flex gap-3 justify-end">
+          {/* FIXME: get the nft booster context and check if there is an active campaign and the user owns the related NFT */}
+          {/* <NFTBoosterCard boostValue={20} nftThumbPath="" title="HI" /> */}
+          <BecomeABuilderButton address={address!} />
+        </div>
       </div>
     )
   }
