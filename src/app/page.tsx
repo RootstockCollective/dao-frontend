ede0a1ff6ba281d227c113c2c9f1b9b5d01f68ef
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2025-02-19 12:51:04 -0300

    fix Hydration error on root page (#711)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,22 @@
 import { useAccount } from 'wagmi'
 import { Login } from './login'
 import User from './user/page'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
   const { isConnected } = useAccount()
+  const [hasMounted, setHasMounted] = useState(false)
+
+  useEffect(() => {
+    // This is to prevent Hydration error on client side
+    // because useAccount hook is not available on server side
+    setHasMounted(true)
+  }, [])
+
+  if (!hasMounted) {
+    return null
+  }
+
   if (isConnected) {
     return <User />
   }
