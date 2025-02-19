577c57d8ff98b79878b2da56dab222b8dd03fa59
Juraj <juraj@iovlabs.org>
2025-02-19 13:50:15 +0000

    fix(header): unify header button size (#708)

--- src/app/proposals/components/HeaderSection.tsx
+++ src/app/proposals/components/HeaderSection.tsx
@@ -1,8 +1,8 @@
-import Link from 'next/link'
 import { Paragraph, HeaderTitle } from '@/components/Typography'
-import { Button } from '@/components/Button'
 import { Popover } from '@/components/Popover'
 import { Dropdown, prepareProposalsData } from '@/components/dropdown'
+import { useRouter } from 'next/navigation'
+import { HeaderButton } from '@/components/Button'
 
 export const HeaderSection = ({ createProposalDisabled = true, threshold = '' }) => (
   <div className="flex flex-row justify-between container">
@@ -15,31 +15,31 @@ export const HeaderSection = ({ createProposalDisabled = true, threshold = '' })
         }
         data={prepareProposalsData}
       />
-      {createProposalDisabled ? (
-        <Popover
-          content={
-            <Paragraph variant="normal" className="text-sm">
-              You need at least {threshold} Voting Power to create a proposal. The easiest way to get more
-              Voting Power is to Stake more RIF.
-            </Paragraph>
-          }
-          trigger="hover"
-          background="dark"
-          size="small"
-        >
-          <CreateProposalButton disabled />
-        </Popover>
-      ) : (
-        <CreateProposalButton />
-      )}
+
+      <Popover
+        content={
+          <Paragraph variant="normal" className="text-sm">
+            You need at least {threshold} Voting Power to create a proposal. The easiest way to get more
+            Voting Power is to Stake more RIF.
+          </Paragraph>
+        }
+        disabled={!createProposalDisabled}
+        trigger="hover"
+        background="dark"
+        size="small"
+      >
+        <CreateProposalButton disabled={createProposalDisabled} />
+      </Popover>
     </div>
   </div>
 )
 
-const CreateProposalButton = ({ disabled = false }) => (
-  <Link href="/proposals/choose">
-    <Button className="h-12 whitespace-nowrap" disabled={disabled} data-testid="CreateProposal">
+const CreateProposalButton = ({ disabled = false }) => {
+  const { push } = useRouter()
+
+  return (
+    <HeaderButton disabled={disabled} data-testid="CreateProposal" onClick={() => push('/proposals/choose')}>
       Create Proposal
-    </Button>
-  </Link>
-)
+    </HeaderButton>
+  )
+}
