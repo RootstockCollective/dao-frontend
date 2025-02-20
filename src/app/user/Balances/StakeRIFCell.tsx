b7ee59701a0d344fb4f9941a74550ff9de024472
Alexander Evchenko <alexivchenko9@gmail.com>
2025-02-20 12:30:33 +0400

    refactor: make Stake and UnStake RIFCell receive modal

--- src/app/user/Balances/StakeRIFCell.tsx
+++ src/app/user/Balances/StakeRIFCell.tsx
@@ -1,25 +1,17 @@
 import { useBalancesContext } from '@/app/user/Balances/context/BalancesContext'
 import { Button } from '@/components/Button'
-import { useModal } from '@/shared/hooks/useModal'
+import { ModalReturn } from '@/shared/hooks/useModal'
 import { useSearchParams } from 'next/navigation'
 import { useEffect, useState } from 'react'
 
-export const StakeRIFCell = () => {
-  const stakeModal = useModal()
-  const searchParams = useSearchParams()
-  const action = searchParams?.get('action')
-  const [openModal, setOpenModal] = useState(action === 'stake')
+interface Props {
+  stakeModal: ModalReturn
+}
 
+export const StakeRIFCell = ({ stakeModal }: Props) => {
   const { balances } = useBalancesContext()
   const { balance } = balances['RIF']
   const hasEnoughBalance = Number(balance) > 0
-
-  useEffect(() => {
-    if (openModal && hasEnoughBalance) {
-      stakeModal.openModal()
-      setOpenModal(false)
-    }
-  }, [openModal, hasEnoughBalance, stakeModal])
 
   return (
     <Button
