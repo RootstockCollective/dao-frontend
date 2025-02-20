11f82d0301d618ae8b740e7f7683fdd8628111df
Alexander Evchenko <alexivchenko9@gmail.com>
2025-02-20 12:30:33 +0400

    refactor: make Stake and UnStake RIFCell receive modal

--- src/app/user/Balances/UnStakeRIFCell.tsx
+++ src/app/user/Balances/UnStakeRIFCell.tsx
@@ -1,12 +1,16 @@
 import { useBalancesContext } from '@/app/user/Balances/context/BalancesContext'
 import { Button } from '@/components/Button'
-import { useModal } from '@/shared/hooks/useModal'
+import { ModalReturn } from '@/shared/hooks/useModal'
 
-export const UnStakeRIFCell = () => {
-  const unstakeModal = useModal()
+interface Props {
+  unstakeModal: ModalReturn
+}
+
+export const UnStakeRIFCell = ({ unstakeModal }: Props) => {
   const { balances } = useBalancesContext()
   const { balance } = balances['stRIF']
   const hasEnoughBalance = Number(balance) > 0
+
   return (
     <Button
       variant="outlined"
