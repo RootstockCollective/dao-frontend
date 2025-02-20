be8554e21f2427fb6d3fcc158dd86006e4897d53
DinizSa <diniz.araujosa@rootstocklabs.com>
2025-02-20 19:11:46 +0000

    chore: separate boost data branch by env

--- .github/scripts/nft_boost/consts.ts
+++ .github/scripts/nft_boost/consts.ts
@@ -1,9 +1,11 @@
 import { getAddress } from 'viem'
+import { ENV } from '@/lib/constants'
 
 // constant address. Calculation based on sc: address public constant COINBASE_ADDRESS = address(uint160(uint256(keccak256("COINBASE_ADDRESS"))));
 const rewardCoinbaseAddress = getAddress('0xf7aB6CfaebbADfe8B5494022c4C6dB776Bd63b6b')
 
-const boostDataFolder = 'boost-data/nft_boost_data'
+const boostDataBranch = ENV === 'mainnet' ? 'boost-data' : 'boost-data-dev'
+const boostDataFolder = `${boostDataBranch}/nft_boost_data`
 const nftActiveBoostPath = `${boostDataFolder}/latest`
 
 export { boostDataFolder, nftActiveBoostPath, rewardCoinbaseAddress }
