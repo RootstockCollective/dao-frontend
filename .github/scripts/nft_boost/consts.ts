a37f691f825a8969475fef4ff825bac6252fc728
DinizSa <diniz.araujosa@rootstocklabs.com>
2025-02-21 17:45:04 +0000

    chore: use unique branch per environment

--- .github/scripts/nft_boost/consts.ts
+++ .github/scripts/nft_boost/consts.ts
@@ -1,10 +1,10 @@
 import { getAddress } from 'viem'
-import { ENV } from '@/lib/constants'
+import { ENV } from '../../../src/lib/constants'
 
 // constant address. Calculation based on sc: address public constant COINBASE_ADDRESS = address(uint160(uint256(keccak256("COINBASE_ADDRESS"))));
 const rewardCoinbaseAddress = getAddress('0xf7aB6CfaebbADfe8B5494022c4C6dB776Bd63b6b')
 
-const boostDataBranch = ENV === 'mainnet' ? 'boost-data' : 'boost-data-dev'
+const boostDataBranch = "boost_data/" + ENV
 const boostDataFolder = `${boostDataBranch}/nft_boost_data`
 const nftActiveBoostPath = `${boostDataFolder}/latest`
 
