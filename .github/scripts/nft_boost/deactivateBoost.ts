355fd4dad59e54227d1e0bfa9ab196ce5842fe84
DinizSa <diniz.araujosa@rootstocklabs.com>
2025-02-23 00:55:53 +0000

    feat: deactivate boost

--- .github/scripts/nft_boost/deactivateBoost.ts
+++
@@ -1,9 +0,0 @@
-import * as fs from 'fs'
-import { nftActiveBoostPath } from './consts'
-
-async function main() {
-  fs.writeFileSync(nftActiveBoostPath, 'None')
-  console.log(`Deactivated nft boost by deleting the file content: ${nftActiveBoostPath}`)
-}
-
-main().catch(console.error)
