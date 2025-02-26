b24d05099cc20e1fa9feaaed77f48134c135b4d8
Juraj Piar <juraj@iovlabs.org>
2025-02-26 10:39:56 +0000

    refactor(boost_script): reduce est calculation

--- .github/scripts/nft_boost/activateBoost.ts
+++ .github/scripts/nft_boost/activateBoost.ts
@@ -1,7 +1,23 @@
 import * as fs from 'fs'
-import { getAddress, zeroAddress } from 'viem'
+import { Address, getAddress, zeroAddress } from 'viem'
 import { getActions } from './actions.utils'
 import { boostPercentage, nftContractAddress } from './process.utils'
+
+export type HolderData = {
+  estimatedRBTCRewards: bigint
+  estimatedRIFRewards: bigint
+  boostedRBTCRewards: bigint
+  boostedRIFRewards: bigint
+}
+
+export type Holders = Record<Address, HolderData>
+
+export type NftBoostData = {
+  nftContractAddress: Address
+  boostPercentage: number
+  calculationBlock: number
+  holders: Holders
+}
 
 const boostDataFolder = `nft_boost_data`
 const nftActiveBoostPath = `${boostDataFolder}/latest`
@@ -10,60 +26,61 @@ const nftActiveBoostPath = `${boostDataFolder}/latest`
   const { getLatestBlockNumber, getNftTransferEvents, getAllGauges, estimatedGaugeRewards } =
     await getActions()
 
-  const blockNumber = await getLatestBlockNumber()
-  console.info(`Latest block: ${blockNumber}`)
+  const calculationBlock = Number(await getLatestBlockNumber())
+  console.info(`Latest block: ${calculationBlock}`)
 
   const nftTransferEvents = await getNftTransferEvents(nftContractAddress)
-  const holdersData: any = {}
 
   const gauges = await getAllGauges()
 
   const normalisationFactor = 100_000 // Scaling factor to handle decimal values in BigInts. A ratio of 1.25 => 125_000
   const boost = BigInt(Math.round(normalisationFactor * (1 + boostPercentage / 100)))
 
+  let holders: Holders = {}
   for (let i = 0; i < nftTransferEvents.length; i++) {
     const event = nftTransferEvents[i]
     const holderAddress = getAddress(`0x${event.topics[2].slice(-40)}`)
     if (holderAddress === zeroAddress) continue
 
     console.info(`Processing ${i + 1} of ${nftTransferEvents.length} events. Nft holder: ${holderAddress}`)
-    const tokenId = BigInt(event.topics[3]).toString()
 
-    let backerEstimatedRBTCRewards = 0n
-    let backerEstimatedRIFRewards = 0n
-    let backerBoostedRBTCRewards = 0n
-    let backerBoostedRIFRewards = 0n
-    for (const gauge of gauges) {
-      const estimatedRewards = await estimatedGaugeRewards(holderAddress, gauge)
-
-      const boostedRBTCRewards = (estimatedRewards.RBTC * boost) / BigInt(normalisationFactor)
-      const boostedRIFRewards = (estimatedRewards.RIF * boost) / BigInt(normalisationFactor)
-
-      backerEstimatedRBTCRewards += estimatedRewards.RBTC
-      backerEstimatedRIFRewards += estimatedRewards.RIF
-      backerBoostedRBTCRewards += boostedRBTCRewards
-      backerBoostedRIFRewards += boostedRIFRewards
-    }
-    holdersData[holderAddress] = {
-      estimatedRBTCRewards: backerEstimatedRBTCRewards.toString(),
-      estimatedRIFRewards: backerEstimatedRIFRewards.toString(),
-      boostedRBTCRewards: backerBoostedRBTCRewards.toString(),
-      boostedRIFRewards: backerBoostedRIFRewards.toString(),
-      tokenId,
-    }
+    holders[holderAddress] = await gauges.reduce<Promise<HolderData>>(
+      async (acc, gauge) => {
+        const { boostedRBTCRewards, boostedRIFRewards, estimatedRBTCRewards, estimatedRIFRewards } = await acc
+        const { RBTC, RIF } = await estimatedGaugeRewards(holderAddress, gauge)
+        return {
+          estimatedRBTCRewards: estimatedRBTCRewards + RBTC,
+          estimatedRIFRewards: estimatedRIFRewards + RIF,
+          boostedRBTCRewards: boostedRBTCRewards + (RBTC * boost) / BigInt(normalisationFactor),
+          boostedRIFRewards: boostedRIFRewards + (RIF * boost) / BigInt(normalisationFactor),
+        }
+      },
+      Promise.resolve({
+        boostedRBTCRewards: 0n,
+        boostedRIFRewards: 0n,
+        estimatedRBTCRewards: 0n,
+        estimatedRIFRewards: 0n,
+      }),
+    )
   }
 
-  const result = {
+  const nftBoostData: NftBoostData = {
     nftContractAddress,
     boostPercentage,
-    calculationBlock: blockNumber.toString(),
-    holders: holdersData,
+    calculationBlock,
+    holders,
   }
-
-  const nftBoostFilename = `${nftContractAddress}-${blockNumber}.json`
+  const nftBoostFilename = `${nftContractAddress}-${calculationBlock}.json`
   const nftBoostPath = `${boostDataFolder}/${nftBoostFilename}.txt`
   fs.mkdirSync(boostDataFolder, { recursive: true })
-  fs.writeFileSync(nftBoostPath, JSON.stringify(result, null, 2))
+  fs.writeFileSync(
+    nftBoostPath,
+    JSON.stringify(
+      nftBoostData,
+      (_: string, value: unknown) => (typeof value === 'bigint' ? value.toString() : value),
+      2,
+    ),
+  )
   console.log(`Nft boost file saved: ${nftBoostPath}`)
 
   fs.writeFileSync(nftActiveBoostPath, nftBoostFilename)
