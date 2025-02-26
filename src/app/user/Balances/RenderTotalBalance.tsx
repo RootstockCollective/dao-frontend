7f753e21adfa281918ef975f448c7f14ba86ba85
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2025-02-26 17:59:28 -0300

    add locators (#739)

--- src/app/user/Balances/RenderTotalBalance.tsx
+++ src/app/user/Balances/RenderTotalBalance.tsx
@@ -2,26 +2,27 @@ import { useBalancesContext } from '@/app/user/Balances/context/BalancesContext'
 import { TokenImage } from '@/components/TokenImage'
 import { Paragraph } from '@/components/Typography'
 import { SupportedTokens } from '@/lib/contracts'
-import { formatCurrency, formatNumberWithCommas, toFixed } from '@/lib/utils'
+import { formatCurrency, formatNumberWithCommas } from '@/lib/utils'
 import Big from '@/lib/big'
 
 interface Props {
   symbol: SupportedTokens
+  context?: string
 }
 
-export const RenderTotalBalance = ({ symbol }: Props) => {
+export const RenderTotalBalance = ({ symbol, context }: Props) => {
   const { balances, prices } = useBalancesContext()
   const token = balances[symbol]
   const tokenBalance = Big(token.balance)
   const tokenBalanceRounded = symbol === 'RBTC' ? tokenBalance.toFixed(8) : tokenBalance.floor()
   return (
     <>
-      <Paragraph size="small" className="flex flex-row" data-testid={`${token.symbol}_Balance`}>
+      <Paragraph size="small" className="flex flex-row" data-testid={`${context}${token.symbol}_Balance`}>
         {formatNumberWithCommas(tokenBalanceRounded)} {token.symbol}
         <TokenImage symbol={token.symbol} className="ml-[8px]" />
       </Paragraph>
       {prices[symbol] && (
-        <Paragraph size="small" className="text-zinc-500" data-testid={`${token.symbol}_USD`}>
+        <Paragraph size="small" className="text-zinc-500" data-testid={`${context}${token.symbol}_USD`}>
           = USD {formatCurrency(tokenBalance.mul(prices[symbol].price)) ?? 0}
         </Paragraph>
       )}
