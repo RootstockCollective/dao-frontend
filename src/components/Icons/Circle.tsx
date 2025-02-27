ebbd47357a82a75ad912d14aae9402c540a18288
Cybervoid <shenshin@me.com>
2025-02-27 06:15:57 +0100

    feat: add Circle icon component and update TableCells to use it

---
+++ src/components/Icons/Circle.tsx
@@ -0,0 +1,23 @@
+import { type IconProps } from './types'
+import { DEFAULT_ICON_SIZE, DEFAULT_ICON_COLOR } from './constants'
+
+export function Circle({
+  'aria-label': ariaLabel = 'Circle Icon',
+  size = DEFAULT_ICON_SIZE,
+  fill = DEFAULT_ICON_COLOR,
+  ...props
+}: IconProps) {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 512 512"
+      fill={fill}
+      xmlns="http://www.w3.org/2000/svg"
+      aria-label={ariaLabel}
+      {...props}
+    >
+      <path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8z" />
+    </svg>
+  )
+}
--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -1,2 +1,3 @@
 export { ArrowDown } from './ArrowDown'
 export { ArrowUp } from './ArrowUp'
+export { Circle } from './Circle'
