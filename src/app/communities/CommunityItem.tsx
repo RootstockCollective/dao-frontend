b81ad154f9befca1930c19432837cc483de931ab
Cybervoid <shenshin@me.com>
2025-02-27 20:13:19 +0100

    feat: add ArrowUpRight icon component and update CommunityItem to use it

--- src/app/communities/CommunityItem.tsx
+++ src/app/communities/CommunityItem.tsx
@@ -1,6 +1,6 @@
 import { Paragraph, Span } from '@/components/Typography'
 import Image from 'next/image'
-import { BsArrowUpRight } from 'react-icons/bs'
+import { ArrowUpRight } from '@/components/Icons'
 import Link from 'next/link'
 import { BoostedBox } from './components/BoostedBox'
 import { BoostedLabel } from './components/BoostedLabel'
@@ -37,7 +37,7 @@ export const CommunityItem = ({ leftImageSrc, title, subtitle, nftAddress, descr
                 {subtitle}
               </Span>
             </div>
-            {nftAddress && <BsArrowUpRight />}
+            {nftAddress && <ArrowUpRight />}
           </div>
           <Paragraph variant="normal" className="mb-[8px] text-[14px]">
             {description}
