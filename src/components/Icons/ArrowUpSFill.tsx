daedf87c1919cee1bbec097988ad5faebf32af6a
Cybervoid <shenshin@me.com>
2025-02-27 06:40:46 +0100

    feat: add ArrowUpSFill and ArrowDownSFill icon components and update TableHeaders to use them

---
+++ src/components/Icons/ArrowUpSFill.tsx
@@ -0,0 +1,23 @@
+import { type IconProps } from './types'
+import { DEFAULT_ICON_SIZE, DEFAULT_ICON_COLOR } from './constants'
+
+export function ArrowUpSFill({
+  'aria-label': ariaLabel = 'Arrow Up Icon',
+  size = DEFAULT_ICON_SIZE,
+  fill = DEFAULT_ICON_COLOR,
+  ...props
+}: IconProps) {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill={fill}
+      xmlns="http://www.w3.org/2000/svg"
+      aria-label={ariaLabel}
+      {...props}
+    >
+      <path d="M12 8L18 14H6L12 8Z" />
+    </svg>
+  )
+}
