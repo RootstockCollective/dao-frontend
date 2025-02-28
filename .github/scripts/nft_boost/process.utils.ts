660a15449122d6d75e051c92c19c109d9d3964ed
DinizSa <39380608+DinizSa@users.noreply.github.com>
2025-02-28 12:51:22 +0000

    TOK-614: Script nfts (#690)
    * feat(cr_booster): add GHA workflow
    
    chore: add nft holders boost script
    
    chore: backer list script
    
    chore: allow nft booster deactivation
    
    refactor(cr_nft_boost): use proj deps
    
    * chore: improve events url construction
    
    * chore: use multical to optimize paralel calls
    
    * docs: explain magic value
    
    * chore: use multical to optimize paralel calls
    
    * chore: separate boost data branch by env
    
    * chore: stop workflow execution on critical error
    
    * chore: read env directly to avoid undefined
    
    * chore: improve types
    
    * chore: use unique branch per environment
    
    * feat: activate boost
    
    * feat: deactivate boost
    
    * chore: improvements
    
    chore: improvements
    
    chore: improvements
    
    chore: improvements
    
    * refactor(boost_script): extract modules
    
    * refactor(boost_script): reduce est calculation
    
    * refactor(boost_stript): avoid recalculation
    
    ---------
    
    Co-authored-by: Juraj Piar <juraj@iovlabs.org>

---
+++ .github/scripts/nft_boost/process.utils.ts
@@ -0,0 +1,47 @@
+import { config as envConfig } from 'dotenv'
+import { Address, getAddress } from 'viem'
+
+export const [, , ...args] = process.argv
+export type Args = {
+  nftContractAddress: Address
+  boostPercentage: number
+  env: string
+}
+const { nftContractAddress, boostPercentage, env } = args.reduce<Args>((acc, val) => {
+  if (val.startsWith('--nft')) {
+    acc.nftContractAddress = getAddress(val.split('=')[1])
+  }
+
+  if (val.startsWith('--boost')) {
+    acc.boostPercentage = parseFloat(val.split('=')[1])
+  }
+
+  if (val.startsWith('--env')) {
+    acc.env = val.split('=')[1]
+  }
+
+  return acc
+}, {} as Args)
+
+if (!env || !nftContractAddress || isNaN(boostPercentage)) {
+  throw new Error(
+    'Usage: npx tsx .github/scripts/nft_boost/activateBoost.ts \\\
+    --nft=<nftContractAddress> \\\
+    --boost=<boostPercentage> \\\
+    --env=<env>',
+  )
+}
+
+console.info('nft: ', nftContractAddress)
+console.info('Boost percentage: ', boostPercentage)
+console.info('env: ', env)
+
+if (boostPercentage < 0) {
+  throw new Error('Boost percentage must be positive')
+}
+
+envConfig({
+  path: `.env.${env}`,
+})
+
+export { boostPercentage, env, nftContractAddress }
