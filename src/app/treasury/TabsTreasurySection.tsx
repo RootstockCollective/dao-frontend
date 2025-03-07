6ef75df0592cf7d0b3303b7a676c17903f98df2e
Cybervoid <shenshin@me.com>
2025-03-07 17:17:02 +0100

    feat: update TabsTreasurySection to use new tab structure and context

--- src/app/treasury/TabsTreasurySection.tsx
+++ src/app/treasury/TabsTreasurySection.tsx
@@ -1,10 +1,20 @@
 import { useState } from 'react'
 import { UnderlineTabs, type BaseTab } from '@/components/Tabs'
+import { treasuryContracts } from '@/lib/contracts'
+import { useTreasuryContext } from '@/app/treasury/TreasuryContext'
 
-const tabs = [{ value: 'Grant' }, { value: 'Growth' }, { value: 'General' }] as const satisfies BaseTab[]
+const tabs = [
+  { value: 'GRANTS', label: 'Grant' },
+  { value: 'GROWTH', label: 'Growth' },
+  { value: 'GENERAL', label: 'General' },
+] as const satisfies BaseTab<keyof typeof treasuryContracts>[]
 
 export function TabsTreasurySection() {
-  const [activeTab, setActiveTab] = useState<(typeof tabs)[number]['value']>('Grant')
+  const { buckets } = useTreasuryContext()
+  console.log('🚀 ~ treasuryContracts:', treasuryContracts)
+  console.log('🚀 ~ TabsTreasurySection ~ buckets:', buckets)
+
+  const [activeTab, setActiveTab] = useState<(typeof tabs)[number]['value']>('GRANTS')
   return (
     <div>
       <UnderlineTabs className="py-4" tabs={tabs} activeTab={activeTab} onTabChange={setActiveTab} />
