f14732663b622d4f9cbf8265f1aec89b91d7287f
DinizSa <diniz.araujosa@rootstocklabs.com>
2025-03-11 23:47:01 +0000

    feat: disable builder settings for no-builders or disconnected

--- src/app/collective-rewards/settings/Settings.tsx
+++ src/app/collective-rewards/settings/Settings.tsx
@@ -3,6 +3,10 @@
 import { useRouter, useSearchParams } from 'next/navigation'
 import { FC, ReactNode } from 'react'
 import { BuilderSettings } from './builder/BuilderSettings'
+import { useAccount } from 'wagmi'
+import { useGetBuilderToGauge } from '@/app/collective-rewards/user'
+import { LoadingSpinner } from '@/components/LoadingSpinner'
+import { zeroAddress } from 'viem'
 
 const settingTypes = ['builder'] as const
 type SettingType = (typeof settingTypes)[number]
@@ -14,13 +18,30 @@ const settings: Record<SettingType, ReactNode> = {
 }
 
 export const Settings: FC = () => {
+  const { isConnected, address } = useAccount()
   const searchParams = useSearchParams()
+  const router = useRouter()
+  const { data: gauge, isLoading: isLoadingGauge } = useGetBuilderToGauge(address!)
+
+  if (!isConnected) {
+    router.push('/')
+    // return to avoid rendering the component while redirecting
+    return
+  }
+
+  if (isLoadingGauge) {
+    return <LoadingSpinner />
+  }
+  if (!gauge || gauge === zeroAddress) {
+    router.push('/')
+    return
+  }
+
   const settingType = searchParams?.get('type') as SettingType
 
-  const router = useRouter()
-
-  if (!isSettingType(settingType)) {
+  if (!settingType || !isSettingType(settingType)) {
     router.push('/_not-found')
+    return
   }
 
   return settings[settingType]
