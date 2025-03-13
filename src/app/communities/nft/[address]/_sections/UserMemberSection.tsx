3bf7fc4ff83f3315887d8f064798e47822b24d37
Antonio <antonio@iovlabs.org>
2025-03-13 09:59:20 +0100

    fix: align nft card and button

--- src/app/communities/nft/[address]/_sections/UserMemberSection.tsx
+++ src/app/communities/nft/[address]/_sections/UserMemberSection.tsx
@@ -17,12 +17,12 @@ export const UserMemberSection = () => {
   const showNFTBoost = isCampaignActive(address)
 
   return (
-    <div>
+    <div className="flex flex-col justify-between gap-4">
       <Paragraph variant="semibold" className="text-[18px]">
         {title} #{tokenId}
       </Paragraph>
       {/* `Owned by 0x00000` colored with 2 colors */}
-      <div className="my-[16px] font-light">
+      <div className="font-light">
         <Span>Owned{address && ' by '}</Span>
         {address && <Span className="text-primary">{truncateMiddle(address, 4, 3)}</Span>}
       </div>
