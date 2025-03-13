94ce8d2b5d527d8d455ea4be66f424f039d9192c
Jesse Clark <hello@developerjesse.com>
2025-03-13 17:18:47 +0200

    Update texts for communities

--- src/app/communities/communityUtils.tsx
+++ src/app/communities/communityUtils.tsx
@@ -36,7 +36,7 @@ export const earlyAdoptersCommunity = {
   title: 'Early Adopters',
   subtitle: 'DeFi',
   description:
-    'The Early Adopters badge symbolizes foresight and commitment, awarded to the first pioneers who saw the potential of RootstockCollective and staked their claim early. These digital trailblazers embody the spirit of innovation and are the foundation of the community.',
+    'The Early Adopters badge symbolizes foresight and commitment, awarded to the first pioneers who saw the potential of RootstockCollective and staked their claim early.',
   nftAddress: nftContracts.EA,
   numberOfMembers: 0,
   cover: '/images/nfts/ea-nft-cover.png',
@@ -46,19 +46,7 @@ export const earlyAdoptersCommunity = {
   ) => (
     <>
       <p className="mt-4">
-        The Early Adopters collection features a vibrant array of digital pioneers, each uniquely crafted to
-        embody the spirit of innovation and community in the blockchain world. From governance and protocol
-        architects to visionary explorers and collaborative creators, these NFTs represent the diverse talents
-        and passions driving the decentralized revolution.
-      </p>
-      <p className="mt-4">
-        Whether blazing new trails as blockchain pioneers, nurturing the ecosystem as open-source champions,
-        or guiding the community as decentralized thinkers, each character in this collection is a testament
-        to the boundless creativity and dedication of those building the future of Bitcoin and beyond.
-      </p>
-      <p className="mt-4">
-        Join the journey with these extraordinary individuals as they carve out a new digital frontier, one
-        block at a time.
+        The Early Adopters badge symbolizes foresight and commitment, awarded to the first pioneers who saw the potential of RootstockCollective and staked their claim early. These digital trailblazers embody the spirit of innovation and are the foundation of the community.
       </p>
       <p className={DEFAULT_CLASS_FOR_ROW_CONTAINER}>
         <Row leftText="SPECIAL POWER" rightText="Voting Booster" />
@@ -74,7 +62,7 @@ export const ogFounders: CommunityItem = {
   title: 'OG Founders',
   subtitle: 'DeFi',
   description:
-    "The OG Founders badge celebrates the visionaries who laid the groundwork for RootstockCollective's success. These early supporters are recognized as the architects of the DAO's future.",
+    'The OG Founders badge celebrates the visionaries who laid the groundwork for RootstockCollective's success.',
   nftAddress: nftContracts.OG_FOUNDERS,
   numberOfMembers: 0,
   cover: '/images/nfts/founders-cover.png',
@@ -82,9 +70,7 @@ export const ogFounders: CommunityItem = {
   longDescription: ({ activation = 'MAY 2025' }) => (
     <>
       <p className="mt-4">
-        In the magical realm of Rootstock, a select group of visionary elves, known as the Founders, received
-        the prestigious OG Badge. This badge was a testament to their foresight and pioneering spirit, marking
-        them as the true architects of their era.
+        The OG Founders badge celebrates the visionaries who laid the groundwork for RootstockCollective's success. These early supporters are recognized as the architects of the DAO's future.
       </p>
       <p className={DEFAULT_CLASS_FOR_ROW_CONTAINER}>
         <Row leftText="SPECIAL POWER" rightText="Voting Booster" />
@@ -100,7 +86,7 @@ export const ogFoundersEcosystemPartners: CommunityItem = {
   title: 'OG Partners',
   subtitle: 'DeFi',
   description:
-    "The OG Partners badge honours external collaborators who joined RootstockCollective in its early days. This badge represents their invaluable contributions to the ecosystem's growth and their role in shaping its trajectory.",
+    'The OG Partners badge honors external collaborators who joined RootstockCollective in its early days.',
   nftAddress: nftContracts.OG_PARTNERS,
   numberOfMembers: 0,
   cover: '/images/nfts/partners-cover.png',
@@ -108,9 +94,7 @@ export const ogFoundersEcosystemPartners: CommunityItem = {
   longDescription: ({ activation = 'JUN 2025' }) => (
     <>
       <p className="mt-4">
-        In the magical realm of Rootstock, a new chapter unfolds as the elves of the RootstockCollective
-        embark on an extraordinary journey. Equipped with the OG Badge, these visionary elves invite their
-        esteemed external partners to join them in exploring and innovating within the Rootstock ecosystem.
+        The OG Partners badge honors external collaborators who joined RootstockCollective in its early days. This badge represents their invaluable contributions to the ecosystem's growth and their role in shaping its trajectory.
       </p>
       <p className={DEFAULT_CLASS_FOR_ROW_CONTAINER}>
         <Row leftText="SPECIAL POWER" rightText="Voting Booster" />
@@ -126,7 +110,7 @@ export const ogFoundersExternalContributors: CommunityItem = {
   title: 'OG Contributors',
   subtitle: 'DeFi',
   description:
-    "Awarded to those who demonstrated exceptional contributions during the collective's formative stages, the OG Contributors badge recognizes the efforts of individuals who went above and beyond to support the DAO's vision, starting with the Shepherds.",
+    "Awarded to those who demonstrated exceptional contributions during the collective's formative stages.",
   nftAddress: nftContracts.OG_CONTRIBUTORS,
   numberOfMembers: 0,
   cover: '/images/nfts/contributors-cover.png',
@@ -134,11 +118,7 @@ export const ogFoundersExternalContributors: CommunityItem = {
   longDescription: ({ activation = 'MAR 2025' }) => (
     <>
       <p className="mt-4">
-        In the enchanting realm of Rootstock, a thrilling new chapter unfolds as this dedicated and
-        specialised club in the community embark on an extraordinary journey making outstanding contributions
-        to the Collective starting with the Shepherds. These visionaries, recognized for their remarkable
-        contributions, are bestowed with the prestigious OG Contributors Badge - a symbol of honor that
-        highlights their passion and engagement with Governance process and community needs.
+        Awarded to those who demonstrated exceptional contributions during the collective's formative stages. The OG Contributors badge recognizes the efforts of individuals who went above and beyond to support the DAO's vision, starting with the Shepherds.
       </p>
       <p className={DEFAULT_CLASS_FOR_ROW_CONTAINER}>
         <Row leftText="SPECIAL POWER" rightText="Delegation Kickstarter OR Voting Booster" />
@@ -154,7 +134,7 @@ export const vanguardCommunity = {
   title: 'Vanguard',
   subtitle: 'DeFi',
   description:
-    'The Voting Vanguard badge celebrates the governance pioneers who actively participate in shaping the DAO. These members are the guiding stars, lighting the path toward shared prosperity.',
+    'The Vanguard badge celebrates the governance pioneers who actively participate in shaping the DAO.',
   nftAddress: nftContracts.VANGUARD,
   numberOfMembers: 0,
   cover: '/images/nfts/vanguard-cover.jpg',
@@ -164,11 +144,7 @@ export const vanguardCommunity = {
   ) => (
     <>
       <p className="mt-4">
-        The Voting Vanguards are the daring cosmonauts of RootstockCollective, charting new frontiers in
-        decentralized governance. To join their ranks, you must engage in DAO voting and help guide the
-        ecosystem toward shared prosperity. Adorned with glowing Bitcoin energy, these spacefaring pioneers
-        symbolize innovation and unity. They embody the courage to explore uncharted territories, lighting the
-        way for a decentralized future and proving that collective action can transform the Bitcoin universe.
+        The Vanguard badge celebrates the governance pioneers who actively participate in shaping the DAO. These members are the guiding stars, lighting the path toward shared prosperity.
       </p>
       <p className={DEFAULT_CLASS_FOR_ROW_CONTAINER}>
         <Row leftText="SPECIAL POWER" rightText="Voting Booster" />
@@ -202,7 +178,7 @@ export const betaBuilders: CommunityItem = {
   title: 'Beta Builders',
   subtitle: 'DeFi',
   description:
-    "Beta Builders are the innovation heroes, creating the dApps and protocols that define our ecosystem. This badge is a testament to their contributions to advancing Bitcoin's utility through cutting-edge development.",
+    'Beta Builders are the innovation heroes, creating the dApps and protocols that define our ecosystem.',
   nftAddress: nftContracts.BB,
   numberOfMembers: 0,
   cover: '/images/nfts/bb-cover.png',
@@ -210,9 +186,7 @@ export const betaBuilders: CommunityItem = {
   longDescription: ({ activation = 'MAY 2025' }) => (
     <>
       <p className="mt-4">
-        The Beta Builders collection is a badge of honor for the visionaries shaping the future of Bitcoin
-        through RootstockCollective. It celebrates the pioneering Builders, Developers, Protocols, and dApps
-        whose innovations are driving Rootstock in becoming the leading Bitcoin Layer 2.
+        Beta Builders are the innovation heroes, creating the dApps and protocols that define our ecosystem. This badge is a testament to their contributions to advancing Bitcoin's utility through cutting-edge development.
       </p>
       <p className={DEFAULT_CLASS_FOR_ROW_CONTAINER}>
         <Row leftText="SPECIAL POWER" rightText="Voting Booster" />
