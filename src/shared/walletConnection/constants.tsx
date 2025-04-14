48bea3b6ea7b490e0df09d92a381dc201253a23a
Jesse Clark <hello@developerjesse.com>
2025-04-14 12:32:18 +0300

    Modify text to remove the first sentence.

--- src/shared/walletConnection/constants.tsx
+++ src/shared/walletConnection/constants.tsx
@@ -8,21 +8,9 @@ export const disclaimerModalText = {
   modalTitle: 'DISCLAIMER',
   modalDescription: () => (
     <>
-      The RootstockCollective has taken actions in order to prevent <br />
-      access to any person located in the prohibited jurisdictions, <br />
-      as mentioned in the{' '}
-      <a
-        href="https://wiki.rootstockcollective.xyz/Terms-of-Use-1041ca6b0b028041a0b1de60e2c16f3c"
-        target="_blank"
-        className="underline cursor-pointer"
-      >
-        Terms of Use
-      </a>
-      , including any person <br />
-      located in the United States of America. Please note that <br />
-      interaction with the dApp by any person or entity considered <br />
-      a resident, or taxpayer in a prohibited jurisdiction, including <br />
-      without limitation the United States of America, is forbidden. <br />
+      Please note that interaction with the dApp by any person or entity <br />
+      considered a resident, or taxpayer in a prohibited jurisdiction, <br />
+      including without limitation the United States of America, is forbidden. <br />
       Please{' '}
       <a
         href="https://wiki.rootstockcollective.xyz/Terms-of-Use-1041ca6b0b028041a0b1de60e2c16f3c"
