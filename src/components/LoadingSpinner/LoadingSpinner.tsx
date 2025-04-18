066ca6fad6eb611eddb46afa62f1f67f8b4d3291
LordCheta <chinaechetam.maduabuchi@iovlabs.org>
2025-04-18 21:04:01 +0100

    refactor: remove lottie-react lib

--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -1,10 +1,27 @@
 import { cn } from '@/lib/utils'
-import Lottie from 'lottie-react'
-import loadingAnimation from '@/public/loading.json'
 
 const LoadingSpinner = ({ className = '' }) => (
-  <div className={cn('flex justify-center', className)}>
-    <Lottie animationData={loadingAnimation} className="w-1/2" loop={true} />
+  <div className={cn('flex justify-center items-center', className)}>
+    <svg 
+      className="animate-spin h-20 w-20 text-indigo-500" 
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none" 
+      viewBox="0 0 24 24"
+    >
+      <circle 
+        className="opacity-25" 
+        cx="12" 
+        cy="12" 
+        r="10" 
+        stroke="currentColor" 
+        strokeWidth="4"
+      />
+      <path 
+        className="opacity-75" 
+        fill="currentColor" 
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
+      />
+    </svg>
   </div>
 )
 
