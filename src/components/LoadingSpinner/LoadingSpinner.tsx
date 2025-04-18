3e6c5fa8d28046330a5849ad92793923ec133441
LordCheta <chinaechetam.maduabuchi@iovlabs.org>
2025-04-18 21:10:13 +0100

    chore: lint fix

--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -2,25 +2,14 @@ import { cn } from '@/lib/utils'
 
 const LoadingSpinner = ({ className = '' }) => (
   <div className={cn('flex justify-center items-center', className)}>
-    <svg 
-      className="animate-spin h-20 w-20 text-indigo-500" 
+    <svg
+      className="animate-spin h-20 w-20 text-indigo-500"
       xmlns="http://www.w3.org/2000/svg"
-      fill="none" 
+      fill="none"
       viewBox="0 0 24 24"
     >
-      <circle 
-        className="opacity-25" 
-        cx="12" 
-        cy="12" 
-        r="10" 
-        stroke="currentColor" 
-        strokeWidth="4"
-      />
-      <path 
-        className="opacity-75" 
-        fill="currentColor" 
-        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
-      />
+      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" />
+      <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z" />
     </svg>
   </div>
 )
