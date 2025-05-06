e3b0e269ac38442e37cccd18429ca65dee74378a
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2025-05-06 02:38:26 -0300

    extract toast config to helper

--- src/shared/lib/toastAlert.tsx
+++ src/shared/lib/toastAlert.tsx
@@ -1,14 +1,41 @@
 import { Paragraph } from '@/components/Typography'
-import { Bounce, toast } from 'react-toastify'
+import { ReactNode } from 'react'
+import { Bounce, Id, toast, ToastOptions } from 'react-toastify'
 
 interface ToastAlertProps {
   severity: 'error' | 'success' | 'info' | 'warning'
   title: string
-  content: string | React.ReactNode
+  content: string | ReactNode
   dismissible?: boolean
   dataTestId?: string
 }
 
+const buildToastProps = ({
+  severity,
+  title,
+  content,
+  dismissible = false,
+}: ToastAlertProps): ToastOptions => ({
+  toastId: `${severity}-${title}-${content}`,
+  type: severity,
+  position: 'top-right',
+  autoClose: dismissible ? 5000 : false,
+  hideProgressBar: !dismissible,
+  closeOnClick: dismissible,
+  closeButton: dismissible,
+  pauseOnHover: false,
+  theme: 'dark',
+  transition: Bounce,
+})
+
+/**
+ * This function is used to show a toast alert with the given properties
+ * @param severity - The severity of the alert (error, success, info, warning)
+ * @param title - The title of the alert
+ * @param content - The content of the alert (can be a string or a ReactNode)
+ * @param dismissible - Whether the alert is dismissible or not (default: false)
+ * @param dataTestId - The data-testid attribute for testing purposes (default: '')
+ */
 export const showToastAlert = ({
   severity,
   title,
@@ -39,17 +66,23 @@ export const showToastAlert = ({
         )}
       </div>
     </div>,
-    {
-      type: severity,
-      position: 'top-right',
-      autoClose: dismissible ? 5000 : false,
-      hideProgressBar: !dismissible,
-      closeOnClick: dismissible,
-      closeButton: dismissible,
-      pauseOnHover: false,
-      theme: 'dark',
-      transition: Bounce,
-    },
+    buildToastProps({
+      severity,
+      title,
+      content,
+      dismissible,
+    }),
   )
 
+/**
+ * This function is used to update a toast alert with the given properties
+ * @param toastId - The ID of the toast alert to update
+ * @param props - The optional properties to update the toast alert with
+ */
+export const updateToastAlert = (toastId: Id, props: Partial<ToastAlertProps>) =>
+  toast.update(toastId, buildToastProps({ ...props } as ToastAlertProps))
+
+/**
+ * This function is used to dismiss all toast alerts
+ */
 export const dismissToastAlerts = () => toast.dismiss()
