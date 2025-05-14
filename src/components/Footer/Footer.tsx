a22695e7d9ccfd0ec4f711ad703f6eaf6ab92e3d
LordCheta <chinaechetam.maduabuchi@iovlabs.org>
2025-05-14 18:29:14 +0100

    feat: wip

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,61 +8,74 @@ interface Props {
   variant?: 'login' | 'container'
 }
 
-const DEFAULT_CONTAINER_CLASS = 'm-5'
-
-const DEFAULT_LOGIN_CLASS = 'absolute left-8 w-2/5 '
-
-const VARIANTS = {
-  container: DEFAULT_CONTAINER_CLASS,
-  login: DEFAULT_LOGIN_CLASS,
-}
-
 export const Footer = ({ variant = 'login' }: Props) => (
-  <footer className={cn('bottom-4 flex justify-between text-sm opacity-60 ', VARIANTS[variant])}>
-    <div className="flex items-start">
+  <>
+    {/* Main footer with three sections: Build ID, FAQs/Whitepaper, Social Icons */}
+    <footer className="bg-[#121212] w-full flex flex-row justify-between items-center py-3 px-5 text-sm text-gray-400">
+      {/* Left: Build ID section */}
+      <div>
+        <a
+          href={`https://github.com/${GITHUB_ORG}/dao-frontend/commit/${BUILD_ID}`}
+          className="hover:underline"
+          target="_blank"
+          data-testid="BuildIDLink"
+        >
+          Build ID: {BUILD_ID ? BUILD_ID.slice(0, 7) : ''} ({ENV})
+        </a>
+      </div>
+
+      {/* Center: Links section - desktop only */}
+      <div className="hidden md:flex space-x-6">
+        <a
+          href="https://wiki.rootstockcollective.xyz/RootstockCollective-FAQ-1031ca6b0b02808c95d3dcb5a0074f4b"
+          className="hover:underline"
+          target="_blank"
+          data-testid="FAQsLink"
+        >
+          FAQs
+        </a>
+        <a
+          href="https://wiki.rootstockcollective.xyz"
+          className="hover:underline"
+          target="_blank"
+          data-testid="WhitepaperLink"
+        >
+          Whitepaper
+        </a>
+      </div>
+
+      {/* Right: Social icons */}
+      <div className="flex space-x-3">
+        <a href="https://x.com/rootstockcoll" target="_blank" data-testid="TwitterLink">
+          <TwitterXIcon size={'1.4em'} />
+        </a>
+        <a href={`https://github.com/${GITHUB_ORG}`} target="_blank" data-testid="GithubLink">
+          <GithubIcon size={'1.5em'} />
+        </a>
+        <a href="https://t.me/rootstockcollective" target="_blank" data-testid="TelegramLink">
+          <TelegramIcon size={'1.5em'} />
+        </a>
+      </div>
+    </footer>
+
+    {/* Mobile only links row */}
+    <div className="md:hidden w-full flex justify-center space-x-6 bg-[#121212] py-3 text-sm text-gray-400">
       <a
-        href={`https://github.com/${GITHUB_ORG}/dao-frontend/commit/${BUILD_ID}`}
-        className="hover:underline me-4 md:me-6"
+        href="https://wiki.rootstockcollective.xyz/RootstockCollective-FAQ-1031ca6b0b02808c95d3dcb5a0074f4b"
+        className="hover:underline"
         target="_blank"
-        data-testid="BuildIDLink"
+        data-testid="FAQsLink-mobile"
       >
-        Build ID: {BUILD_ID ? BUILD_ID.slice(0, 7) : ''} ({ENV})
+        FAQs
+      </a>
+      <a
+        href="https://wiki.rootstockcollective.xyz"
+        className="hover:underline"
+        target="_blank"
+        data-testid="WhitepaperLink-mobile"
+      >
+        Whitepaper
       </a>
     </div>
-    {variant === 'container' && (
-      <>
-        <div className="flex items-start">
-          <a
-            href="https://wiki.rootstockcollective.xyz/RootstockCollective-FAQ-1031ca6b0b02808c95d3dcb5a0074f4b"
-            className="hover:underline me-4 md:me-6"
-            target="_blank"
-            data-testid="FAQsLink"
-          >
-            FAQs
-          </a>
-        </div>
-        <div className="flex items-start">
-          <a
-            href="https://wiki.rootstockcollective.xyz"
-            className="hover:underline me-4 md:me-6"
-            target="_blank"
-            data-testid="WhitepaperLink"
-          >
-            Whitepaper
-          </a>
-        </div>
-      </>
-    )}
-    <div className="flex items-start justify-end">
-      <a href="https://x.com/rootstockcoll" target="_blank" data-testid="TwitterLink">
-        <TwitterXIcon className="mr-4" size={'1.4em'} />
-      </a>
-      <a href={`https://github.com/${GITHUB_ORG}`} target="_blank" data-testid="GithubLink">
-        <GithubIcon className="mr-4" size={'1.5em'} />
-      </a>
-      <a href="https://t.me/rootstockcollective" target="_blank" data-testid="TelegramLink">
-        <TelegramIcon className="mr-4" size={'1.5em'} />
-      </a>
-    </div>
-  </footer>
+  </>
 )
