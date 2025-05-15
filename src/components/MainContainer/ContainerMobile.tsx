c1005dcf79f01ef6d7771fbb4b30a6b373977a5b
Cybervoid <shenshin@me.com>
2025-05-15 05:51:29 +0200

    feat: add animated drawer in mobile layout

--- src/components/MainContainer/ContainerMobile.tsx
+++ src/components/MainContainer/ContainerMobile.tsx
@@ -4,12 +4,14 @@ import { HTMLAttributes } from 'react'
 import { cn } from '@/lib/utils'
 import { HeaderMobile } from './headers/HeaderMobile'
 import { FooterMobile } from '../Footer'
+import { SidebarMobile } from './sidebars/SidebarMobile'
 
 export default function ContainerMobile({ className, children, ...props }: HTMLAttributes<HTMLDivElement>) {
   return (
     <div {...props} className={cn('flex flex-col min-h-screen', className)}>
       <HeaderMobile />
-      <div className="p-4 grow flex flex-col">
+      <div className="relative p-4 grow flex flex-col">
+        <SidebarMobile />
         <main className="grow mb-25">{children}</main>
         <FooterMobile />
       </div>
