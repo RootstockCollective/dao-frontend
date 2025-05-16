ea5d6ccd0f992ddde9b2053c0cbd054ff5b52011
LordCheta <chinaechetam.maduabuchi@iovlabs.org>
2025-05-16 06:25:38 +0100

    feat: wip

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -11,51 +11,50 @@ interface Props {
 export const Footer = ({ variant = 'login' }: Props) => (
   <>
     {/* Main footer with three sections: Build ID, FAQs/Whitepaper, Social Icons */}
-    <footer className="bg-[#121212] w-full flex flex-row justify-between items-center py-3 px-5 text-sm text-gray-400">
-      {/* Left: Build ID section */}
-      <div>
-        <a
-          href={`https://github.com/${GITHUB_ORG}/dao-frontend/commit/${BUILD_ID}`}
-          className="hover:underline"
-          target="_blank"
-          data-testid="BuildIDLink"
-        >
-          Build ID: {BUILD_ID ? BUILD_ID.slice(0, 7) : ''} ({ENV})
-        </a>
-      </div>
+    <footer>
+      <section className="bg-[#121212] w-full flex flex-row justify-between items-center p-6 text-sm border-t border-gray-600 text-gray-400">
+        {/* Left: Build ID section */}
+        <div className="hidden md:flex space-x-6 divide-x-1 divide-gray-600 divide-solid px-4">
+          <a
+            href={`https://github.com/${GITHUB_ORG}/dao-frontend/commit/${BUILD_ID}`}
+            className="hover:underline"
+            target="_blank"
+            data-testid="BuildIDLink"
+          >
+            Build ID: {BUILD_ID ? BUILD_ID.slice(0, 7) : ''} ({ENV})
+          </a>
 
-      {/* Center: Links section - desktop only */}
-      <div className="hidden md:flex space-x-6">
-        <a
-          href="https://wiki.rootstockcollective.xyz/RootstockCollective-FAQ-1031ca6b0b02808c95d3dcb5a0074f4b"
-          className="hover:underline"
-          target="_blank"
-          data-testid="FAQsLink"
-        >
-          FAQs
-        </a>
-        <a
-          href="https://wiki.rootstockcollective.xyz"
-          className="hover:underline"
-          target="_blank"
-          data-testid="WhitepaperLink"
-        >
-          Whitepaper
-        </a>
-      </div>
+          <a
+            href="https://wiki.rootstockcollective.xyz/RootstockCollective-FAQ-1031ca6b0b02808c95d3dcb5a0074f4b"
+            className="hover:underline"
+            target="_blank"
+            data-testid="FAQsLink"
+          >
+            FAQs
+          </a>
+          <a
+            href="https://wiki.rootstockcollective.xyz"
+            className="hover:underline"
+            target="_blank"
+            data-testid="WhitepaperLink"
+          >
+            Whitepaper
+          </a>
+        </div>
 
-      {/* Right: Social icons */}
-      <div className="flex space-x-3">
-        <a href="https://x.com/rootstockcoll" target="_blank" data-testid="TwitterLink">
-          <TwitterXIcon size={'1.4em'} />
-        </a>
-        <a href={`https://github.com/${GITHUB_ORG}`} target="_blank" data-testid="GithubLink">
-          <GithubIcon size={'1.5em'} />
-        </a>
-        <a href="https://t.me/rootstockcollective" target="_blank" data-testid="TelegramLink">
-          <TelegramIcon size={'1.5em'} />
-        </a>
-      </div>
+        {/* Right: Social icons */}
+        <div className="flex space-x-3">
+          <a href="https://x.com/rootstockcoll" target="_blank" data-testid="TwitterLink">
+            <TwitterXIcon size={'1.4em'} />
+          </a>
+          <a href={`https://github.com/${GITHUB_ORG}`} target="_blank" data-testid="GithubLink">
+            <GithubIcon size={'1.5em'} />
+          </a>
+          <a href="https://t.me/rootstockcollective" target="_blank" data-testid="TelegramLink">
+            <TelegramIcon size={'1.5em'} />
+          </a>
+        </div>
+      </section>
     </footer>
 
     {/* Mobile only links row */}
