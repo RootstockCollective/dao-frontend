6d042ba35a61a7c92f932e2c6da26ef81c4978f4
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2025-05-21 18:00:01 -0300

    refactor: simplify ProgressButton and its icons by removing props and using hooks directly

--- src/components/ProgressButton/ProgressButton.tsx
+++ src/components/ProgressButton/ProgressButton.tsx
@@ -1,18 +1,9 @@
 import { cn } from '@/lib/utils'
 import { Span } from '../TypographyNew'
-import { useInterval, useProgressAnimation } from './hooks'
 import { AnimatedHourglassIcon, AnimatedProgressPattern } from './icons'
 
-const PROGRESS_DURATION = 5000
-const PROGRESS_FLIP_DURATION = 700
-const HOURGLASS_FLIP_DURATION = 2000
 const SIZE_CLASSES = 'w-64 h-12'
-
 export const ProgressButton = () => {
-  const progress = useProgressAnimation(PROGRESS_DURATION)
-  const patternFlip = useInterval(PROGRESS_FLIP_DURATION)
-  const hourglassFlip = useInterval(HOURGLASS_FLIP_DURATION)
-
   return (
     <div
       className={cn(
@@ -21,11 +12,11 @@ export const ProgressButton = () => {
       )}
     >
       <div className={cn('absolute inset-y-0 left-0 overflow-hidden', SIZE_CLASSES)}>
-        <AnimatedProgressPattern progress={progress} flip={patternFlip} />
+        <AnimatedProgressPattern />
       </div>
 
       <div className="relative z-10 flex items-center space-x-1 justify-center">
-        <AnimatedHourglassIcon flip={hourglassFlip} />
+        <AnimatedHourglassIcon />
         <Span className="text-[#D4CFC4]" variant="body" bold>
           In progress
         </Span>
