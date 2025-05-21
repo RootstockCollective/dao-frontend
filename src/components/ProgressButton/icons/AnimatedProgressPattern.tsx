f6b345f9f10ce7bb176d65268c238df3ebc7cefe
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2025-05-21 17:56:02 -0300

    refactor: replace magic numbers

--- src/components/ProgressButton/icons/AnimatedProgressPattern.tsx
+++ src/components/ProgressButton/icons/AnimatedProgressPattern.tsx
@@ -7,6 +7,9 @@ interface Props {
   flip: boolean
 }
 
+const PATTERN_OFFSET = 48
+const PATTERN_ADJUSTMENT = 106
+
 export const AnimatedProgressPattern = ({ progress, flip }: Props) => {
   const containerRef = useRef<HTMLDivElement>(null)
   const ratio = progress / 100
@@ -19,7 +22,7 @@ export const AnimatedProgressPattern = ({ progress, flip }: Props) => {
         ref={containerRef}
         className="absolute inset-y-0 w-full"
         initial={false}
-        animate={{ x: ratio * (width + 48) - 106 }}
+        animate={{ x: ratio * (width + PATTERN_OFFSET) - PATTERN_ADJUSTMENT }}
         transition={{ duration: 0 }}
       >
         <div className="absolute inset-y-0 left-[calc(-100%_+_10px)] w-full bg-[#66605C]" />
