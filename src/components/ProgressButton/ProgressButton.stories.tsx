5a2dce8c1b3afc60f1198093b82f715f17457688
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2025-05-21 17:02:40 -0300

    feat: implement ProgressButton component with animation and icons

---
+++ src/components/ProgressButton/ProgressButton.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { ProgressButton } from './ProgressButton'
+
+export default {
+  title: 'Components/ProgressButton',
+  component: ProgressButton,
+} as Meta<typeof ProgressButton>
+
+type Story = StoryObj<typeof ProgressButton>
+
+export const Default: Story = {}
---
+++ src/components/ProgressButton/icons/index.ts
@@ -0,0 +1,2 @@
+export * from './HourglassIcon'
+export * from './ProgressPatternIcon'
