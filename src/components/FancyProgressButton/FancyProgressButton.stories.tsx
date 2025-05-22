ca5d98f38b67c601626052e906470ac03580f983
Cybervoid <shenshin@me.com>
2025-05-22 20:37:47 +0200

    feat: implement FancyProgressButton component with animated grid and styles

---
+++ src/components/FancyProgressButton/FancyProgressButton.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { FancyProgressButton } from './FancyProgressButton'
+
+export default {
+  title: 'Components/ProgressButton/FancyProgressButton',
+  component: FancyProgressButton,
+} as Meta<typeof FancyProgressButton>
+
+type Story = StoryObj<typeof FancyProgressButton>
+
+export const Default: Story = {}
