5fac8e7b586b4b965efd2dffdb57872bcf1b3431
Cybervoid <mail@cybervoid.me>
2025-05-23 15:43:52 +0200

    feat: enhance FancyProgressButton with dynamic wave animation and improved props handling

--- src/components/FancyProgressButton/FancyProgressButton.stories.tsx
+++ src/components/FancyProgressButton/FancyProgressButton.stories.tsx
@@ -8,4 +8,32 @@ export default {
 
 type Story = StoryObj<typeof FancyProgressButton>
 
-export const Default: Story = {}
+export const Default: Story = {
+  args: {
+    children: <p>Lorem ipsum, dolor sit amet</p>,
+  },
+}
+
+export const Colored: Story = {
+  args: {
+    children: <p>Colored Progress Bar</p>,
+    colors: ['#ff0000', '#00ff00', '#0000ff'],
+    width: 400,
+    height: 50,
+    tileSize: 5,
+    speed: 20,
+    className: 'border-none',
+  },
+}
+
+export const ThinLine: Story = {
+  args: {
+    colors: ['#3b41eb', '#ed6522'],
+    tileSize: 3,
+    height: 9,
+    width: 500,
+    className: 'border-none',
+    speed: 70,
+    dispersion: 0.3,
+  },
+}
--- src/components/FancyProgressButton/FancyProgressButton.tsx
+++ src/components/FancyProgressButton/FancyProgressButton.tsx
@@ -1,11 +1,15 @@
 import { cn } from '@/lib/utils'
-import { useMemo, HTMLAttributes } from 'react'
+import { useMemo, HTMLAttributes, useState, useEffect } from 'react'
 import { motion } from 'motion/react'
 
+type Color = `#${string}`
 interface Props extends HTMLAttributes<HTMLButtonElement> {
   width: number
   height: number
   tileSize?: number
+  colors: Color[]
+  speed: number
+  dispersion: number
 }
 
 interface Cell {
@@ -14,50 +18,72 @@ interface Cell {
   delay: number
 }
 
-function getRandomArbitrary(min: number, max: number) {
+function rand(min: number, max: number) {
   return Math.random() * (max - min) + min
 }
 
-export function FancyProgressButton({ className, tileSize = 15, width = 300, height = 60, children }: Props) {
-  // grid of squares covering all the button
-  const grid = useMemo<Cell[]>(() => {
-    const cols = Math.floor(width / tileSize)
-    const rows = Math.floor(height / tileSize)
+export function FancyProgressButton({
+  className,
+  tileSize = 15,
+  width = 300,
+  height = 60,
+  speed = 8,
+  dispersion = 0.7,
+  colors = ['#25211E', '#66605C'], // bg80 , bg40
+  children,
+}: Props) {
+  const safeTileSize = tileSize || 1 // avoid dividing by zero
+  const [wave, setWave] = useState(0)
+
+  /** current and next wave colors */
+  const currentColor = colors[wave % colors.length]
+  const nextColor = colors[(wave + 1) % colors.length]
+
+  /** cell grid */
+  const { grid, lastIndex } = useMemo(() => {
+    const cols = Math.floor(width / safeTileSize)
+    const rows = Math.floor(height / safeTileSize)
+    // 1. Build the array of cells
     const cells: Cell[] = []
     for (let y = 0; y < rows; y++) {
       for (let x = 0; x < cols; x++) {
-        const delay = x / 8 + getRandomArbitrary(0, 0.5)
-        cells.push({ x, y, delay })
+        cells.push({ x, y, delay: x / speed + rand(0, dispersion) })
       }
     }
-    return cells
-  }, [tileSize, width, height])
+    // 2. Find the index of the cell with the maximum delay
+    const idxMax = cells.reduce(
+      (bestIdx, cell, idx, arr) => (cell.delay > arr[bestIdx].delay ? idx : bestIdx),
+      0,
+    )
+    return { grid: cells, lastIndex: idxMax }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [safeTileSize, width, height, speed, colors, wave, dispersion])
 
   return (
-    <button
-      className={cn('overflow-hidden rounded-md bg-neutral-800 border border-neutral-400', className)}
-      style={{
-        width: `${width}px`,
-        height: `${height}px`,
-      }}
+    <div
+      className={cn('relative overflow-hidden rounded-md border border-neutral-400', className)}
+      style={{ width, height, backgroundColor: currentColor }}
     >
       <svg width={width} height={height} viewBox={`0 0 ${width} ${height}`}>
         {grid.map(({ x, y, delay }, i) => (
           <motion.rect
-            key={i}
-            x={x * tileSize}
-            y={y * tileSize}
-            width={tileSize}
-            height={tileSize}
-            fill="#a1a1a1"
+            key={`${wave}-${i}`} // remount the grid by setting iteration to the key
+            x={x * safeTileSize}
+            y={y * safeTileSize}
+            width={safeTileSize * 1.1} // to avoid overlapping multiply by 1.1
+            height={safeTileSize * 1.1}
+            fill={nextColor}
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ delay, duration: 0.1 }}
-            className=""
+            onAnimationComplete={() => {
+              // run next wave after the last rect finishes animation
+              if (i === lastIndex) setWave(i => i + 1)
+            }}
           />
         ))}
       </svg>
-      <span>{children}</span>
-    </button>
+      <span className="absolute inset-0 flex items-center justify-center">{children}</span>
+    </div>
   )
 }
