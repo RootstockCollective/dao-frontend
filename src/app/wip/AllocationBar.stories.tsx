b77892d210a0886c988efbfb275e2b1e6f150202
Antonio <antonio@iovlabs.org>
2025-05-26 12:18:51 +0200

    feat: add an allocation bar component

---
+++ src/app/wip/AllocationBar.stories.tsx
@@ -0,0 +1,100 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import AllocationBar, { AllocationItem } from './AllocationBar'
+
+const meta = {
+  title: 'Components/AllocationBar',
+  component: AllocationBar,
+} satisfies Meta<typeof AllocationBar>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+const addUnallocated = (items: AllocationItem[]): AllocationItem[] => {
+  const allocatedSum = items.reduce((sum, { value }) => sum + value, 0)
+  if (allocatedSum >= 100) {
+    return items
+  }
+  return [
+    ...items,
+    {
+      key: 'uncallocated',
+      label: 'available funds',
+      value: 100 - allocatedSum,
+      color: 'bg-[#25211E]',
+      displayColor: '#25211E',
+    },
+  ]
+}
+
+const defaultItems: AllocationItem[] = [
+  {
+    key: 'boltz',
+    label: 'Boltz',
+    value: 20,
+    color: 'bg-purple-400',
+    displayColor: '#a78bfa', // Tailwind purple-400 hex
+  },
+  {
+    key: 'wallet',
+    label: '0x1D11...2D00',
+    value: 10,
+    color: 'bg-cyan-300',
+    displayColor: '#67e8f9', // Tailwind cyan-300 hex
+  },
+  {
+    key: 'abuilder',
+    label: 'another builder',
+    value: 10,
+    color: 'bg-yellow-300',
+    displayColor: '#DEFF1A', // Tailwind cyan-300 hex
+  },
+]
+const defaultItemsWithUnallocated = addUnallocated(defaultItems)
+
+export const Default: Story = {
+  args: {
+    itemsData: defaultItemsWithUnallocated,
+  },
+}
+
+export const WithoutPercent: Story = {
+  args: {
+    itemsData: defaultItemsWithUnallocated,
+    showPercent: false,
+  },
+}
+
+export const WithoutLegend: Story = {
+  args: {
+    itemsData: defaultItemsWithUnallocated,
+    showLegend: false,
+  },
+}
+
+export const WithoutLegendAndPercent: Story = {
+  args: {
+    itemsData: defaultItemsWithUnallocated,
+    showPercent: false,
+    showLegend: false,
+  },
+}
+
+const dataWithTemp: AllocationItem[] = [
+  ...defaultItems,
+  {
+    key: 'money-on-chain',
+    label: 'MoneyOnChain',
+    value: 35,
+    color: 'bg-green-400', // #4ade80
+    displayColor: '#4ade80', // Tailwind green-400 hex
+    isTemporary: true,
+  },
+]
+export const WithoutTemporaryValues: Story = {
+  args: {
+    itemsData: addUnallocated(dataWithTemp),
+    showPercent: true,
+    showLegend: true,
+  },
+}
