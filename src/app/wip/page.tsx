be4710194001a1e831527183c5acca700e614821
Antonio <antonio@iovlabs.org>
2025-05-27 16:37:16 +0200

    fix: lint

--- src/app/wip/page.tsx
+++ src/app/wip/page.tsx
@@ -1,7 +1,5 @@
 'use client'
-import { useState } from 'react'
 import AllocationBar, { AllocationItem } from './AllocationBar'
-import PortfolioBar from './AllocationBar2'
 // FIXME: to be removed
 export default function Wip() {
   const items: AllocationItem[] = [
@@ -36,31 +34,9 @@ export default function Wip() {
     },
   ]
 
-  const items2 = [
-    { key: 'a', label: 'BTC', color: 'bg-orange-400', displayColor: '#fb923c' },
-    { key: 'b', label: 'ETH', color: 'bg-purple-400', displayColor: '#a78bfa' },
-    { key: 'c', label: 'USDC', color: 'bg-cyan-300', displayColor: '#67e8f9' },
-  ]
-
-  const [allocations, setAllocations] = useState([30, 50, 20])
-  const [sortedItems, setSortedItems] = useState(items2)
-
   return (
     <>
       <AllocationBar initialItemsData={items} />
-
-      <PortfolioBar
-        initialValues={allocations}
-        initialItemsData={sortedItems}
-        onChange={(vals, items) => {
-          setAllocations(vals)
-          setSortedItems(items)
-        }}
-      />
-      <pre className="text-gray-300 mt-8">
-        Allocations: {JSON.stringify(allocations)} {'\n'}
-        Labels: {JSON.stringify(sortedItems.map(i => i.label))}
-      </pre>
     </>
   )
 }
