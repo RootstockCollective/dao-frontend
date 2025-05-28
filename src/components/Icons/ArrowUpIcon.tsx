6ed63c416b6426d1383e6199f0fe96edfbb968d0
DinizSa <diniz.araujosa@rootstocklabs.com>
2025-05-28 08:10:16 +0300

    refactor: create components from svgs

--- src/components/Icons/ArrowUpIcon.tsx
+++ src/components/Icons/ArrowUpIcon.tsx
@@ -12,12 +12,19 @@ export function ArrowUpIcon({
     <svg
       width={size}
       height={size}
-      viewBox="0 0 448 512"
-      fill={fill}
+      viewBox="0 0 16 16"
+      fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <path d="M34.9 289.5l-22.2-22.2c-9.4-9.4-9.4-24.6 0-33.9L207 39c9.4-9.4 24.6-9.4 33.9 0l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9L413 289.4c-9.5 9.5-25 9.3-34.3-.4L264 168.6V456c0 13.3-10.7 24-24 24h-32c-13.3 0-24-10.7-24-24V168.6L69.2 289.1c-9.3 9.8-24.8 10-34.3.4z" />
+      <path d="M8 13.5V2.5" stroke={color} strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
+      <path
+        d="M3.5 7L8 2.5L12.5 7"
+        stroke={color}
+        strokeWidth="1.25"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
     </svg>
   )
 }
