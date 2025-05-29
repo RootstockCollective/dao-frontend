520a9fbe8ea3d81e771481535c3c84837e6425a6
DinizSa <diniz.araujosa@rootstocklabs.com>
2025-05-29 10:51:48 +0300

    feat: add builder collapsible banner

---
+++ src/app/builders/components/BecomeBuilderBanner/BecomeBuilderBanner.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import BecomeBuilderCollapsible from './BecomeBuilderBanner'
+
+const meta: Meta<typeof BecomeBuilderCollapsible> = {
+  title: 'Builders/BecomeBuilderBanner',
+  component: BecomeBuilderCollapsible,
+  parameters: {
+    layout: 'fullscreen',
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof BecomeBuilderCollapsible>
+
+export const Default: Story = {}
