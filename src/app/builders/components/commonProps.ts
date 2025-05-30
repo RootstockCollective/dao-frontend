ab730b6ad97273a408362dac2becea740576deb8
Hakob23 <hakob.arshakyan230@gmail.com>
2025-05-30 06:40:30 +0300

    feat: Adding commonProps.ts to builders folder

---
+++ src/app/builders/components/commonProps.ts
@@ -0,0 +1,12 @@
+import { HTMLAttributes } from 'react'
+
+export type TestableComponentProps = {
+  dataTestid: string
+}
+
+export type StylableComponentProps<T extends Element> = {
+  className?: HTMLAttributes<T>['className']
+}
+
+export type CommonComponentProps<T extends Element = HTMLDivElement> = TestableComponentProps &
+  StylableComponentProps<T>
