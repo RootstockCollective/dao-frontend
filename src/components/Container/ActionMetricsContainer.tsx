7f157a399681ea7da6ec00bd6279b07fa8830985
Hakob23 <hakob.arshakyan230@gmail.com>
2025-06-02 18:49:55 +0300

    refactor: removing dataTestid

--- src/components/Container/ActionMetricsContainer.tsx
+++ src/components/Container/ActionMetricsContainer.tsx
@@ -4,14 +4,10 @@ import { CommonComponentProps } from '../commonProps'
 
 export type ActionMetricsContainerProps = CommonComponentProps & PropsWithChildren
 
-export const ActionMetricsContainer: FC<ActionMetricsContainerProps> = ({
-  dataTestid,
-  className,
-  children,
-}) => {
+export const ActionMetricsContainer: FC<ActionMetricsContainerProps> = ({ className = '', children }) => {
   return (
     <div
-      data-testid={`${dataTestid}_ActionsMetrics`}
+      data-testid={'ActionsMetrics'}
       className={cn('flex flex-col w-full items-start gap-2 rounded-sm p-6 bg-v3-bg-accent-80', className)}
     >
       <div className="flex items-start gap-14">{children}</div>
