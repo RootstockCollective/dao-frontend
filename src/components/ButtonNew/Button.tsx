702e7aa8da191824ea284a66c55355a4a6022e2a
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2025-06-03 18:26:44 -0300

    Add Secondary Outline Button variant

--- src/components/ButtonNew/Button.tsx
+++ src/components/ButtonNew/Button.tsx
@@ -5,7 +5,7 @@ import { Span } from '../TypographyNew'
 type ButtonVariant = 'primary' | 'secondary' | 'secondary-outline'
 
 const DEFAULT_CLASSES =
-  'relative overflow-hidden px-4 py-2 rounded-sm font-bold text-base transition-all duration-150 flex items-center justify-center'
+  'relative overflow-hidden px-4 py-2 rounded-md font-bold text-base transition-all duration-150 flex items-center justify-center gap-2'
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: ButtonVariant
@@ -22,12 +22,9 @@ export const Button: FC<Props> = ({
   ...props
 }) => {
   const styles = {
-    primary:
-      'bg-primary text-bg-100 border border-primary disabled:bg-disabled-primary disabled:border-transparent',
-    secondary:
-      'bg-bg-100 text-text-100 border border-bg-100 disabled:opacity-80 disabled:border-disabled-primary disabled:border-opacity-80',
-    'secondary-outline':
-      'bg-transparent text-text-100 border border-bg-0 disabled:opacity-50 disabled:border-disabled-primary disabled:border-opacity-50',
+    primary: 'bg-primary text-bg-100 disabled:bg-disabled-primary',
+    secondary: 'bg-bg-100 text-text-100 border-0 disabled:opacity-80',
+    'secondary-outline': 'bg-transparent text-text-100 border border-bg-0 disabled:opacity-50',
   }
 
   return (
