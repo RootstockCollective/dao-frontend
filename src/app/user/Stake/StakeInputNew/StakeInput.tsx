afe124cef27295852a19ccc56d7918358c51d1a8
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2025-06-02 21:31:16 -0300

    fix mobile style

--- src/app/user/Stake/StakeInputNew/StakeInput.tsx
+++ src/app/user/Stake/StakeInputNew/StakeInput.tsx
@@ -4,6 +4,7 @@ import { Paragraph } from '@/components/TypographyNew'
 import { variantClasses } from '@/components/TypographyNew/Typography'
 import { cn } from '@/lib/utils'
 import Image from 'next/image'
+import { ReactNode } from 'react'
 
 interface Props {
   onChange: (value: string) => void
