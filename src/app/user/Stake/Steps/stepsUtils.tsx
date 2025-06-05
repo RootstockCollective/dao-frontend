548805d0fc25f147645bc99bde1db0a6e3a8f373
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2025-06-05 00:01:56 -0300

    Update button text and data-testid in StepOne for improved action clarity

--- src/app/user/Stake/Steps/stepsUtils.tsx
+++ src/app/user/Stake/Steps/stepsUtils.tsx
@@ -2,9 +2,11 @@ export const textsDependingOnAction = {
   STAKE: {
     inputLabel: 'Amount to stake',
     amountError: 'This is more than the available RIF balance. Please update the amount.',
+    buttonText: 'Continue',
   },
   UNSTAKE: {
     inputLabel: 'Unstake amount',
     amountError: 'This is more than the available stRIF balance. Please update the amount.',
+    buttonText: 'Unstake',
   },
 }
