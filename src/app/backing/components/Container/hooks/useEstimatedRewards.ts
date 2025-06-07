0beca258d93fbf8fa18d25b477511e13a70a6df7
DinizSa <diniz.araujosa@rootstocklabs.com>
2025-06-07 23:47:13 +0100

    refactor: optimize reward calculation

--- src/app/backing/components/Container/hooks/useEstimatedRewards.ts
+++ src/app/backing/components/Container/hooks/useEstimatedRewards.ts
@@ -1,26 +1,31 @@
 import { BuildersRewards } from '@/app/collective-rewards/rewards/builders/hooks/useGetBuildersRewards'
-import { getCombinedFiatAmount } from '@/app/collective-rewards/utils/getCombinedFiatAmount'
+import { getFiatAmount } from '@/app/collective-rewards/utils/getCombinedFiatAmount'
+import Big from '@/lib/big'
 
 export function useEstimatedRewards(rewardsData: BuildersRewards[]) {
-  let totalEstimatedRif = 0n
-  let totalEstimatedRbtc = 0n
-  let totalEstimatedUsd = 0
-
-  if (rewardsData && rewardsData.length > 0) {
-    totalEstimatedRif = rewardsData.reduce(
-      (acc, builder) => acc + (builder.estimatedRewards.rif.amount.value ?? 0n),
-      0n,
-    )
-    totalEstimatedRbtc = rewardsData.reduce(
-      (acc, builder) => acc + (builder.estimatedRewards.rbtc.amount.value ?? 0n),
-      0n,
-    )
-    const allRewardAmounts = rewardsData.flatMap(builder => [
-      builder.estimatedRewards.rif.amount,
-      builder.estimatedRewards.rbtc.amount,
-    ])
-    totalEstimatedUsd = getCombinedFiatAmount(allRewardAmounts).toNumber()
+  if (!rewardsData?.length) {
+    return { totalEstimatedRif: 0n, totalEstimatedRbtc: 0n, totalEstimatedUsd: 0 }
   }
 
-  return { totalEstimatedRif, totalEstimatedRbtc, totalEstimatedUsd }
+  const { totalEstimatedRif, totalEstimatedRbtc, totalEstimatedUsd } = rewardsData.reduce(
+    (acc, builder) => {
+      const rifAmount = builder.estimatedRewards.rif.amount.value ?? 0n
+      const rbtcAmount = builder.estimatedRewards.rbtc.amount.value ?? 0n
+
+      return {
+        totalEstimatedRif: acc.totalEstimatedRif + rifAmount,
+        totalEstimatedRbtc: acc.totalEstimatedRbtc + rbtcAmount,
+        totalEstimatedUsd: acc.totalEstimatedUsd
+          .add(getFiatAmount(builder.estimatedRewards.rif.amount))
+          .add(getFiatAmount(builder.estimatedRewards.rbtc.amount)),
+      }
+    },
+    { totalEstimatedRif: 0n, totalEstimatedRbtc: 0n, totalEstimatedUsd: Big(0) },
+  )
+
+  return {
+    totalEstimatedRif,
+    totalEstimatedRbtc,
+    totalEstimatedUsd: totalEstimatedUsd.toNumber(),
+  }
 }
