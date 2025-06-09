e59f1e0f117a48efe369fbbe383346a0f7af77ea
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2025-06-09 15:33:34 -0300

    Fix staking step visualization

--- src/app/user/Stake/Steps/StakeSteps.tsx
+++ src/app/user/Stake/Steps/StakeSteps.tsx
@@ -9,11 +9,11 @@ export const StakeSteps = ({ currentStep }: Props) => (
   <div className="flex justify-between items-center">
     <Span variant="tag">SELECT AMOUNT</Span>
     <CaretRight />
-    <Span variant="tag" className={currentStep >= 2 ? '' : 'text-text-60'}>
+    <Span variant="tag" className={currentStep >= 1 ? '' : 'text-text-60'}>
       REQUEST ALLOWANCE
     </Span>
     <CaretRight />
-    <Span variant="tag" className={currentStep >= 3 ? '' : 'text-text-60'}>
+    <Span variant="tag" className={currentStep >= 2 ? '' : 'text-text-60'}>
       CONFIRM STAKE
     </Span>
   </div>
