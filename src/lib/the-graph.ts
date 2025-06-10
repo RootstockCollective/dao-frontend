4eb10dc6a558cbe0d17a0a4e790c29bd613b838a
Juraj <juraj@iovlabs.org>
2025-06-10 18:16:43 +0200

    TOK-727: get abi from graph (#981)
    * feat: use apollo client
    
    * fix: replace api with SSR
    
    * fix: remove apollo provider
    
    * chore: add a comment for the RBI
    
    * fix: replace axios in RBI
    
    * fix: apply PR suggestions
    
    * feat: enables user flags on qa and local
    
    * build: enable the feature flag qa
    
    * fix: import
    
    * chore: remove unused comments
    
    * fix: set env variables to QA
    
    ---------
    
    Co-authored-by: Antonio <antonio@iovlabs.org>
    Co-authored-by: Francisco Tobar <francisco.tobar@iovlabs.org>

---
+++ src/lib/the-graph.ts
@@ -0,0 +1,2 @@
+// IMPORTANT: we don't expose the key to the client
+export const fetchCrTheGraphEndpoint = `${process.env.THE_GRAPH_URL}/${process.env.THE_GRAPH_API_KEY}/${process.env.THE_GRAPH_ID}`
