ee956611f4584d6636b0981060a3b013381784aa
DinizSa <diniz.araujosa@rootstocklabs.com>
2025-06-13 00:17:39 +0100

    fix: propagate icon color

--- src/components/Icons/MoreIcon.tsx
+++ src/components/Icons/MoreIcon.tsx
@@ -1,11 +1,10 @@
 import { type IconProps } from './types'
-import { DEFAULT_ICON_COLOR } from './constants'
+import { DEFAULT_ICON_COLOR, DEFAULT_ICON_SIZE } from './constants'
 
 export function MoreIcon({
   'aria-label': ariaLabel = 'More Icon',
-  size = 24,
+  size = DEFAULT_ICON_SIZE,
   color = DEFAULT_ICON_COLOR,
-  fill = color,
   ...props
 }: IconProps) {
   return (
@@ -20,17 +19,17 @@ export function MoreIcon({
       <g clipPath="url(#clip0_3855_5851)">
         <path
           d="M12 4.1875C12.7939 4.1875 13.4375 4.83109 13.4375 5.625C13.4375 6.41891 12.7939 7.0625 12 7.0625C11.2061 7.0625 10.5625 6.41891 10.5625 5.625C10.5625 4.83109 11.2061 4.1875 12 4.1875Z"
-          fill="white"
-          stroke="white"
+          fill={color}
+          stroke={color}
           strokeWidth="0.125"
         />
         <path
           d="M12 13.5C12.8284 13.5 13.5 12.8284 13.5 12C13.5 11.1716 12.8284 10.5 12 10.5C11.1716 10.5 10.5 11.1716 10.5 12C10.5 12.8284 11.1716 13.5 12 13.5Z"
-          fill="white"
+          fill={color}
         />
         <path
           d="M12 19.875C12.8284 19.875 13.5 19.2034 13.5 18.375C13.5 17.5466 12.8284 16.875 12 16.875C11.1716 16.875 10.5 17.5466 10.5 18.375C10.5 19.2034 11.1716 19.875 12 19.875Z"
-          fill="white"
+          fill={color}
         />
       </g>
       <defs>
