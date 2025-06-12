0c10ffce5802175fd54f2893e6e32a4a802031d7
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2025-06-12 08:59:24 -0300

    Add JSDoc comments for useContractWrite hook and related interfaces

--- src/app/user/Stake/hooks/useContractWrite.tsx
+++ src/app/user/Stake/hooks/useContractWrite.tsx
@@ -3,6 +3,14 @@ import { Address, Hash } from 'viem'
 import { useWriteContract } from 'wagmi'
 import { useTransactionStatus } from './useTransactionStatus'
 
+/**
+ * Configuration for a contract write operation.
+ * @interface ContractWriteConfig
+ * @property {any} abi - The ABI of the contract
+ * @property {Address} address - The address of the contract
+ * @property {string} functionName - The name of the function to call
+ * @property {any[]} args - The arguments to pass to the function
+ */
 interface ContractWriteConfig {
   abi: any
   address: Address
@@ -10,6 +18,16 @@ interface ContractWriteConfig {
   args: any[]
 }
 
+/**
+ * Result of the useContractWrite hook.
+ * @interface UseContractWriteResult
+ * @template T - The type of the transaction request function
+ * @property {T} onRequestTransaction - Function to execute the contract write
+ * @property {boolean} isRequesting - Whether the transaction is being requested (ex: user wallet interaction)
+ * @property {boolean} isTxPending - Whether the transaction is pending confirmation
+ * @property {boolean} isTxFailed - Whether the transaction has failed
+ * @property {Hash | undefined} txHash - The hash of the transaction, if any
+ */
 interface UseContractWriteResult<T> {
   onRequestTransaction: T
   isRequesting: boolean
@@ -18,6 +36,24 @@ interface UseContractWriteResult<T> {
   txHash: Hash | undefined
 }
 
+/**
+ * A hook that provides a standardized way to interact with smart contracts for write operations.
+ * It handles the contract write execution and transaction status tracking.
+ *
+ * @template T - The type of the transaction request function
+ * @param {ContractWriteConfig} config - The configuration for the contract write operation
+ * @returns {UseContractWriteResult<T>} An object containing the transaction request function and status flags
+ *
+ * @example
+ * ```tsx
+ * const { onRequestTransaction, isRequesting, isTxPending, isTxFailed, txHash } = useContractWrite({
+ *   abi: tokenABI,
+ *   address: tokenAddress,
+ *   functionName: 'approve',
+ *   args: [spenderAddress, amount]
+ * });
+ * ```
+ */
 export const useContractWrite = <T extends () => Promise<Hash>>(
   config: ContractWriteConfig,
 ): UseContractWriteResult<T> => {
