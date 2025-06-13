70bf6909e82f5c28bb7b230225ad85be548d009a
Alexander Evchenko <alexivchenko9@gmail.com>
2025-06-13 18:57:01 +0400

    revert: Typography changes since it has been deprecated

--- src/components/Typography/types.ts
+++ src/components/Typography/types.ts
@@ -1,4 +1,4 @@
-export type TypographyTagVariants = 'h1' | 'h2' | 'h3' | 'p' | 'span' | 'label'
+export type TypographyTagVariants = 'h1' | 'h2' | 'p' | 'span' | 'label'
 
 export type LabelVariants = 'normal' | 'semibold' | 'light'
 
