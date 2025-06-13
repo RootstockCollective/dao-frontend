e0cf13a86a3f8f90a9e2088cd67da4716f443cb5
DinizSa <diniz.araujosa@rootstocklabs.com>
2025-06-13 13:23:27 +0100

    chore: improve dropdown component name

---
+++ src/app/builders/components/TableColumnDropdown.stories.tsx
@@ -0,0 +1,10 @@
+import { TableColumnDropdown } from './TableColumnDropdown'
+
+export default {
+  title: 'Builders/TableColumnDropdown',
+  component: TableColumnDropdown,
+}
+
+export const Basic = () => (
+  <TableColumnDropdown className="flex justify-end items-start p-10 min-h-[200px] w-[500px] relative" />
+)
