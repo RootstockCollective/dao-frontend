94d2e90485a09b3ea40b7e67088486c4a6211f0b
DinizSa <diniz.araujosa@rootstocklabs.com>
2025-06-16 16:48:04 +0100

    chore: revert new button changes

--- src/components/ButtonNew/Button.tsx
+++ src/components/ButtonNew/Button.tsx
@@ -2,17 +2,14 @@ import { cn } from '@/lib/utils'
 import { FC, ButtonHTMLAttributes } from 'react'
 import { Span } from '../TypographyNew'
 
-type ButtonVariant = 'primary' | 'secondary' | 'secondary-outline' | 'pagination'
+type ButtonVariant = 'primary' | 'secondary' | 'secondary-outline'
 
 const DEFAULT_CLASSES =
   'relative overflow-hidden px-4 py-2 rounded-sm font-bold text-base transition-all duration-150 flex items-center justify-center'
 
-const COMMON_TEXT_CLASSES = 'text-text-100'
-
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: ButtonVariant
   'data-testid'?: string
-  textClassName?: string
 }
 
 export const Button: FC<Props> = ({
@@ -21,7 +18,6 @@ export const Button: FC<Props> = ({
   disabled = false,
   onClick,
   className = '',
-  textClassName = '',
   'data-testid': dataTestId,
   ...props
 }) => {
@@ -32,15 +28,6 @@ export const Button: FC<Props> = ({
       'bg-bg-100 text-text-100 border border-bg-100 disabled:opacity-80 disabled:border-disabled-primary disabled:border-opacity-80',
     'secondary-outline':
       'bg-transparent text-text-100 border border-bg-0 disabled:opacity-50 disabled:border-disabled-primary disabled:border-opacity-50',
-    pagination:
-      'bg-transparent text-text-100 border border-v3-bg-accent-40 disabled:opacity-50 disabled:border-disabled-primary disabled:border-opacity-50 px-2 py-1',
-  }
-
-  const textStyles = {
-    primary: 'text-bg-100',
-    secondary: cn(COMMON_TEXT_CLASSES, 'font-bold'),
-    'secondary-outline': cn(COMMON_TEXT_CLASSES, 'font-bold'),
-    pagination: cn(COMMON_TEXT_CLASSES, 'font-normal text-[14px]'),
   }
 
   return (
@@ -53,7 +40,7 @@ export const Button: FC<Props> = ({
       // All props must be forwarded to the underlying component to ensure that wrapping components (like Tooltip) function correctly.
       {...props}
     >
-      <Span className={cn(textStyles[variant], textClassName)}>{children}</Span>
+      <Span bold>{children}</Span>
     </button>
   )
 }
