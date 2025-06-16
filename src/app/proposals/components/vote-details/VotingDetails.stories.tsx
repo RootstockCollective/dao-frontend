3a5a499ee61f8dca6473ae6ff21e4057e57f7c3f
Alexander Evchenko <alexivchenko9@gmail.com>
2025-06-16 17:10:50 +0400

    test: add more VotingDetails stories

--- src/app/proposals/components/vote-details/VotingDetails.stories.tsx
+++ src/app/proposals/components/vote-details/VotingDetails.stories.tsx
@@ -14,16 +14,72 @@ type Story = StoryObj<typeof VotingDetails>
 export const Default: Story = {
   args: {
     voteData: {
-      for: '99,999,999',
-      against: '99,999,999',
+      for: '77,999,999',
+      against: '99,333,999',
+      abstain: '99,77,999',
+      quorum: '55,456,789',
+    },
+
+    votingPower: '4,300',
+    buttonAction: {
+      onButtonClick: e => console.log('Allegedly Voted for proposal', e),
+      actionName: 'Vote proposal',
+    },
+  },
+}
+
+export const HasVotedFor: Story = {
+  args: {
+    voteData: {
+      for: '11,999,111',
+      against: '99,444,999',
       abstain: '99,999,999',
-      quorum: '123,456,789',
+      quorum: '564,456,111',
     },
     votingPower: '4,300',
     buttonAction: {
-      onButtonClick: () => {},
-      actionName: 'Vote proposal',
+      onButtonClick: e => console.log('Allegedly call Put on queue'),
+      actionName: 'Put on queue',
     },
-    // hasVoted: 'for',
+    hasVoted: 'for',
+  },
+}
+
+export const HasVotedButNoActionAvailable: Story = {
+  args: {
+    voteData: {
+      for: '8,999,999',
+      against: '99,800,999',
+      abstain: '99,999,555',
+      quorum: '35,35,789',
+    },
+    votingPower: '4,300',
+    hasVoted: 'for',
+  },
+}
+
+export const HasVotedAgainst: Story = {
+  args: {
+    voteData: {
+      for: '99,444,999',
+      against: '99,444,999',
+      abstain: '99,67,890',
+      quorum: '777,456,789',
+    },
+    votingPower: '4,300',
+    hasVoted: 'against',
+  },
+}
+
+export const HasVotedAbstain: Story = {
+  args: {
+    voteData: {
+      for: '11,999,999',
+      against: '99,999,777',
+      abstain: '99,555,999',
+      quorum: '333,444,789',
+    },
+    votingPower: '4,300',
+    hasVoted: 'abstain',
   },
 }
