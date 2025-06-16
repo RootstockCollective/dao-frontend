a3dbc9feafca4ec8a5c17add78f07aac42f53b90
Francis Rodriguez <francisngg@gmail.com>
2025-06-16 11:20:11 -0400

    - Fixed font weight

--- src/shared/walletConnection/components/DisconnectButton.tsx
+++ src/shared/walletConnection/components/DisconnectButton.tsx
@@ -2,8 +2,13 @@ import { Button } from '@/components/ButtonNew/Button'
 import { PowerIconKoto } from '@/components/Icons'
 
 export const DisconnectButton = () => (
-  <Button data-testid="ButtonDisconnect" variant="secondary-outline" className="px-2 py-1 border-bg-40">
-    <div className="flex flex-row gap-1 items-center text-[14px] font-normal">
+  <Button
+    data-testid="ButtonDisconnect"
+    variant="secondary-outline"
+    className="px-2 py-1 border-bg-40"
+    textClassName="font-normal"
+  >
+    <div className="flex flex-row gap-1 items-center text-[14px]">
       <PowerIconKoto /> Disconnect wallet
     </div>
   </Button>
