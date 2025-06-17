d99f12d4e38ae3b5b035651f7da7c13711c34565
Francis Rodriguez <francisngg@gmail.com>
2025-06-17 10:40:02 -0400

    - Will now show address on mobile version

--- src/shared/walletConnection/components/DisconnectButton.tsx
+++ src/shared/walletConnection/components/DisconnectButton.tsx
@@ -1,12 +1,17 @@
 import { Button } from '@/components/ButtonNew/Button'
 import { PowerIconKoto } from '@/components/Icons'
 
-export const DisconnectButton = () => (
+interface DisconnectButtonProps {
+  onClick: () => void
+}
+
+export const DisconnectButton = ({ onClick }: DisconnectButtonProps) => (
   <Button
     data-testid="ButtonDisconnect"
     variant="secondary-outline"
     className="px-2 py-1 border-bg-40"
     textClassName="font-normal"
+    onClick={onClick}
   >
     <div className="flex flex-row gap-1 items-center text-[14px]">
       <PowerIconKoto /> Disconnect wallet
