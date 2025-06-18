716e8b1422dcf4993ef31fcc8e179d4bac6514ee
LordCheta <cmaduabuchi@live.com>
2025-06-17 23:15:18 -0700

    fix: correct import for toast component

--- src/config/ledgerConnector.ts
+++ src/config/ledgerConnector.ts
@@ -1,6 +1,6 @@
 import { createConnector } from 'wagmi'
 import { Chain, Address, UserRejectedRequestError, SwitchChainError } from 'viem'
-import { showToast, ToastAlertOptions } from '../shared/lib/toastUtils'
+import { showToast, ToastAlertOptions } from '@/shared/notification'
 
 export interface LedgerConnectorOptions {
   chainId?: number
