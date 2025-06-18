6ae923537397adbc85775ec980071db14a9c742b
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2025-06-18 05:13:10 -0300

    Refactor IntroModal to load multiple images before opening and improve layout structure

--- src/app/user/IntroModal/IntroModal.tsx
+++ src/app/user/IntroModal/IntroModal.tsx
@@ -2,7 +2,7 @@ import { Button } from '@/components/ButtonNew'
 import { ArrowUpRightLightIcon } from '@/components/Icons'
 import { ArrowRightIcon } from '@/components/Icons/ArrowRightIcon'
 import { Modal } from '@/components/Modal'
-import { Label, Header, Paragraph, Span } from '@/components/TypographyNew'
+import { Header, Label, Paragraph, Span } from '@/components/TypographyNew'
 import { cn } from '@/lib/utils'
 import { useModal } from '@/shared/hooks/useModal'
 import Image from 'next/image'
@@ -11,21 +11,38 @@ import { useEffect } from 'react'
 const GLASS_STYLE =
   'rounded bg-[rgba(255,255,255,0.16)] shadow-[inset_0px_0px_14px_0px_rgba(255,255,255,0.25)] backdrop-blur-[3px]'
 
+const IMAGE_PATHS = ['/images/intro/rbtc-rif-bg.svg', '/images/intro/rbtc-rif-squares-desktop.svg']
+
 export const IntroModal = () => {
   const introModal = useModal()
   useEffect(() => {
-    const image = new window.Image()
-    image.src = '/images/intro/rbtc-rif-bg.svg'
-    image.onload = () => {
-      introModal.openModal()
+    let loadedCount = 0
+    const handleLoad = () => {
+      if (++loadedCount === IMAGE_PATHS.length) {
+        introModal.openModal()
+      }
     }
+
+    IMAGE_PATHS.forEach(path => {
+      const image = new window.Image()
+      image.src = path
+      image.onload = handleLoad
+    })
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
     introModal.isModalOpened && (
       <Modal width={920} onClose={introModal.closeModal} closeButtonColor="black">
-        <div className="flex flex-row p-4 bg-text-80 gap-6">
+        <div className="flex flex-row p-4 bg-text-80 gap-6 relative">
+          <Image
+            src="/images/intro/rbtc-rif-squares-desktop.svg"
+            alt="Squares Divider"
+            width={40}
+            height={30}
+            className="absolute left-1/2 top-8 -translate-x-1/2 z-10"
+            style={{ pointerEvents: 'none' }}
+          />
           <div className="flex-1">
             <div className="relative">
               <Image
@@ -70,17 +87,19 @@ export const IntroModal = () => {
             </div>
           </div>
           <div className="flex-1 flex flex-col justify-between">
-            <div className="flex flex-1 flex-col justify-center">
+            <div className="flex flex-1 flex-col justify-center gap-4">
               <Label variant="tag" className="text-bg-100" caps>
                 STAKE
               </Label>
-              <Header variant="e2" className="text-bg-20" caps>
-                Before you stake
-              </Header>
-              <Header variant="e2" className="text-bg-100" caps>
-                add RBTC & RIF to your wallet
-              </Header>
-              <Paragraph className="text-bg-100 mt-4">
+              <div>
+                <Header variant="e2" className="text-bg-20" caps>
+                  Before you stake
+                </Header>
+                <Header variant="e2" className="text-bg-100" caps>
+                  add RBTC & RIF to your wallet
+                </Header>
+              </div>
+              <Paragraph className="text-bg-100">
                 RIF is the token required for staking, and RBTC is used to cover transaction fees. You&apos;ll
                 need both to participate in the DAO.
               </Paragraph>
