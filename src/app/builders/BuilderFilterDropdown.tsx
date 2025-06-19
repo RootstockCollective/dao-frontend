6bbbdc1ca7f421bb0f978fa2fb1fb586522845f7
DinizSa <diniz.araujosa@rootstocklabs.com>
2025-06-19 13:52:03 +0100

    chore: imporve naming and props

--- src/app/builders/BuilderFilterDropdown.tsx
+++ src/app/builders/BuilderFilterDropdown.tsx
@@ -9,7 +9,7 @@ import {
 
 export interface BuilderFilterDropdownProps {
   className?: string
-  onSelected?: (option: { id: string; content: ReactNode }) => void
+  onSelected: (option: { id: string; content: ReactNode }) => void
 }
 
 const BuilderFilterOptions = [
@@ -19,23 +19,22 @@ const BuilderFilterOptions = [
 ]
 
 export const BuilderFilterDropdown: FC<BuilderFilterDropdownProps> = ({ className, onSelected }) => {
-  const [value, setValue] = useState<string>(BuilderFilterOptions[0].id)
+  const [selectedOptionId, setSelectedOptionId] = useState<string>(BuilderFilterOptions[0].id)
 
   useEffect(() => {
-    const selected = BuilderFilterOptions.find(opt => opt.id === value)
-    if (selected && onSelected) {
+    const selected = BuilderFilterOptions.find(opt => opt.id === selectedOptionId)
+    if (selected) {
       onSelected(selected)
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [value])
+  }, [selectedOptionId, onSelected])
 
   return (
-    <Dropdown value={value} onValueChange={setValue}>
+    <Dropdown value={selectedOptionId} onValueChange={setSelectedOptionId}>
       <DropdownTrigger className={className} data-testid="dropdown-trigger">
         <DropdownValue />
       </DropdownTrigger>
       <DropdownContent>
-        {dummyOptions.map(opt => (
+        {BuilderFilterOptions.map(opt => (
           <DropdownItem key={opt.id} value={opt.id} data-testid={`dropdown-option-${opt.id}`}>
             {opt.content}
           </DropdownItem>
