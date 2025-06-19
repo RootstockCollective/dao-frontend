2753140a1be0470ddcccca36a29656f420fe4fba
DinizSa <diniz.araujosa@rootstocklabs.com>
2025-06-19 09:59:36 +0100

    feat: add builder filter dropdown

---
+++ src/app/builders/BuilderFilterDropdown.tsx
@@ -0,0 +1,48 @@
+import { FC, ReactNode, useEffect, useState } from 'react'
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownContent,
+  DropdownItem,
+  DropdownValue,
+} from '../../components/SingleSelectDropdown/SingleSelectDropdown'
+
+export interface BuilderFilterDropdownProps {
+  className?: string
+  onSelected?: (option: { id: string; content: ReactNode }) => void
+}
+
+const dummyOptions = [
+  { id: 'active', content: 'Active builders' },
+  { id: 'inactive', content: 'Inactive builders' },
+  { id: 'all', content: 'All builders' },
+]
+
+export const BuilderFilterDropdown: FC<BuilderFilterDropdownProps> = ({ className, onSelected }) => {
+  const [value, setValue] = useState<string>(dummyOptions[0].id)
+
+  useEffect(() => {
+    const selected = dummyOptions.find(opt => opt.id === value)
+    if (selected && onSelected) {
+      onSelected(selected)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [value])
+
+  return (
+    <Dropdown value={value} onValueChange={setValue}>
+      <DropdownTrigger className={className} data-testid="dropdown-trigger">
+        <DropdownValue />
+      </DropdownTrigger>
+      <DropdownContent>
+        {dummyOptions.map(opt => (
+          <DropdownItem key={opt.id} value={opt.id} data-testid={`dropdown-option-${opt.id}`}>
+            {opt.content}
+          </DropdownItem>
+        ))}
+      </DropdownContent>
+    </Dropdown>
+  )
+}
+
+export default BuilderFilterDropdown
