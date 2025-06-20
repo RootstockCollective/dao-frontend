c0b70b4eb839cdd168344f33a90e50d7618a7a09
Antonio <antonio@iovlabs.org>
2025-06-20 14:41:09 +0200

    fix: vertical alignment

--- src/app/backing/components/TotalBackingMetric/TotalBackingMetric.tsx
+++ src/app/backing/components/TotalBackingMetric/TotalBackingMetric.tsx
@@ -24,15 +24,13 @@ export const TotalBackingMetric = ({ totalBacking }: TotalBackingMetricProps) =>
           <div className="flex flex-col items-start gap-2 self-stretch">
             <div className="flex h-10 items-center gap-6">
               <div className="flex items-center gap-2">
-                <div className="pt-2 items-center">
-                  <Typography
-                    variant="h1"
-                    caps
-                    className="text-white overflow-hidden text-ellipsis line-clamp-1 font-kk-topo text-[32px] font-normal leading-[125%]"
-                  >
-                    {totalBacking}
-                  </Typography>
-                </div>
+                <Typography
+                  variant="h1"
+                  caps
+                  className="text-white overflow-hidden text-ellipsis line-clamp-1 font-kk-topo text-[32px] font-normal leading-[125%]"
+                >
+                  {totalBacking}
+                </Typography>
                 <div className="flex py-2 pl-2 pr-0 items-center gap-1 rounded">
                   <TokenImage symbol={stRIF} size={24} />
                   <Typography variant="body-l" bold className="text-white text-right">
