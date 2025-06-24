1aa53ba3ef730d473ad27cbef84287ab77f28e31
DinizSa <diniz.araujosa@rootstocklabs.com>
2025-06-24 12:48:13 +0100

    chore: add selector icons

---
+++ src/components/Icons/CheckPriority.tsx
@@ -0,0 +1,35 @@
+import { type IconProps } from './types'
+import { DEFAULT_ICON_COLOR, DEFAULT_ICON_SIZE } from './constants'
+
+export function CheckPriorityIcon({
+  'aria-label': ariaLabel = 'Check Priority Icon',
+  size = DEFAULT_ICON_SIZE,
+  color = DEFAULT_ICON_COLOR,
+  fill = 'none',
+  className = '',
+  ...props
+}: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill={fill}
+      aria-label={ariaLabel}
+      {...props}
+    >
+      <path
+        d="M10.2472 17L18 9.05889L16.9662 8L10.2472 14.8822L7.03378 11.5907L6 12.6496L10.2472 17Z"
+        fill={color}
+      />
+      <path
+        d="M18 9.05889L10.2472 17L6 12.6496L7.03378 11.5907L10.2472 14.8822L16.9662 8L18 9.05889Z"
+        fill={color}
+      />
+    </svg>
+  )
+}
+
+export default CheckPriorityIcon
---
+++ src/components/Icons/Elipse.tsx
@@ -0,0 +1,28 @@
+import { type IconProps } from './types'
+import { DEFAULT_ICON_COLOR, DEFAULT_ICON_SIZE } from './constants'
+
+export function ElipseIcon({
+  'aria-label': ariaLabel = 'Elipse Icon',
+  size = 28,
+  color = DEFAULT_ICON_COLOR,
+  fill = 'none',
+  className = '',
+  ...props
+}: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      viewBox="0 0 28 28"
+      width={size}
+      height={size}
+      fill={fill}
+      aria-label={ariaLabel}
+      {...props}
+    >
+      <circle cx="14" cy="14" r="13.5" fill={fill} stroke={color} />
+    </svg>
+  )
+}
+
+export default ElipseIcon
