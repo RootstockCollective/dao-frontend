8c13d4b4f21bc14f3291657a8a2e030631ffc8a8
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2025-06-25 18:26:29 -0300

    Remove redundant useIsMobile

--- src/shared/hooks/useIsMobile.ts
+++
@@ -1,9 +0,0 @@
-import { useMediaQuery } from 'react-responsive'
-import { MOBILE_DESKTOP_BREAKPOINT } from '@/lib/constants'
-
-/**
- * Hook to check if the screen is mobile.
- */
-export function useIsMobile() {
-  return useMediaQuery({ maxWidth: MOBILE_DESKTOP_BREAKPOINT - 1 })
-}
