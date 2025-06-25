17709b1eea75a0a45863a9d18a5f6cc7cdb51904
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2025-06-25 00:11:55 -0300

    RBTC Intro Modal

--- src/app/user/IntroModal/IntroModal.tsx
+++ src/app/user/IntroModal/IntroModal.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable quotes */
 import { Button } from '@/components/ButtonNew'
 import { ArrowUpRightLightIcon } from '@/components/Icons'
 import { ArrowRightIcon } from '@/components/Icons/ArrowRightIcon'
@@ -15,6 +16,11 @@ const RBTC_RIF_BG_DESKTOP_SVG = '/images/intro/rbtc-rif-bg-desktop.svg'
 const RBTC_RIF_BG_MOBILE_SVG = '/images/intro/rbtc-rif-bg-mobile.svg'
 const RBTC_RIF_SQUARES_DESKTOP_SVG = '/images/intro/rbtc-rif-squares-desktop.svg'
 const RBTC_RIF_SQUARES_MOBILE_SVG = '/images/intro/rbtc-rif-squares-mobile.svg'
+
+const RBTC_BG_DESKTOP_SVG = '/images/intro/rbtc-bg-desktop.svg'
+const RBTC_BG_MOBILE_SVG = '/images/intro/rbtc-bg-mobile.svg'
+const RBTC_SQUARES_DESKTOP_SVG = '/images/intro/rbtc-squares-desktop.svg'
+const RBTC_SQUARES_MOBILE_SVG = '/images/intro/rbtc-squares-mobile.svg'
 
 const IMAGE_PATHS = [
   RBTC_RIF_BG_DESKTOP_SVG,
@@ -68,68 +74,124 @@ export const IntroModal = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isLoaded])
 
+  const status = 2
   return (
     introModal.isModalOpened &&
     isLoaded && (
       <Modal width={920} onClose={introModal.closeModal} closeButtonColor="black" className="bg-text-80">
         <div className="flex flex-col md:flex-row p-4 md:gap-6 relative">
-          {isMobile ? (
-            <div className="mt-12 flex flex-col gap-4">
-              <div className="relative">
-                <Image
-                  src={RBTC_RIF_BG_MOBILE_SVG}
-                  alt="Intro Modal"
-                  height={0}
-                  width={0}
-                  className="h-auto w-full"
-                />
-                <Image
-                  src={RBTC_RIF_SQUARES_MOBILE_SVG}
-                  alt="Squares Divider"
-                  width={40}
-                  height={30}
-                  className="absolute bottom-[-40px] right-0"
-                />
-              </div>
-              <StakeDescription />
-              <ContinueButton className="mt-12" />
-            </div>
-          ) : (
+          {status === 1 && (
             <>
-              <Image
-                src={RBTC_RIF_SQUARES_DESKTOP_SVG}
-                alt="Squares Divider"
-                width={40}
-                height={30}
-                className="absolute block left-1/2 top-8 -translate-x-[calc(55%)] z-10"
-              />
-              <div className="flex-1 relative">
+              {isMobile ? (
+                <div className="mt-12 flex flex-col gap-4">
+                  <div className="relative">
+                    <Image
+                      src={RBTC_RIF_BG_MOBILE_SVG}
+                      alt="Intro Modal"
+                      height={0}
+                      width={0}
+                      className="h-auto w-full"
+                    />
+                    <Image
+                      src={RBTC_RIF_SQUARES_MOBILE_SVG}
+                      alt="Squares Divider"
+                      width={40}
+                      height={30}
+                      className="absolute bottom-[-40px] right-0"
+                    />
+                  </div>
+                  <StakeDescription status={status} />
+                  <ContinueButton className="mt-12" />
+                </div>
+              ) : (
+                <>
+                  <Image
+                    src={RBTC_RIF_SQUARES_DESKTOP_SVG}
+                    alt="Squares Divider"
+                    width={40}
+                    height={30}
+                    className="absolute block left-1/2 top-8 -translate-x-[calc(55%)] z-10"
+                  />
+                  <div className="flex-1 relative">
+                    <div className="relative">
+                      <Image
+                        src={RBTC_RIF_BG_DESKTOP_SVG}
+                        alt="Intro Modal"
+                        height={0}
+                        width={0}
+                        className="h-auto w-full"
+                      />
+                      <YourWalletInfo status={status} />
+                    </div>
+                  </div>
+                  <div className="flex-1 flex flex-col justify-between p-4 md:p-0">
+                    <StakeDescription status={status} />
+                    <div className="flex justify-end">
+                      <ContinueButton />
+                    </div>
+                  </div>
+                </>
+              )}
+            </>
+          )}
+          {status === 2 &&
+            (isMobile ? (
+              <div className="mt-12 flex flex-col gap-4">
                 <div className="relative">
                   <Image
-                    src={RBTC_RIF_BG_DESKTOP_SVG}
+                    src={RBTC_BG_MOBILE_SVG}
                     alt="Intro Modal"
                     height={0}
                     width={0}
                     className="h-auto w-full"
                   />
-                  <YourWalletInfo />
+                  <Image
+                    src={RBTC_SQUARES_MOBILE_SVG}
+                    alt="Squares Divider"
+                    width={40}
+                    height={30}
+                    className="absolute bottom-[-40px] right-0"
+                  />
                 </div>
+                <StakeDescription status={status} />
+                <ContinueButton className="mt-12" />
               </div>
-              <div className="flex-1 flex flex-col justify-between p-4 md:p-0">
-                <StakeDescription />
-                <div className="flex justify-end">
-                  <ContinueButton />
+            ) : (
+              <>
+                <Image
+                  src={RBTC_SQUARES_DESKTOP_SVG}
+                  alt="Squares Divider"
+                  width={40}
+                  height={30}
+                  className="absolute block left-1/2 top-8 -translate-x-[calc(55%)] z-10"
+                />
+                <div className="flex-1 relative">
+                  <div className="relative">
+                    <Image
+                      src={RBTC_BG_DESKTOP_SVG}
+                      alt="Intro Modal"
+                      height={0}
+                      width={0}
+                      className="h-auto w-full"
+                    />
+                    <YourWalletInfo status={status} />
+                  </div>
                 </div>
-              </div>
-            </>
-          )}
+                <div className="flex-1 flex flex-col justify-between p-4 md:p-0">
+                  <StakeDescription status={status} />
+                  <div className="flex justify-end">
+                    <ContinueButton />
+                  </div>
+                </div>
+              </>
+            ))}
         </div>
       </Modal>
     )
   )
 }
 
-const StakeDescription = () => (
+const StakeDescription = ({ status }: { status: number }) => (
   <div className="flex flex-1 flex-col justify-center gap-4">
     <Label variant="tag" className="text-bg-100" caps>
       STAKE
@@ -139,17 +201,18 @@ const StakeDescription = () => (
         Before you stake
       </Header>
       <Header variant="e2" className="text-bg-100" caps>
-        add RBTC & RIF to your wallet
+        {status === 1 ? 'add RBTC & RIF to your wallet' : 'add RBTC to your wallet'}
       </Header>
     </div>
     <Paragraph className="text-bg-100">
-      RIF is the token required for staking, and RBTC is used to cover transaction fees. You&apos;ll need both
-      to participate in the DAO.
+      {status === 1
+        ? "RIF is the token required for staking, and RBTC is used to cover transaction fees. You'll need both to participate in the DAO."
+        : "RBTC is used to cover transaction fees. You'll need both RBTC and RIF to participate in the DAO."}
     </Paragraph>
   </div>
 )
 
-const YourWalletInfo = () => (
+const YourWalletInfo = ({ status }: { status: number }) => (
   <div
     className={cn(
       'absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2',
@@ -169,17 +232,19 @@ const YourWalletInfo = () => (
         RBTC
       </Span>
     </Header>
-    <Header variant="e2" className="text-text-100 flex flex-row items-end gap-2" caps>
-      <Span className="flex flex-row items-center gap-2">
-        USD
-        <ArrowRightIcon size={16} />
-      </Span>
-      <Span variant="e2" className="text-text-100">
-        RIF
-      </Span>
-    </Header>
+    {status === 1 && (
+      <Header variant="e2" className="text-text-100 flex flex-row items-end gap-2" caps>
+        <Span className="flex flex-row items-center gap-2">
+          USD
+          <ArrowRightIcon size={16} />
+        </Span>
+        <Span variant="e2" className="text-text-100">
+          RIF
+        </Span>
+      </Header>
+    )}
     <Paragraph variant="body-s" className="text-text-100 mt-2">
-      You need RBTC to pay fees & RIF to stake
+      {status === 1 ? 'You need RBTC to pay fees & RIF to stake' : 'You need RBTC for the transaction fees'}
     </Paragraph>
   </div>
 )
