f0db9220b5bd012facf21f2eee4af5463eb2b6b1
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2025-06-24 23:32:36 -0300

    fix rebase

--- src/shared/walletConnection/components/ConnectButtonComponent.tsx
+++ src/shared/walletConnection/components/ConnectButtonComponent.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/ButtonNew/Button'
 import { Button as OldButton } from '@/components/Button/Button'
 import { ConnectButtonComponentProps } from '../types'
+import { Span } from '@/components/TypographyNew'
 
 /**
  * New button that shows Connect Wallet
@@ -13,11 +14,11 @@ export const ConnectButtonComponent = ({ onClick }: ConnectButtonComponentProps)
     data-testid="ConnectWallet"
     variant="secondary-outline"
     className="px-2 py-1 border-bg-40"
-    textClassName="text-[14px] font-normal"
   >
-    Connect Wallet
+    <Span className="text-[14px] font-normal">Connect Wallet</Span>
   </Button>
 )
+
 export const ConnectButtonComponentSecondary = ({ onClick }: ConnectButtonComponentProps) => (
   <OldButton onClick={onClick} data-testid="ConnectWallet" variant="secondary">
     Connect Wallet
