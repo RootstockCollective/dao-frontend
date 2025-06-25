92feacaf8574fa73908d82da9090f48fc93f6be0
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2025-06-25 08:39:59 -0300

    If the user has stRIF and no RIF, don’t show modal

--- src/app/user/IntroModal/hooks/useRequiredTokens.ts
+++ src/app/user/IntroModal/hooks/useRequiredTokens.ts
@@ -1,6 +1,6 @@
 import { useBalancesContext } from '@/app/user/Balances/context/BalancesContext'
 import Big from '@/lib/big'
-import { RBTC, RIF } from '@/lib/constants'
+import { RBTC, RIF, stRIF } from '@/lib/constants'
 import { useMemo } from 'react'
 import { useAccount } from 'wagmi'
 import { IntroModalStatus } from '../config'
@@ -15,23 +15,28 @@ export const useRequiredTokens = (): IntroModalStatus | null => {
       return null
     }
 
+    const hasStRif = Big(balances[stRIF].balance).gt(0)
     const hasRif = Big(balances[RIF].balance).gt(0)
     const hasRbtc = Big(balances[RBTC].balance).gt(0)
 
+    const needRif = !hasStRif && !hasRif
+    const needRbtc = !hasRbtc
+
     // Determine which modal to show based on what tokens the user has
-    if (!hasRif && !hasRbtc) {
+    if (needRif && needRbtc) {
       // User has neither RIF nor RBTC
       return 'NEED_BOTH'
     }
-    if (hasRif && !hasRbtc) {
+    if (needRif) {
       // User has RIF but no RBTC
       return 'NEED_RBTC'
     }
-    if (!hasRif && hasRbtc) {
+    if (needRbtc) {
       // User has RBTC but no RIF
       return 'NEED_RIF'
     }
-    // User has both RIF and RBTC
+
+    // User has both RIF and RBTC, don't show modal
     return null
   }, [isConnected, isBalancesLoading, balances])
 }
