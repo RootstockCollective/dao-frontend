de5d025e040a3548d7fb07fe4136c66d469f39dd
DinizSa <diniz.araujosa@rootstocklabs.com>
2025-06-26 12:39:38 +0100

    feat: backers percentage cell

---
+++ src/app/builders/components/Table/BackersPercentageCell.tsx
@@ -0,0 +1,23 @@
+import { DeltaIndicator } from '@/components/DeltaIndicator/DeltaIndicator'
+import { BackerRewardPercentage } from '@/app/collective-rewards/rewards/types'
+import { weiToPercentage } from '@/app/collective-rewards/settings'
+import { cn } from '@/lib/utils'
+import { FC } from 'react'
+
+interface BackersPercentageCellProps {
+  percentage: BackerRewardPercentage
+  className?: string
+}
+
+export const BackersPercentageCell: FC<BackersPercentageCellProps> = ({ className, percentage }) => {
+  const currentPct = weiToPercentage(percentage.current, 0)
+  const nextPct = weiToPercentage(percentage.next, 0)
+  return (
+    <div className={cn('border-b-0', className)}>
+      <div className="flex flex-row gap-x-1 font-rootstock-sans justify-center gap-1 ">
+        {percentage && <div>{currentPct}</div>}
+        <DeltaIndicator currentPct={Number(currentPct)} nextPct={Number(nextPct)} />
+      </div>
+    </div>
+  )
+}
