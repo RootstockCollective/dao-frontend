72ee41b49656b19e0cc405dbfc13c703afc864cf
Alexander Evchenko <alexivchenko9@gmail.com>
2025-06-27 11:03:16 +0400

    fix: VotingDetails stories params

--- src/app/proposals/components/vote-details/VotingDetails.stories.tsx
+++ src/app/proposals/components/vote-details/VotingDetails.stories.tsx
@@ -14,10 +14,10 @@ type Story = StoryObj<typeof VotingDetails>
 export const Default: Story = {
   args: {
     voteData: {
-      for: '77,999,999',
-      against: '99,333,999',
-      abstain: '99,77,999',
-      quorum: '55,456,789',
+      for: BigInt('77999999'),
+      against: BigInt('99333999'),
+      abstain: BigInt('9977999'),
+      quorum: BigInt('55456789'),
     },
 
     votingPower: '4,300',
@@ -31,10 +31,10 @@ export const Default: Story = {
 export const HasVotedFor: Story = {
   args: {
     voteData: {
-      for: '11,999,111',
-      against: '99,444,999',
-      abstain: '99,999,999',
-      quorum: '564,456,111',
+      for: BigInt('11999111'),
+      against: BigInt('99444999'),
+      abstain: BigInt('99999999'),
+      quorum: BigInt('564456111'),
     },
     votingPower: '4,300',
     buttonAction: {
@@ -48,10 +48,10 @@ export const HasVotedFor: Story = {
 export const HasVotedButNoActionAvailable: Story = {
   args: {
     voteData: {
-      for: '8,999,999',
-      against: '99,800,999',
-      abstain: '99,999,555',
-      quorum: '35,35,789',
+      for: BigInt('8999999'),
+      against: BigInt('99800999'),
+      abstain: BigInt('99999555'),
+      quorum: BigInt('3535789'),
     },
     votingPower: '4,300',
     hasVoted: 'for',
@@ -61,10 +61,10 @@ export const HasVotedButNoActionAvailable: Story = {
 export const HasVotedAgainst: Story = {
   args: {
     voteData: {
-      for: '99,444,999',
-      against: '99,444,999',
-      abstain: '99,67,890',
-      quorum: '777,456,789',
+      for: BigInt('99444999'),
+      against: BigInt('99444999'),
+      abstain: BigInt('9967890'),
+      quorum: BigInt('777456789'),
     },
     votingPower: '4,300',
     hasVoted: 'against',
@@ -74,10 +74,10 @@ export const HasVotedAgainst: Story = {
 export const HasVotedAbstain: Story = {
   args: {
     voteData: {
-      for: '11,999,999',
-      against: '99,999,777',
-      abstain: '99,555,999',
-      quorum: '333,444,789',
+      for: BigInt('11999999'),
+      against: BigInt('99999777'),
+      abstain: BigInt('99555999'),
+      quorum: BigInt('333,444,789'),
     },
     votingPower: '4,300',
     hasVoted: 'abstain',
