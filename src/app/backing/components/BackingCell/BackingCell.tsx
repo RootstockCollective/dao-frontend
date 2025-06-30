0282d5c00d4357be8f35e9c185ad5146dc86cda5
Hakob23 <36648940+Hakob23@users.noreply.github.com>
2025-06-30 22:42:39 +0300

    Backing Cell (#1079)
    * init commit
    
    * refactor: separate duplicate components
    
    * refactor: integrating separated components into backingCell
    
    * refactor: simplify currency formatting in AvailableBackingUSD component
    
    * fix: title typo
    
    * feat: redirecting the files
    
    * refactor: move AvailableBackingUSD component to a new directory
    
    * refactor: integrate Metric component in BackingCell
    
    * refactor: update BackingCell layout and styling
    
    * refactor: correct BackingCell layout and typography
    
    * refactor: fixed alignment and structure
    
    * feat: add state management to BackingCell component with new story examples
    
    * refactor: replace StRIFToken with RIFToken in BackingCell component
    
    * refactor: revert strifToken changes
    
    * feat: integrate context providers and update story examples
    
    * refactor: update BackingCell stories
    
    * feat: add USD display to BackingCell component
    
    * refactor: simplify background styles and adjust typography in BackingCell component
    
    * refactor: remove title props from BackingCell story examples
    
    * refactor: enhance BackingCell component layout
    
    * refactor: streamline BackingCell component structure and improve layout
    
    * refactor: update typography and layout in BackingCell component
    
    * refactor: remove unused state prop and simplify BackingCell component
    
    * refactor: improve formatting and layout in BackingCell component
    
    * refactor: remove AvailableBackingUSD component and clean up BackingCell exports
    
    * fix: align rif icon with text
    
    ---------
    
    Co-authored-by: Hakob23 <hakob.arshakyan230@gmail.com>
    Co-authored-by: Antonio <antonio@iovlabs.org>

---
+++ src/app/backing/components/BackingCell/BackingCell.tsx
@@ -0,0 +1,61 @@
+import { AllocationsContext } from '@/app/collective-rewards/allocations/context'
+import { formatSymbol, getFiatAmount } from '@/app/collective-rewards/rewards/utils/formatter'
+import { TokenImage } from '@/components/TokenImage'
+import { Typography } from '@/components/TypographyNew/Typography'
+import { RIF } from '@/lib/constants'
+import { cn, formatCurrency } from '@/lib/utils'
+import { usePricesContext } from '@/shared/context/PricesContext'
+import { FC, useContext } from 'react'
+import { Address } from 'viem'
+
+type BackingCellProps = {
+  className?: string
+  builderAddress: Address
+}
+
+export const BackingCell: FC<BackingCellProps> = ({ className, builderAddress }) => {
+  const { prices } = usePricesContext()
+  const {
+    state: { allocations },
+  } = useContext(AllocationsContext)
+
+  const amount = allocations[builderAddress] ?? 0n
+  const rifPrice = prices[RIF]?.price ?? 0
+  const formattedAmount = formatSymbol(amount, 'stRIF')
+  const formattedUsdAmount = formatCurrency(getFiatAmount(amount, rifPrice), { currency: 'USD' })
+
+  return (
+    <div className={cn('flex justify-end items-end gap-2', className)} data-testid="BackingCell">
+      <div className="flex flex-col items-end">
+        <Typography
+          variant="body"
+          className={cn(
+            'font-rootstock-sans text-base font-normal leading-6 text-right',
+            amount > 0n ? 'text-v3-text-100' : 'text-v3-bg-accent-20',
+          )}
+        >
+          {formattedAmount}
+        </Typography>
+        {amount > 0n && (
+          <Typography
+            variant="body"
+            className="font-rootstock-sans text-xs font-normal leading-[18px] text-right text-v3-bg-accent-0"
+          >
+            {formattedUsdAmount}
+          </Typography>
+        )}
+      </div>
+      <div className="flex flex-col items-start gap-1">
+        <div className="flex justify-center items-center aspect-square">
+          <TokenImage symbol={RIF} />
+        </div>
+        <Typography
+          variant="body"
+          className="font-rootstock-sans text-xs font-normal leading-[18px] text-v3-bg-accent-0"
+        >
+          USD
+        </Typography>
+      </div>
+    </div>
+  )
+}
