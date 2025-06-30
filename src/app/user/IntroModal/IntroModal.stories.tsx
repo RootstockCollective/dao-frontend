9c3e657d7a23c51d7de5887082f6603aca0e1357
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2025-06-30 12:49:42 -0300

    Refactor IntroModal stories: rename and update token statuses for desktop and mobile variants

--- src/app/user/IntroModal/IntroModal.stories.tsx
+++ src/app/user/IntroModal/IntroModal.stories.tsx
@@ -38,28 +38,10 @@ export const NeedBothRBTCAndRIFDesktop: Story = {
   },
 }
 
-export const NeedBothRBTCAndRIFMobile: Story = {
-  args: {
-    tokenStatus: 'NEED_RBTC_RIF',
-    isDesktop: false,
-    onClose: () => console.log('Modal closed'),
-    onContinue: () => console.log('Continue'),
-  },
-}
-
 export const NeedRBTCDesktop: Story = {
   args: {
     tokenStatus: 'NEED_RBTC',
     isDesktop: true,
-    onClose: () => console.log('Modal closed'),
-    onContinue: () => console.log('Continue'),
-  },
-}
-
-export const NeedRBTCMobile: Story = {
-  args: {
-    tokenStatus: 'NEED_RBTC',
-    isDesktop: false,
     onClose: () => console.log('Modal closed'),
     onContinue: () => console.log('Continue'),
   },
@@ -74,21 +56,38 @@ export const NeedRIFDesktop: Story = {
   },
 }
 
-export const NeedRIFMobile: Story = {
-  args: {
-    tokenStatus: 'NEED_RIF',
-    isDesktop: false,
-    onClose: () => console.log('Modal closed'),
-    onContinue: () => console.log('Continue'),
-  },
-}
-
 export const NeedSTRIFDesktop: Story = {
   args: {
     tokenStatus: 'NEED_STRIF',
     isDesktop: true,
     rbtcBalance: '0.1234',
     rifBalance: '543.21',
+    onClose: () => console.log('Modal closed'),
+    onContinue: () => console.log('Continue'),
+  },
+}
+
+export const NeedBothRBTCAndRIFMobile: Story = {
+  args: {
+    tokenStatus: 'NEED_RBTC_RIF',
+    isDesktop: false,
+    onClose: () => console.log('Modal closed'),
+    onContinue: () => console.log('Continue'),
+  },
+}
+export const NeedRBTCMobile: Story = {
+  args: {
+    tokenStatus: 'NEED_RBTC',
+    isDesktop: false,
+    onClose: () => console.log('Modal closed'),
+    onContinue: () => console.log('Continue'),
+  },
+}
+
+export const NeedRIFMobile: Story = {
+  args: {
+    tokenStatus: 'NEED_RIF',
+    isDesktop: false,
     onClose: () => console.log('Modal closed'),
     onContinue: () => console.log('Continue'),
   },
