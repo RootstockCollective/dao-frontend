0366432602430cbf86192df840c821581c4413eb
Cybervoid <mail@cybervoid.my>
2025-07-15 05:05:37 +0200

    fix(PreviewLabel): adjust layout to prevent text wrapping and ensure consistent display

--- src/app/proposals/new/review/components/PreviewLabel.tsx
+++ src/app/proposals/new/review/components/PreviewLabel.tsx
@@ -3,9 +3,9 @@ import InfoIcon from './InfoIcon'
 
 export default function PreviewLabel() {
   return (
-    <div className="flex gap-1 items-center justify-end">
+    <div className="flex gap-1 items-center justify-end flex-nowrap">
       <InfoIcon />
-      <p className="text-sm  md:text-base font-rootstock-sans leading-tight text-text-40">
+      <p className="text-sm  md:text-base font-rootstock-sans leading-tight text-text-40 whitespace-nowrap">
         This is a preview of how the proposal will look
       </p>
     </div>
