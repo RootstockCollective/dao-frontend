0b216c8fd05b2ed91c216e0db7d3c75c9711a8eb
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2025-07-15 16:00:36 -0300

    remove padding

--- src/app/treasury/page.tsx
+++ src/app/treasury/page.tsx
@@ -7,7 +7,7 @@ import { HoldersSection } from '@/app/treasury/sections/HoldersSection'
 export default function Treasury() {
   return (
     <TreasuryContextProviderWithPrices>
-      <div className="pl-2 flex flex-col gap-12">
+      <div className="flex flex-col gap-12">
         <div className="flex flex-col gap-10 p-6 bg-bg-80 rounded-md">
           <MetricsSection />
           <TabsSection />
