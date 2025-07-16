2020f3874a4334fbff0e2ef804fc6a1dfe39a7c7
DinizSa <diniz.araujosa@rootstocklabs.com>
2025-07-16 11:22:41 +0100

    chore: allow show percentage modal in my rewards

--- src/app/my-rewards/page.tsx
+++ src/app/my-rewards/page.tsx
@@ -1,9 +1,19 @@
+'use client'
 import { withServerFeatureFlag } from '@/shared/context/FeatureFlag'
 import { MyRewardsPage } from './MyRewardsPage'
+import { BuilderSettingsProvider } from '../collective-rewards/settings/builder/context'
 
 const MyRewardsPageWithFeature = withServerFeatureFlag(MyRewardsPage, {
   feature: 'v3_design',
   redirectTo: '/',
 })
 
-export default MyRewardsPageWithFeature
+const MyRewardsPageWithProviders = () => {
+  return (
+    <BuilderSettingsProvider>
+      <MyRewardsPageWithFeature />
+    </BuilderSettingsProvider>
+  )
+}
+
+export default MyRewardsPageWithProviders
