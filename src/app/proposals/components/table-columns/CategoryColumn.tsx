749d541fb0ac9aa289d5fa6c095a045818da5a9f
Alexander Evchenko <alexivchenko9@gmail.com>
2025-07-16 11:33:18 +0400

    refactor: make VotesColumn, QuorumColumn and TimeColumn extendable

--- src/app/proposals/components/table-columns/CategoryColumn.tsx
+++ src/app/proposals/components/table-columns/CategoryColumn.tsx
@@ -2,6 +2,8 @@ import { Tooltip } from '@/components/Tooltip'
 import { GrantsIcon } from './icons/GrantsIcon'
 import { ToolsIcon } from './icons/ToolsIcon'
 import { ProposalCategory } from '@/shared/types'
+import { ClassNameValue } from 'tailwind-merge'
+import { cn } from '@/lib/utils'
 
 const icons = {
   [ProposalCategory.Grants]: <GrantsIcon />,
@@ -11,11 +13,12 @@ const icons = {
 
 interface Props {
   category: ProposalCategory
+  className?: ClassNameValue
 }
 
-export function CategoryColumn({ category }: Props) {
+export function CategoryColumn({ category, className }: Props) {
   return (
-    <div className="w-full flex justify-center">
+    <div className={cn('w-full flex justify-center', className)}>
       <Tooltip text={category}>
         <div>{icons[category]}</div>
       </Tooltip>
