b73b639636c3ca68329070a08c28c73a4c8e5ef4
Cybervoid <mail@cybervoid.my>
2025-07-16 13:07:29 +0200

    refactor: optimize treasury transfer proposal creation with improved transaction ID retrieval and enhanced memoization

--- src/app/proposals/hooks/useCreateTreasuryTransferProposal.ts
+++ src/app/proposals/hooks/useCreateTreasuryTransferProposal.ts
@@ -8,6 +8,7 @@ import { Address, encodeFunctionData, Hash, parseEther, zeroAddress } from 'viem
 import { useWriteContract } from 'wagmi'
 import { readContract } from '@wagmi/core'
 import { config } from '@/config'
+import { useCallback, useMemo } from 'react'
 
 const DEFAULT_DAO_CONFIG = {
   abi: GovernorAbi,
@@ -18,29 +19,46 @@ export const useCreateTreasuryTransferProposal = () => {
   const { canCreateProposal } = useVotingPower()
   const { writeContractAsync: propose, isPending: isPublishing } = useWriteContract()
 
-  const onCreateTreasuryTransferProposal = async (
-    address: Address,
-    amount: string,
-    description: string,
-    tokenAddress: string,
-  ) => {
-    if (!canCreateProposal) {
-      throw NoVotingPowerError
-    }
-    let calldata
-    if (tokenAddress === zeroAddress) {
-      calldata = encodeTreasuryTransfer(address, amount)
-    } else {
-      calldata = encodeTreasuryERC20Transfer(address, amount)
-    }
-    const { proposal } = createProposal([TreasuryAddress], [0n], [calldata], description)
-    return propose({
+  const onCreateTreasuryTransferProposal = useCallback(
+    async (address: Address, amount: string, description: string, tokenAddress: string) => {
+      console.log('🚀 ~ useCreateTreasuryTransferProposal ~ canCreateProposal:', canCreateProposal)
+      if (!canCreateProposal) {
+        throw NoVotingPowerError
+      }
+      let calldata
+      if (tokenAddress === zeroAddress) {
+        calldata = encodeTreasuryTransfer(address, amount)
+      } else {
+        calldata = encodeTreasuryERC20Transfer(address, amount)
+      }
+      const { proposal, proposalToRunHash } = createProposal([TreasuryAddress], [0n], [calldata], description)
+      return {
+        txHash: await propose({
+          ...DEFAULT_DAO_CONFIG,
+          functionName: 'propose',
+          args: proposal,
+        }),
+        txId: await getProposalId(proposalToRunHash),
+      }
+    },
+    [canCreateProposal, propose],
+  )
+  return useMemo(
+    () => ({ onCreateTreasuryTransferProposal, isPublishing }),
+    [isPublishing, onCreateTreasuryTransferProposal],
+  )
+}
+
+async function getProposalId(proposal: [Address[], bigint[], Hash[], Hash]): Promise<bigint | undefined> {
+  try {
+    return await readContract(config, {
       ...DEFAULT_DAO_CONFIG,
-      functionName: 'propose',
+      functionName: 'hashProposal',
       args: proposal,
     })
+  } catch {
+    undefined
   }
-  return { onCreateTreasuryTransferProposal, isPublishing }
 }
 
 export const encodeTreasuryERC20Transfer = (address: Address, amountToTransfer: string) => {
