578b23f52502d7dfe9ca53bc1912619815062211
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2025-07-23 18:07:45 -0300

    Add LatestCollectiveSection

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -5,15 +5,20 @@ import { useSearchParams } from 'next/navigation'
 import { IntroModal } from './IntroModal'
 import { StackingNotifications } from '@/app/user/StackingNotifications/StackingNotifications'
 import { useAccount } from 'wagmi'
+import { LatestCollectiveSection } from './latest-collective'
+import { useGetProposalsWithGraph } from '../proposals/hooks/useGetProposalsWithGraph'
 
 export default function User() {
   const { isConnected } = useAccount()
   const searchParams = useSearchParams()
+  const { activeProposals } = useGetProposalsWithGraph()
+
   return (
     <div className="flex flex-col gap-2">
       {searchParams.get('action') !== 'stake' && <IntroModal />}
       {isConnected && <StackingNotifications />}
       <BalancesSection />
+      {activeProposals.length > 0 && <LatestCollectiveSection proposal={activeProposals[0]} />}
       <CommunitiesSection />
     </div>
   )
