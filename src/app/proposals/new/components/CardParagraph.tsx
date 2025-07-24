cb7c9fc7921099103b1d5bed2cb3ad5a3367176e
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2025-07-23 21:33:53 -0300

    Adjust the Create builder Rewards text

--- src/app/proposals/new/components/CardParagraph.tsx
+++ src/app/proposals/new/components/CardParagraph.tsx
@@ -1,5 +1,5 @@
 import { ArrowUpRightLightIcon } from '@/components/Icons'
-import { cn } from '@/lib/utils'
+import { Label, Paragraph } from '@/components/TypographyNew'
 import Link from 'next/link'
 
 interface Props extends React.PropsWithChildren {
@@ -7,18 +7,18 @@ interface Props extends React.PropsWithChildren {
   url: string
 }
 
-const baseClasses = 'font-rootstock-sans text-bg-100 text-base leading-normal tracking-normal'
-
 export function CardParagraph({ header, url, children }: Props) {
   return (
-    <div className="flex flex-col gap-[10px]">
+    <div className="flex flex-col gap-2">
       <Link href={url} target="_blank" rel="noopener noreferrer" referrerPolicy="no-referrer">
         <div className="flex gap-1 items-center">
-          <h4 className={cn(baseClasses, 'font-bold')}>{header}</h4>
+          <Label className="text-bg-100" bold>
+            {header}
+          </Label>
           <ArrowUpRightLightIcon size={20} strokeWidth="1.8" className="text-bg-100" />
         </div>
       </Link>
-      <div className={cn(baseClasses, 'font-normal')}>{children}</div>
+      <Paragraph className="text-bg-100">{children}</Paragraph>
     </div>
   )
 }
--- src/app/proposals/new/components/NewProposalCardExtended.tsx
+++ src/app/proposals/new/components/NewProposalCardExtended.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link'
 import { DotsOverlayHoriz } from '../images/DotsOverlayHoriz'
 import { DotsOverlayVert } from '../images/DotsOverlayVert'
 import { CardButton } from './CardButton'
+import { Header } from '@/components/TypographyNew'
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
   card: NewProposalCardExtendedData
@@ -38,23 +39,24 @@ export function NewProposalCardExtended({ card, className, cancelCardSelection, 
       </div>
 
       {/* Right Side - Content Section */}
-      <div className="basis-1/2 pl-2 sm:pl-8 pr-2 py-6 flex flex-col justify-end">
+      <div className="basis-1/2 pl-2 md:pl-8 pr-2 py-6 flex flex-col justify-end gap-8">
         {/* Top Section */}
-        <div className="mt-2 sm:mt-0 flex flex-col gap-8">
-          <h2 className="text-3xl sm:text-[44px] leading-none text-bg-100 uppercase font-kk-topo">
+        <div className="mt-2 md:mt-10 flex flex-col gap-8">
+          <Header variant="e2" className="text-bg-100 text-3xl md:text-11" caps>
             {cardTitle}
-          </h2>
-          {/* Description Section */}
-          <div className="flex flex-col gap-6 text-bg-100 font-rootstock-sans">{textBlock}</div>
+          </Header>
         </div>
 
         {/* Bottom Section */}
-        <div className="mt-10 flex flex-col gap-6">
-          <h3 className="text-2xl leading-tight text-bg-100 uppercase font-kk-topo tracking-wide">
-            {bottomTitle}
-          </h3>
+        <div className="flex flex-col gap-6">
+          {/* Description Section */}
+          <div className="flex flex-col gap-6 text-bg-100 font-rootstock-sans">{textBlock}</div>
 
-          <div className="flex flex-col gap-[26px]">
+          <Header variant="h3" className="mt-4 text-bg-100" caps>
+            {bottomTitle}
+          </Header>
+
+          <div className="flex flex-col gap-6">
             {bottomTextBlock.map(({ header, text, url }, index) => (
               <CardParagraph key={index} header={header} url={url}>
                 {text}
@@ -64,7 +66,7 @@ export function NewProposalCardExtended({ card, className, cancelCardSelection, 
         </div>
 
         {/* Action Buttons */}
-        <div className="mt-8 flex justify-end gap-4">
+        <div className="flex justify-end gap-4">
           <CardButton className="border border-bg-0" onClick={cancelCardSelection}>
             Cancel
           </CardButton>
