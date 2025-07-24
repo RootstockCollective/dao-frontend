a01a85d64b5e6ce6cd08ecd9c2c91dcb3fbd8fe0
Antonio <antonio@iovlabs.org>
2025-07-24 10:23:06 +0200

    Revert "fix: remove the NFT booster card"
    This reverts commit d9d1ffe2004be82e36a30925dbace2b99719b09e.

--- src/shared/walletConnection/components/topPageHeader/TopPageHeader.tsx
+++ src/shared/walletConnection/components/topPageHeader/TopPageHeader.tsx
@@ -1,4 +1,8 @@
 import { TopPageHeaderLeftSlotStrategy } from './TopPageHeaderLeftSlotStrategy'
+import { usePathname } from 'next/navigation'
+import { SelfContainedNFTBoosterCard } from '@/app/shared/components/NFTBoosterCard/SelfContainedNFTBoosterCard'
+import { collectiveRewards, home } from '@/shared/constants'
+import { useNFTBoosterContext } from '@/app/providers/NFT/BoosterContext'
 /**
  * This component will render first for all pages. It should contain the user connection workflow.
  * It will also render the left slot strategy component.
@@ -7,10 +11,22 @@ import { TopPageHeaderLeftSlotStrategy } from './TopPageHeaderLeftSlotStrategy'
  * @constructor
  */
 export function TopPageHeader() {
+  const pathname = usePathname()
+  const isMyCollective = pathname === home
+  const isCollectiveRewards = pathname === collectiveRewards
+  const { isBoosted } = useNFTBoosterContext()
+  const forceRender = isCollectiveRewards || (isMyCollective && isBoosted)
+
   return (
     <div className="grid grid-cols-[1fr_auto] gap-x-3 mb-4">
       <div className="flex justify-start items-center">
         <TopPageHeaderLeftSlotStrategy />
+      </div>
+      <div className="flex justify-end flex-row gap-5 items-center">
+        <SelfContainedNFTBoosterCard forceRender={forceRender} />
+        {/* Commented buttons to test reown  */}
+        {/*<appkit-account-button />*/}
+        {/*<appkit-network-button />*/}
       </div>
     </div>
   )
