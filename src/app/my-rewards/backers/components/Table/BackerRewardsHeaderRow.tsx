96c16676f620ff5998f385f092f6f7e220610015
Juraj Piar <juraj@iovlabs.org>
2025-07-25 17:18:37 +0100

    fix(my-rewards): table headers wording
    + and adds tooltips

--- src/app/my-rewards/backers/components/Table/BackerRewardsHeaderRow.tsx
+++ src/app/my-rewards/backers/components/Table/BackerRewardsHeaderRow.tsx
@@ -2,39 +2,20 @@
 
 import {
   BuilderHeaderCell,
-  BuilderHeaderCellBase,
   CombinedActionsHeaderCell,
-  HeaderCellBase,
+  HeaderCell,
   HeaderSubtitle,
   HeaderTitle,
   TableColumnDropdown,
 } from '@/app/builders/components/Table'
 import { Action } from '@/app/builders/components/Table/Cell/ActionCell'
 import { Button } from '@/components/ButtonNew'
-import { CommonComponentProps } from '@/components/commonProps'
 import { CloseIconKoto } from '@/components/Icons'
 import { TableHeaderCell } from '@/components/TableNew'
+import { Paragraph } from '@/components/TypographyNew'
 import { useTableActionsContext } from '@/shared/context'
-import { ReactNode, Suspense } from 'react'
-import { COLUMN_TRANSFORMS, ColumnId, LABELS } from './BackerRewardsTable.config'
-
-const HeaderCell = ({
-  className,
-  children,
-  columnId,
-  ...props
-}: CommonComponentProps & { columnId: ColumnId }): ReactNode => {
-  return (
-    <HeaderCellBase<ColumnId>
-      className={className}
-      columnId={columnId}
-      columnTransforms={COLUMN_TRANSFORMS}
-      {...props}
-    >
-      {children}
-    </HeaderCellBase>
-  )
-}
+import { Suspense } from 'react'
+import { COLUMN_TRANSFORMS, LABELS } from './BackerRewardsTable.config'
 
 export type BackerRewardsHeaderRowProps = {
   actions: Action[]
@@ -52,30 +33,65 @@ export const BackerRewardsHeaderRow = ({ actions }: BackerRewardsHeaderRowProps)
         <BuilderHeaderCell key="builder" columnId="builder" />
         {actionCount <= 1 && (
           <>
-            <HeaderCell key="backer_rewards" columnId="backer_rewards">
+            <HeaderCell key="backer_rewards" columnId="backer_rewards" columnTransforms={COLUMN_TRANSFORMS}>
               <HeaderTitle>Backer Rewards</HeaderTitle>
-              <HeaderSubtitle>current % - change</HeaderSubtitle>
+              <HeaderSubtitle>%</HeaderSubtitle>
             </HeaderCell>
-            <HeaderCell key="unclaimed" columnId="unclaimed">
-              <HeaderTitle>Rewards</HeaderTitle>
-              <HeaderSubtitle>past cycle</HeaderSubtitle>
+            <HeaderCell
+              key="unclaimed"
+              columnId="unclaimed"
+              columnTransforms={COLUMN_TRANSFORMS}
+              tooltip={{
+                text: 'Your rewards from each Builder available to claim',
+                side: 'top',
+                sideOffset: 10,
+              }}
+            >
+              <HeaderTitle>Unclaimed</HeaderTitle>
+              <HeaderSubtitle className="h-full text-v3-bg-accent-80">balls</HeaderSubtitle>
+              {/* TODO: temporary fix to align the text to the top */}
+              <HeaderSubtitle></HeaderSubtitle>
             </HeaderCell>
-            <HeaderCell key="estimated" columnId="estimated">
-              <HeaderTitle>Rewards</HeaderTitle>
-              <HeaderSubtitle>upcoming cycle, estimated</HeaderSubtitle>
+            <HeaderCell
+              key="estimated"
+              columnId="estimated"
+              columnTransforms={COLUMN_TRANSFORMS}
+              tooltip={{
+                text: (
+                  <div className="flex flex-col gap-2 text-wrap max-w-[35rem]">
+                    <Paragraph>
+                      An estimate of the remainder of this Cycle&apos;s rewards from each Builder that will
+                      become fully claimable by the end of the current Cycle. These rewards gradually
+                      transition into your &apos;Claimable Rewards&apos; as the cycle progresses.
+                    </Paragraph>
+                    <Paragraph className="mt-2 mb-2">
+                      To check the cycle`s completion, go to Collective Rewards → Current Cycle.
+                    </Paragraph>
+                    <Paragraph>
+                      The displayed information is dynamic and may vary based on total rewards and user
+                      activity. This data is for informational purposes only.
+                    </Paragraph>
+                  </div>
+                ),
+                side: 'top',
+                sideOffset: 10,
+              }}
+            >
+              <HeaderTitle>Estimated</HeaderTitle>
+              <HeaderSubtitle>this cycle</HeaderSubtitle>
             </HeaderCell>
-            <HeaderCell key="total" columnId="total">
+            <HeaderCell key="total" columnId="total" columnTransforms={COLUMN_TRANSFORMS}>
               <HeaderTitle>Total</HeaderTitle>
               <HeaderSubtitle>lifetime</HeaderSubtitle>
             </HeaderCell>
-            <HeaderCell key="backing" columnId="backing">
+            <HeaderCell key="backing" columnId="backing" columnTransforms={COLUMN_TRANSFORMS}>
               <HeaderTitle>Backing</HeaderTitle>
-              <HeaderSubtitle className="h-full text-v3-bg-accent-80">balls</HeaderSubtitle>{' '}
+              <HeaderSubtitle className="h-full text-v3-bg-accent-80">balls</HeaderSubtitle>
               {/* TODO: temporary fix to align the text to the top */}
             </HeaderCell>
-            <HeaderCell columnId="actions">
+            <HeaderCell columnId="actions" columnTransforms={COLUMN_TRANSFORMS}>
               <HeaderTitle>Actions</HeaderTitle>
-              <HeaderSubtitle className="h-full text-v3-bg-accent-80">balls</HeaderSubtitle>{' '}
+              <HeaderSubtitle className="h-full text-v3-bg-accent-80">balls</HeaderSubtitle>
               {/* TODO: temporary fix to align the text to the top */}
             </HeaderCell>
             <th>
