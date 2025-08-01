982588e3064ae18eaf33af73da252f6531b97a5e
DinizSa <diniz.araujosa@rootstocklabs.com>
2025-08-01 09:23:09 +0100

    fix: show proper title for builder and backer

--- src/app/user/my-holdings/MyActivityAndBalances.tsx
+++ src/app/user/my-holdings/MyActivityAndBalances.tsx
@@ -3,21 +3,29 @@
 import { MyBacking } from './components/backing'
 import { SectionContainer } from '@/app/communities/components/SectionContainer'
 import { BalancesSection } from '../Balances/BalancesSection'
+import { useReadBuilderRegistry } from '@/shared/hooks/contracts'
+import { useAccount } from 'wagmi'
+import { zeroAddress } from 'viem'
 
 const Separator = () => <hr className="w-full bg-bg-60 border-none h-px my-10" />
 
 export const MyActivityAndBalances = () => {
-  const isUserBuilder = true // @TODO
+  const { address } = useAccount()
+  const { data: gauge } = useReadBuilderRegistry(
+    {
+      functionName: 'builderToGauge',
+      args: [address ?? zeroAddress],
+    },
+    { enabled: !!address },
+  )
+
+  const isUserBuilder = Boolean(gauge && gauge !== zeroAddress)
   const sectionTitle = isUserBuilder ? 'MY ACTIVITY & BALANCES' : 'MY BALANCES'
 
   return (
     <SectionContainer title={sectionTitle} headerVariant="h3">
-      {isUserBuilder && (
-        <>
-          <MyBacking />
-          <Separator />
-        </>
-      )}
+      <MyBacking />
+      <Separator />
       <BalancesSection />
     </SectionContainer>
   )
