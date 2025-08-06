7018b54bcf7bbc7607500dd4ecaba1c9f8de271a
Alexander Evchenko <alexivchenko9@gmail.com>
2025-08-06 12:15:23 +0400

    fix: interceptor triggered on not back behaviour

--- src/app/proposals/new/components/ProposalSubfooter.tsx
+++ src/app/proposals/new/components/ProposalSubfooter.tsx
@@ -30,16 +30,21 @@ export const ProposalSubfooter = ({
   const isDesktop = useIsDesktop()
 
   const [showModal, setShowModal] = useState(false)
+  const [isBackPressed, setIsBackPressed] = useState(false)
 
   const { active, accept, reject } = useNavigationGuard({
-    enabled: pathname.startsWith('/proposals/new/details'),
+    enabled: true,
   })
 
   useEffect(() => {
-    if (active) {
+    if (active && isBackPressed && pathname.startsWith('/proposals/new/details')) {
       setShowModal(true)
     }
-  }, [active])
+    // If the active state is true but we are not on the guarded page, we accept navigation
+    else if (active && !pathname.startsWith('/proposals/new/details')) {
+      accept()
+    }
+  }, [active, isBackPressed, accept, pathname])
 
   // --- Modal Button Handlers ---
 
@@ -53,16 +58,27 @@ export const ProposalSubfooter = ({
     reject()
   }, [reject])
 
+  const handleBack = useCallback(() => {
+    setIsBackPressed(true)
+    router.back()
+  }, [router])
+
+  const handleNext = () => {
+    setIsBackPressed(false)
+    accept()
+    submitForm()
+  }
+
   if (!isDesktop && isSidebarOpen) return null
 
   return (
     <>
       <ActionsContainer className="bg-bg-60" containerClassName="items-center">
         <div className="flex items-center justify-center gap-2">
-          <Button disabled={backDisabled || disabled} onClick={router.back} variant="secondary-outline">
+          <Button disabled={backDisabled || disabled} onClick={handleBack} variant="secondary-outline">
             Back
           </Button>
-          <Button disabled={nextDisabled || disabled} onClick={submitForm}>
+          <Button disabled={nextDisabled || disabled} onClick={handleNext}>
             {buttonText}
           </Button>
         </div>
