ac4e9da6e162556b91b5951d8851460ce177c1b6
franciscotobar <100875069+franciscotobar@users.noreply.github.com>
2025-08-06 01:16:14 -0600

    TOK-889/fix-linting-warnings (#1274)
    * fix: linting warnings
    
    * refactor: pr comments

--- src/app/communities/components/ImageDebris.tsx
+++ src/app/communities/components/ImageDebris.tsx
@@ -16,11 +16,14 @@ export const ImageDebris = ({ image }: Props) => {
   const { extractPixelsByConfig } = usePixelExtractor()
   useEffect(() => {
     extractPixelsByConfig(image).then(data => setDebris(data))
-  }, [])
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <>
       {debris.map(debri => (
+        // We're using a plain <img> tag instead of next/image because the image debris have unknown dimensions,
+        // making it impossible to leverage next/image's optimization features like resizing or lazy loading.
+        // eslint-disable-next-line @next/next/no-img-element
         <img
           key={`${debri.coordinates.x}${debri.coordinates.y}${debri.className}`}
           src={debri.data}
