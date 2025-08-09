7c99370616e54559ada4ad6407df9af047a48ef8
chrisarevalo11 <christian.arevalo@rootstocklabs.com>
2025-08-08 19:48:10 -0500

    fix: resolved fixes from PR discussion

--- src/app/backing/components/AllocationBar/AllocationBarTooltip.tsx
+++ src/app/backing/components/AllocationBar/AllocationBarTooltip.tsx
@@ -1,38 +1,40 @@
-import { motion, AnimatePresence } from 'motion/react'
-import { useState } from 'react'
-import { b } from 'vitest/dist/chunks/suite.d.FvehnV49.js'
+import { HourglassIcon } from '@/components/Icons/HourglassIcon'
+import { Typography } from '@/components/TypographyNew/Typography'
+import { shortAddress } from '@/lib/utils'
 
 interface AllocationBarTooltipProps {
   builderAddress: string
   currentBacking: number
+  pendingBacking: number
+  isTemporary?: boolean
 }
 
-export const AllocationBarTooltip = ({ builderAddress, currentBacking }: AllocationBarTooltipProps) => {
-  console.log(builderAddress)
-
+export const AllocationBarTooltip = ({
+  builderAddress,
+  currentBacking,
+  pendingBacking,
+  isTemporary,
+}: AllocationBarTooltipProps) => {
   return (
-    <motion.div
-      initial={{ opacity: 0, y: -5, scale: 0.95 }}
-      animate={{ opacity: 1, y: 0, scale: 1 }}
-      exit={{ opacity: 0, y: -5, scale: 0.95 }}
-      transition={{ duration: 0.2, ease: 'easeOut' }}
-      className="flex-col p-2 hidden group-hover:flex absolute -top-8 left-5 bg-st-white rounded-md border z-[100000]"
-    >
-      <span className="font-medium text-sm font-rootstock-sans text-black">
-        {builderAddress === 'unallocated'
-          ? 'Unallocated'
-          : builderAddress.slice(0, 6) + '...' + builderAddress.slice(-4)}
-      </span>
+    <div className="w-[230px] p-3">
+      <Typography variant="tag-s" className="text-foreground">
+        {builderAddress === 'unallocated' ? 'Unallocated' : shortAddress(builderAddress as `0x${string}`)}
+      </Typography>
       <div className="flex flex-col gap-1 mt-2">
+        {isTemporary && (
+          <div className="flex justify-between items-center text-secondary gap-5">
+            <Typography>Pending</Typography>
+            <span className="inline-flex items-center gap-1">
+              <HourglassIcon className="size-5" color="var(--background-40)" />
+              <Typography>{pendingBacking.toLocaleString()}</Typography>
+            </span>
+          </div>
+        )}
         <div className="flex justify-between items-center text-secondary gap-5">
-          <p className="text-sm">Pending</p>
-          <p className="font-medium">{currentBacking.toFixed(2)}</p>
-        </div>
-        <div className="flex justify-between items-center text-secondary gap-5">
-          <p className="text-sm whitespace-nowrap">Current backing</p>
-          <p className="font-medium">{currentBacking.toFixed(2)}</p>
+          <Typography>Current backing</Typography>
+          <Typography>{currentBacking.toLocaleString()}</Typography>
         </div>
       </div>
-    </motion.div>
+    </div>
   )
 }
