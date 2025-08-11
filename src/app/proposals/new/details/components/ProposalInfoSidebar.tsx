515dedcb78aa8efcbc771690a3d960bb734915ff
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2025-08-11 12:26:57 -0300

    add info to create proposal pages (#1299)

---
+++ src/app/proposals/new/details/components/ProposalInfoSidebar.tsx
@@ -0,0 +1,36 @@
+import { Paragraph, Span } from '@/components/TypographyNew'
+import { ExternalLink } from '@/components/Link/ExternalLink'
+import InfoIcon from '../../review/components/InfoIcon'
+
+interface ProposalInfoSidebarProps {
+  kycLink: string
+}
+
+export const ProposalInfoSidebar = ({ kycLink }: ProposalInfoSidebarProps) => {
+  return (
+    <>
+      <InfoIcon className="w-5 h-5 text-bg-0" />
+      <div className="flex flex-col gap-2">
+        <Paragraph className="text-bg-0">
+          Before you submit a proposal, make sure that you have{' '}
+          <ExternalLink href={kycLink} className="text-v3-primary" target="_blank" rel="noopener noreferrer">
+            <Span>completed your KYC</Span>
+          </ExternalLink>{' '}
+          and that you have submitted and discussed your proposal on{' '}
+          <ExternalLink
+            href="https://gov.rootstockcollective.xyz/"
+            className="text-v3-primary"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Span>Discourse</Span>
+          </ExternalLink>
+          .
+        </Paragraph>
+        <Paragraph className="text-bg-0">
+          Builders who skip these steps often receive overwhelmingly negative votes.
+        </Paragraph>
+      </div>
+    </>
+  )
+}
