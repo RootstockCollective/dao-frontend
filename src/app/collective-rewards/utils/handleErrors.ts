11750c21222b59a0f05f3f5fc108c3d088f97dac
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2025-08-13 09:52:53 -0300

    DAO-1438: Replace Alert context with showToast for notifications (#1316)
    * refactor: replace Alert context with showToast for notifications
    
    * fully remove alert provider

--- src/app/collective-rewards/utils/handleErrors.ts
+++ src/app/collective-rewards/utils/handleErrors.ts
@@ -1,18 +1,16 @@
 import { useEffect } from 'react'
-import { useAlertContext } from '@/app/providers'
+import { showToast } from '@/shared/notification'
 
 type ErrorHandler = (params: { error?: Error | null; title: string; content?: string }) => void
 export const useHandleErrors: ErrorHandler = ({ error, title, content }) => {
-  const { setMessage } = useAlertContext()
-
   useEffect(() => {
     if (error) {
-      setMessage({
+      showToast({
         severity: 'error',
         title,
         content: content ?? error.message,
       })
       console.error(`🐛 ${title}:`, error)
     }
-  }, [error, title, content, setMessage])
+  }, [error, title, content])
 }
