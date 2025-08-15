b81abcf35ed2e0129898de278a217cdc4d461136
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2025-08-15 12:33:17 -0300

    add locators

--- src/components/TokenAmountDisplay/TokenAmountDisplay.tsx
+++ src/components/TokenAmountDisplay/TokenAmountDisplay.tsx
@@ -27,22 +27,22 @@ export const TokenAmountDisplay = ({
   return (
     <div className={cn('flex-1', isFlexEnd ? 'flex-col md:items-end' : 'mb-4 md:mb-0', className)}>
       {label && (
-        <Label variant="tag" className="text-bg-0">
+        <Label variant="tag" className="text-bg-0" data-testid={`${label}Label`}>
           {label}
         </Label>
       )}
       <div className="flex items-center gap-2 mt-2">
-        <Header variant="h1" className="font-bold">
+        <Header variant="h1" className="font-bold" data-testid={`${label}Amount`}>
           {amount}
         </Header>
         <TokenImage symbol={tokenSymbol} size={24} />
-        <Span variant="body-l" bold>
+        <Span variant="body-l" bold data-testid={`${label}Symbol`}>
           {tokenSymbol}
         </Span>
         {actions}
       </div>
       {amountInCurrency ? (
-        <Span variant="body-s" bold className="text-bg-0 mt-1">
+        <Span variant="body-s" bold className="text-bg-0 mt-1" data-testid={`${label}Currency`}>
           {amountInCurrency}
         </Span>
       ) : (
@@ -51,7 +51,7 @@ export const TokenAmountDisplay = ({
       {balance && (
         <div className="flex items-center gap-2 mt-4">
           <TokenImage symbol={tokenSymbol} size={12} />
-          <Paragraph variant="body-s" className="text-bg-0">
+          <Paragraph variant="body-s" className="text-bg-0" data-testid={`${label}Balance`}>
             {tokenSymbol} Balance: {balance}
           </Paragraph>
         </div>
