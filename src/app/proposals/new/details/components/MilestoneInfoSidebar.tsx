13670c626bdb78805058f3602fb26f2d776cc2d4
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2025-08-20 12:33:59 -0300

    Add milestone info text

--- src/app/proposals/new/details/components/MilestoneInfoSidebar.tsx
+++ src/app/proposals/new/details/components/MilestoneInfoSidebar.tsx
@@ -12,8 +12,9 @@ export const MilestoneInfoSidebar = () => {
       <InfoIcon className="w-5 h-5 text-bg-0" />
       <div className="flex flex-col gap-2">
         <Paragraph className="text-bg-0">
-          Explain what a milestone is... lorem ipsum dolor sit amet consectetur adipiscing elit nam id purus
-          luctus, mollis leo at, ullamcor per risus donec ac tellus tellus ut faucibus a metus id dictum.
+          Incremental value unlocks based on Milestone completion paid in $RIF, $rBTC, $USDRIF. After
+          discussion with the community off chain and assessment from Delegates you should set your proposed
+          Milestone here…
         </Paragraph>
 
         <Paragraph
