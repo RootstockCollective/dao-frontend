1c7ca13af7febd42b00b23ccfde40a298f3125ac
chrisarevalo11 <christian.arevalo@rootstocklabs.com>
2025-08-20 10:57:39 -0500

    refactor: value decimals typing

--- src/app/backing/components/AllocationBar/types.ts
+++ src/app/backing/components/AllocationBar/types.ts
@@ -22,7 +22,7 @@ export interface AllocationBarValueDisplay {
   showValue?: boolean
   format?: {
     percentDecimals?: number
-    valueDecimals?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20
+    valueDecimals?: IntRange<1, 20>
   }
 }
 
@@ -36,3 +36,12 @@ export interface AllocationBarProps {
   className?: string
   onChange?: (data: AllocationChangeData) => void
 }
+
+type Enumerate<
+  N extends number,
+  Acc extends number[] = []
+> = Acc['length'] extends N ? Acc[number] : Enumerate<N, [...Acc, Acc['length']]>;
+
+// Build F..T (inclusive)
+type IntRange<F extends number, T extends number> =
+  Exclude<Enumerate<T>, Enumerate<F>> | F;
