9c66b74c4fd07ec6248d6c404d017764ff921a68
Alexander Evchenko <alexivchenko9@gmail.com>
2025-08-20 13:34:45 +0400

    feat: gradient color for Category image

--- src/app/proposals/components/table-columns/CategoryColumn.tsx
+++ src/app/proposals/components/table-columns/CategoryColumn.tsx
@@ -4,21 +4,22 @@ import { ProposalCategory } from '@/shared/types'
 import { ClassNameValue } from 'tailwind-merge'
 import { cn } from '@/lib/utils'
 
-const icons = {
-  [ProposalCategory.Grants]: <GrantsIcon />,
-  [ProposalCategory.Activation]: <ToolsIcon />,
-  [ProposalCategory.Deactivation]: <ToolsIcon />,
-  [ProposalCategory.Milestone1]: <Milestone1Icon />,
-  [ProposalCategory.Milestone2]: <Milestone2Icon />,
-  [ProposalCategory.Milestone3]: <Milestone3Icon />,
-} as const satisfies Record<ProposalCategory, React.JSX.Element>
-
 interface Props {
   category: ProposalCategory
   className?: ClassNameValue
+  hasGradient?: boolean
 }
 
-export function CategoryColumn({ category, className }: Props) {
+export function CategoryColumn({ category, className, hasGradient = false }: Props) {
+  const icons = {
+    [ProposalCategory.Grants]: <GrantsIcon />,
+    [ProposalCategory.Activation]: <ToolsIcon />,
+    [ProposalCategory.Deactivation]: <ToolsIcon />,
+    [ProposalCategory.Milestone1]: <Milestone1Icon hasGradient={hasGradient} />,
+    [ProposalCategory.Milestone2]: <Milestone2Icon hasGradient={hasGradient} />,
+    [ProposalCategory.Milestone3]: <Milestone3Icon hasGradient={hasGradient} />,
+  } as const satisfies Record<ProposalCategory, React.JSX.Element>
+
   return (
     <div className={cn('w-full flex justify-center', className)}>
       <Tooltip text={category}>
