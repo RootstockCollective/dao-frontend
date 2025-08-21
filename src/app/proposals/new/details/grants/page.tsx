cc5b25877870832eb118e1db82e7c053c2789728
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2025-08-21 16:46:32 -0300

    make milestone info sidebar aligned with dropdown

--- src/app/proposals/new/details/grants/page.tsx
+++ src/app/proposals/new/details/grants/page.tsx
@@ -107,9 +107,11 @@ export default function GrantsProposalForm() {
           </div>
         </div>
       </form>
-      <div className="flex flex-col gap-10 basis-1/4">
+      <div className="flex flex-col gap-10 basis-1/4 justify-between">
         <ProposalInfoSidebar kycLink="https://gov.rootstockcollective.xyz/t/general-guidelines-for-grant-applications/94/7" />
-        <MilestoneInfoSidebar />
+        <div className="mb-40">
+          <MilestoneInfoSidebar />
+        </div>
       </div>
     </div>
   )
