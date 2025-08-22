ba7d279802296fdbfbbf0e07c24dfb292f2c60c8
DinizSa <39380608+DinizSa@users.noreply.github.com>
2025-08-22 10:01:42 +0100

    TOK-923: allow tooltip on mobile (#1364)
    * feat: allow tooltip on mobile
    
    * fix: do not use hooks conditioanlly
    
    ---------
    
    Co-authored-by: Antonio <antonio@iovlabs.org>

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,6 +1,7 @@
-import { ReactNode } from 'react'
+import { ReactNode, useEffect, useState } from 'react'
 import * as RadixTooltip from '@radix-ui/react-tooltip'
 import { cn } from '@/lib/utils'
+import { useIsDesktop } from '@/shared/hooks/useIsDesktop'
 
 export interface TooltipProps extends RadixTooltip.TooltipContentProps {
   text: ReactNode
@@ -18,15 +19,21 @@ export function Tooltip({
   delayDuration = 100,
   ...props
 }: TooltipProps) {
+  const [open, setOpen] = useState(false)
+
+  const isDesktop = useIsDesktop()
   if (disabled) {
     return children
   }
+
   return (
-    <RadixTooltip.Root delayDuration={delayDuration}>
-      <RadixTooltip.Trigger asChild>{children}</RadixTooltip.Trigger>
+    <RadixTooltip.Root delayDuration={delayDuration} open={open} onOpenChange={setOpen}>
+      <RadixTooltip.Trigger asChild>
+        <div onMouseEnter={() => setOpen(true)}>{children}</div>
+      </RadixTooltip.Trigger>
       <RadixTooltip.Portal>
         <RadixTooltip.Content
-          side={side}
+          side={isDesktop ? side : 'bottom'}
           sideOffset={sideOffset}
           className={cn(
             'rounded-sm bg-v3-text-80 text-v3-bg-accent-60 px-2 py-1 text-xs font-normal shadow-lg font-rootstock-sans',
