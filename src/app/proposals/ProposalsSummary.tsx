b3b12be518aa53b45ae5fa0bd5ce8e9c5d4e0173
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2025-09-01 22:55:17 -0300

    Voting Power section should be mobile friendly

--- src/app/proposals/ProposalsSummary.tsx
+++ src/app/proposals/ProposalsSummary.tsx
@@ -1,21 +1,10 @@
 'use client'
 import { VotingPowerCard } from '@/app/delegate/components/VotingPowerContainer/VotingPowerCard'
-import { NotConnectedContent } from '@/app/delegate/sections/VotingPowerSection/NotConnectedVotingPowerContainer'
-import { useAccount } from 'wagmi'
 
 export function ProposalsSummary({ totalProposals }: { totalProposals: string }) {
-  const { isConnected } = useAccount()
   return (
-    <>
-      <div className="p-6 bg-bg-80 my-2">
-        <div className="flex flex-col gap-[8px] sm:flex-row">
-          {isConnected ? (
-            <VotingPowerCard title="Total proposals" contentValue={totalProposals} />
-          ) : (
-            <VotingPowerCard title="Total proposals" contentValue={<NotConnectedContent />} />
-          )}
-        </div>
-      </div>
-    </>
+    <div className="p-6 bg-bg-80 my-2">
+      <VotingPowerCard title="Total proposals" contentValue={totalProposals} />
+    </div>
   )
 }
