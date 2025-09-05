22406eb9332a60e15bb0711ebee9730b7b4aa554
Antonio <antonio@iovlabs.org>
2025-09-05 15:27:58 +0200

    feat(WIP): add rewards details for mobile view

--- src/app/my-rewards/backers/components/BackerRewards.tsx
+++ src/app/my-rewards/backers/components/BackerRewards.tsx
@@ -4,16 +4,36 @@ import { FC } from 'react'
 import { Address } from 'viem'
 import { BackerRewardsMetrics } from './BackerRewardsMetrics'
 import { BackerRewardsTableContainer } from './Table'
+import { useIsDesktop } from '@/shared/hooks/useIsDesktop'
+import { Span } from '@/components/Typography'
+
+const RewardDetailsItem = () => <div className="flex flex-col align-start gap-4 px-0 py-5">Builder A</div>
+
+const MobileRewardsDetails = () => {
+  return (
+    <div className="block md:hidden">
+      <Span variant="h4">REWARDS DETAILS</Span>
+      <div className="flex flex-col align-start">
+        <RewardDetailsItem />
+        <RewardDetailsItem />
+      </div>
+    </div>
+  )
+}
 
 export const BackerRewards: FC<{ backer: Address }> = ({ backer }) => {
+  const isDesktop = useIsDesktop()
   return (
     <div className="flex flex-col w-full gap-10">
       <BackerRewardsContextProvider backer={backer} tokens={TOKENS}>
         <BackerRewardsMetrics backer={backer} />
         {/* FIXME: Temporary hiding of the table on mobile */}
-        <div className="hidden md:block">
-          <BackerRewardsTableContainer />
-        </div>
+        {isDesktop && (
+          <div className="hidden md:block">
+            <BackerRewardsTableContainer />
+          </div>
+        )}
+        {!isDesktop && <MobileRewardsDetails />}
       </BackerRewardsContextProvider>
     </div>
   )
