b6ef2f623a3fd7024e9c931367b3ed6547a4c5d5
chrisarevalo11 <christian.arevalo@rootstocklabs.com>
2025-09-10 16:26:57 -0500

    style: added responsive on banner

--- src/app/builders/components/BecomeBuilderBanner/BecomeBuilderBanner.tsx
+++ src/app/builders/components/BecomeBuilderBanner/BecomeBuilderBanner.tsx
@@ -9,9 +9,9 @@ const ExpandedContent = () => {
   const router = useRouter()
 
   return (
-    <div className="flex flex-col md:flex-row gap-0 items-start h-full">
+    <div className="flex flex-col lg:flex-row gap-0 items-start h-full">
       {/* Left: Image */}
-      <div className="relative overflow-hidden h-full min-h-[304px] basis-1/4 md:basis-1/4 flex-shrink-0 w-full transition-all duration-300 ease-in-out">
+      <div className="relative overflow-hidden hidden lg:flex h-full min-h-[340px] basis-1/4 md:basis-1/4 flex-shrink-0 w-full transition-all duration-300 ease-in-out">
         <Image
           src="/images/become-a-builder-banner-big.png"
           alt="Become a Builder Banner"
@@ -21,23 +21,23 @@ const ExpandedContent = () => {
         />
       </div>
       {/* Center: Main Content */}
-      <div className="flex flex-col text-base justify-center gap-4 basis-1/2 md:basis-1/2 mt-16">
+      <div className="flex flex-col text-base justify-center gap-4 basis-1/2 md:basis-1/2 md:mt-10">
         <div>
           <BaseTypography variant="h1" className="text-v3-text-0">
-            BECOME A COLLECTIVE BUILDER.
+            BECOME A <br className="md:hidden" /> COLLECTIVE BUILDER.
           </BaseTypography>
           <br />
           <BaseTypography variant="h1" className="text-v3-bg-accent-20 mt-2">
-            SECURE FUNDING. EARN
-            <br />
-            CONTINUOUSLY.
+            SECURE FUNDING.
+            <br className="md:hidden" /> EARN
+            <br className="md:hidden" /> CONTINUOUSLY.
           </BaseTypography>
         </div>
         <Paragraph className="text-v3-text-0">
           Join a growing network of innovators building the future of decentralised infrastructure. Get
           grants, earn rewards, and grow with the Collective.
         </Paragraph>
-        <div className="flex gap-4 mt-2">
+        <div className="gap-4 mt-2 lg:flex hidden">
           <Button variant="primary" onClick={() => router.push('/proposals/new?type=Builder')}>
             Join Builder Rewards
           </Button>
@@ -51,7 +51,7 @@ const ExpandedContent = () => {
         </div>
       </div>
       {/* Right: Why Become a Builder */}
-      <div className="flex flex-col justify-center bg-transparent basis-1/4 md:basis-1/4 w-full md:pl-8 mt-16">
+      <div className="flex flex-col justify-center bg-transparent basis-1/4 md:basis-1/2 lg:basis-1/4 w-full lg:pl-8 mt-8 llg:mt-10">
         <BaseTypography variant="h3" className="text-v3-text-0 mb-4">
           WHY BECOME A BUILDER?
         </BaseTypography>
@@ -67,28 +67,55 @@ const ExpandedContent = () => {
           </li>
         </ul>
       </div>
+
+      <div className="flex gap-4 mt-6 lg:hidden w-full *:flex-1">
+        <Button variant="primary" onClick={() => router.push('/proposals/new?type=Builder')} className="py-3">
+          Join Rewards
+        </Button>
+        <Button
+          variant="secondary-outline"
+          onClick={() => router.push('/proposals/new?type=Grants')}
+          textClassName="text-bg-100"
+        >
+          Apply for a Grant
+        </Button>
+      </div>
     </div>
   )
 }
 
-const CollapsedContent = () => (
-  <div className="flex items-center basis-3/4 justify-start">
-    <div className="relative overflow-hidden h-full basis-1/4 md:basis-1/4 flex-shrink-0 w-full">
-      <div className="relative w-full h-[56px]">
-        <Image
-          src="/images/become-a-builder-banner-small.png"
-          alt="Become a Builder Banner"
-          fill
-          className="object-fill"
-          priority
-        />
+const CollapsedContent = () => {
+  const router = useRouter()
+
+  return (
+    <div className="flex flex-col text-base justify-center gap-4 basis-1/2 md:basis-1/2">
+      <div>
+        <BaseTypography variant="h1" className="text-v3-text-0">
+          BECOME A <br className="md:hidden" /> COLLECTIVE BUILDER.
+        </BaseTypography>
+        <br />
+        <BaseTypography variant="h1" className="text-v3-bg-accent-20 mt-2">
+          SECURE FUNDING.
+          <br className="md:hidden" /> EARN
+          <br />
+          CONTINUOUSLY.
+        </BaseTypography>
+      </div>
+      <div className="flex gap-4 mt-2">
+        <Button variant="primary" onClick={() => router.push('/proposals/new?type=Builder')} className="py-3">
+          Join Rewards
+        </Button>
+        <Button
+          variant="secondary-outline"
+          onClick={() => router.push('/proposals/new?type=Grants')}
+          textClassName="text-bg-100 whitespace-nowrap"
+        >
+          Apply for a Grant
+        </Button>
       </div>
     </div>
-    <Paragraph className="text-v3-text-100 ml-1">
-      Learn how you can become a Collective Builder and how to apply for a Grant
-    </Paragraph>
-  </div>
-)
+  )
+}
 
 const BecomeBuilderBanner = () => {
   return (
@@ -100,8 +127,8 @@ const BecomeBuilderBanner = () => {
         chevronColor: 'text-v3-bg-accent-100',
       }}
       collapsedState={{
-        backgroundColor: 'bg-v3-bg-accent-80',
-        chevronColor: 'text-v3-text-100',
+        backgroundColor: 'bg-v3-text-80',
+        chevronColor: 'text-v3-bg-accent-100',
       }}
       defaultOpen={true}
     />
