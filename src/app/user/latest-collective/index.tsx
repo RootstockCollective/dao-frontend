c0931fe5a4afe9dc4d6119743d5882ba5639361e
Francisco Tobar <francisco.tobar@iovlabs.org>
2025-09-15 11:59:34 -0600

    refactor: pr comments

--- src/app/user/latest-collective/index.tsx
+++ src/app/user/latest-collective/index.tsx
@@ -4,7 +4,6 @@ import { Proposal } from '@/app/proposals/shared/types'
 import { Header } from '@/components/Typography'
 import { useRouter } from 'next/navigation'
 import { LatestActiveProposalCard, LatestProposalCard } from './components'
-import { ActionsContainer } from '@/components/containers/ActionsContainer'
 import { SpotlightBuildersGrid } from '@/app/shared/components/SpotlightBuildersGrid'
 import { useBuilderContext } from '@/app/collective-rewards/user/context/BuilderContext'
 
@@ -31,36 +30,40 @@ export const LatestCollectiveSection = ({
   }
 
   return (
-    <div className="bg-bg-80 p-6 mt-2">
-      <Header variant="h3">THE LATEST IN THE COLLECTIVE</Header>
-      <Header variant="h4" className="mt-10">
-        PROPOSALS
-      </Header>
-      {activeProposal ? (
-        <LatestActiveProposalCard
-          className="mt-4"
-          proposal={activeProposal}
-          /**TODO: when votingPower returned as BigInt consume as is */
-          votingPower={votingPowerRaw ?? BigInt(0)}
-          buttonAction={buttonAction}
-        />
-      ) : (
-        <div className="flex gap-2 mt-4">
-          {latestProposals.map((p, index) => (
-            <LatestProposalCard key={p.proposalId} proposal={p} data-testid={`LatestProposalCard-${index}`} />
-          ))}
+    <div className="bg-bg-80 mt-2">
+      <div className="p-6">
+        <Header variant="h3">THE LATEST IN THE COLLECTIVE</Header>
+        <Header variant="h4" className="mt-10">
+          PROPOSALS
+        </Header>
+        {activeProposal ? (
+          <LatestActiveProposalCard
+            className="mt-4"
+            proposal={activeProposal}
+            /**TODO: when votingPower returned as BigInt consume as is */
+            votingPower={votingPowerRaw ?? BigInt(0)}
+            buttonAction={buttonAction}
+          />
+        ) : (
+          <div className="flex gap-2 mt-4">
+            {latestProposals.map((p, index) => (
+              <LatestProposalCard
+                key={p.proposalId}
+                proposal={p}
+                data-testid={`LatestProposalCard-${index}`}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+      <div className="mt-10 pb-6">
+        <Header variant="h4" caps className="px-6">
+          Builders in the spotlight
+        </Header>
+        <div className="mt-4 md:px-6 pl-6 pr-0">
+          <SpotlightBuildersGrid builders={randomBuilders} />
         </div>
-      )}
-      <ActionsContainer
-        title={
-          <Header variant="h4" caps>
-            Builders in the spotlight
-          </Header>
-        }
-        className="mt-10 p-0 gap-4"
-      >
-        <SpotlightBuildersGrid builders={randomBuilders} />
-      </ActionsContainer>
+      </div>
     </div>
   )
 }
