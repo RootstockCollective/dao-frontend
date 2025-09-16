5c9a832bb7be289394b54540023e5cf5ca8fc7df
Alexander Ivchenko <47615361+TravellerOnTheRun@users.noreply.github.com>
2025-09-16 15:34:31 +0400

    DAO-1522 [Mobile] Proposal List Page - Edit ProposalsSummary to adapt to mobile (#1407)
    * feat: mobile and dektop ProposalsSummary with active proposals
    
    * fix: use correct type

--- src/app/proposals/ProposalsSummary.tsx
+++ src/app/proposals/ProposalsSummary.tsx
@@ -1,10 +1,51 @@
 'use client'
 import { VotingPowerCard } from '@/app/delegate/components/VotingPowerContainer/VotingPowerCard'
+import { NotConnectedContent } from '@/app/delegate/sections/VotingPowerSection/NotConnectedVotingPowerContainer'
+import { cn } from '@/lib/utils'
+import { useAccount } from 'wagmi'
+import { useIsDesktop } from '@/shared/hooks/useIsDesktop'
+import { ConnectWorkflow } from '@/shared/walletConnection/connection/ConnectWorkflow'
+import { Header } from '@/components/Typography'
 
-export function ProposalsSummary({ totalProposals }: { totalProposals: string }) {
+interface Props {
+  totalProposals: string
+  activeProposalsCount?: string
+}
+
+export function ProposalsSummary({ totalProposals, activeProposalsCount }: Props) {
+  const { isConnected } = useAccount()
+  const isDesktop = useIsDesktop()
   return (
-    <div className="p-6 bg-bg-80 my-2">
-      <VotingPowerCard title="Total proposals" contentValue={totalProposals} />
-    </div>
+    <>
+      <div className="p-6 bg-bg-80 my-2">
+        <div className="flex flex-col gap-[8px] sm:flex-row">
+          <div className={cn('flex', isDesktop ? 'gap-14' : 'gap-4')}>
+            {activeProposalsCount && (
+              <VotingPowerCard
+                title={isDesktop ? 'Proposals you can vote' : 'Open for voting'}
+                tooltipTitle={'This is the number of proposals you can vote on.'}
+                contentValue={
+                  !isConnected ? (
+                    <div className="flex">
+                      {isDesktop ? (
+                        <Header variant="h1" className="mr-8">
+                          -
+                        </Header>
+                      ) : (
+                        ''
+                      )}
+                      <ConnectWorkflow />
+                    </div>
+                  ) : (
+                    activeProposalsCount
+                  )
+                }
+              />
+            )}
+            <VotingPowerCard title="Total proposals" contentValue={totalProposals} />
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
