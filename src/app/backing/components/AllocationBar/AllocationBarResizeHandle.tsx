704778ac800266c917b174455f38a06f048f89f4
Juraj Piar <juraj@iovlabs.org>
2025-09-16 09:51:39 +0100

    chore: squash_me - remove toast

--- src/app/backing/components/AllocationBar/AllocationBarResizeHandle.tsx
+++ src/app/backing/components/AllocationBar/AllocationBarResizeHandle.tsx
@@ -1,15 +1,18 @@
 export const AllocationBarResizeHandle = ({
   onHandleMouseDown,
   dragIndex,
+  isEditable,
   index,
 }: {
   onHandleMouseDown: (idx: number) => (e: React.MouseEvent) => void
   dragIndex: number | null
+  isEditable: boolean
   index: number
 }) => {
+  const cursorClass = isEditable ? 'cursor-ew-resize' : 'cursor-not-allowed'
   return (
     <div
-      className="w-2 cursor-ew-resize h-full flex items-center justify-center z-10 ml-1 mr-1 bg-transparent absolute -right-3 top-0 bottom-0"
+      className={`w-2 h-full flex items-center justify-center z-10 ml-1 mr-1 bg-transparent absolute -right-3 top-0 bottom-0 ${cursorClass}`}
       onMouseDown={onHandleMouseDown(index)}
     >
       <div
