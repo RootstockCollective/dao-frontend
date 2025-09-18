b569772ada02ec08f5d5e44a6b777303e38ddef4
Francisco Tobar <francisco.tobar@iovlabs.org>
2025-09-18 06:26:57 -0600

    fix: typings

--- src/app/shared/components/SpotlightBuildersGrid/SpotlightBuildersGrid.tsx
+++ src/app/shared/components/SpotlightBuildersGrid/SpotlightBuildersGrid.tsx
@@ -1,16 +1,11 @@
 import { BackMoreBuildersCard, BuilderCardControl, BuilderCardControlProps } from '../BuilderCard'
 
 interface SpotlightBuildersGridProps {
-  builderCardControls: BuilderCardControlProps[]
-  isInteractive?: boolean
+  builders: BuilderCardControlProps[]
   showBackMoreBuildersCard?: boolean
 }
 
-export const SpotlightBuildersGrid = ({
-  builderCardControls,
-  isInteractive = false,
-  showBackMoreBuildersCard,
-}: SpotlightBuildersGridProps) => (
+export const SpotlightBuildersGrid = ({ builders, showBackMoreBuildersCard }: SpotlightBuildersGridProps) => (
   <div
     className="grid grid-cols-4 gap-2 w-full items-stretch 
       max-sm:flex max-sm:overflow-x-auto max-sm:gap-4 
@@ -18,14 +13,9 @@ export const SpotlightBuildersGrid = ({
       max-sm:snap-x max-sm:snap-proximity
       scrollbar-none px-4"
   >
-    {builderCardControls.map(({ builder, showAnimation }, index) => (
+    {builders.map(({ builder, ...props }, index) => (
       <div key={builder.address} className="max-sm:flex-shrink-0 max-sm:snap-center max-sm:w-64 flex w-full">
-        <BuilderCardControl
-          builder={builder}
-          isInteractive={isInteractive}
-          index={index}
-          showAnimation={showAnimation}
-        />
+        <BuilderCardControl builder={builder} index={index} {...props} />
       </div>
     ))}
     {showBackMoreBuildersCard && (
