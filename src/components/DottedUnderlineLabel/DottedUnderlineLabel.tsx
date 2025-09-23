f271da8da81c9b4135a22d9e0bab74206a34c712
DinizSa <diniz.araujosa@rootstocklabs.com>
2025-09-23 16:03:42 +0100

    style: circle underline dots

--- src/components/DottedUnderlineLabel/DottedUnderlineLabel.tsx
+++ src/components/DottedUnderlineLabel/DottedUnderlineLabel.tsx
@@ -4,7 +4,16 @@ import { FC } from 'react'
 
 export const DottedUnderlineLabel: FC<CommonComponentProps> = ({ className = '', children }) => {
   return (
-    <span className={cn('underline decoration-dotted decoration-[0.2rem] underline-offset-4', className)}>
+    <span
+      className={cn('relative inline-block', className)}
+      style={{
+        backgroundImage: 'radial-gradient(circle, currentColor 1px, transparent 1px)',
+        backgroundSize: '6px 6px',
+        backgroundPosition: '0 calc(100% - 2px)',
+        backgroundRepeat: 'repeat-x',
+        paddingBottom: '4px',
+      }}
+    >
       {children}
     </span>
   )
