3a177f2ace896d36a6dfb1518ab04fc7d0b9d41a
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2025-09-30 00:26:02 -0300

    Fix no title on all mobile pages

--- src/components/MainContainer/ContainerMobile.tsx
+++ src/components/MainContainer/ContainerMobile.tsx
@@ -7,6 +7,7 @@ import { FooterMobile } from './footers/FooterMobile'
 import { SidebarMobile } from './sidebars/SidebarMobile'
 import { useLayoutContext } from './LayoutProvider'
 import { BottomDrawer } from '@/components/MainContainer/drawers/BottomDrawer'
+import { TopPageHeader } from '@/shared/walletConnection/components/topPageHeader/TopPageHeader'
 
 export default function ContainerMobile({ className, children, ...props }: HTMLAttributes<HTMLDivElement>) {
   const { isSidebarOpen } = useLayoutContext()
@@ -21,7 +22,10 @@ export default function ContainerMobile({ className, children, ...props }: HTMLA
       <HeaderMobile />
       <div className="relative p-4 grow flex flex-col">
         <SidebarMobile />
-        <main className="grow mb-8 md:mb-25">{children}</main>
+        <main className="grow mb-8 md:mb-25">
+          <TopPageHeader />
+          {children}
+        </main>
         <BottomDrawer />
         <FooterMobile />
       </div>
