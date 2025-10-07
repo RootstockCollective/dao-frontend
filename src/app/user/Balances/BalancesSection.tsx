3417e8f859972a93efc1a8779f5f4a96f5a4eb0e
Alexander Evchenko <alexivchenko9@gmail.com>
2025-10-07 10:56:56 +0400

    fix: 1st token alignment on desktop

--- src/app/user/Balances/BalancesSection.tsx
+++ src/app/user/Balances/BalancesSection.tsx
@@ -35,7 +35,7 @@ export const BalancesSection = () => {
   return (
     <>
       <div className="flex flex-row justify-between mb-6 flex-wrap">
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 md:mt-6">
           <BalanceInfoForUser symbol={RIF} className="min-w-[268px]" />
           <StakeButton onClick={stakeModal.openModal} />
         </div>
