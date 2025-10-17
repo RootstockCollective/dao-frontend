192f09c51c50b059c8a2fb3b528aa9fca03041bf
chrisarevalo11 <christian.arevalo@rootstocklabs.com>
2025-10-17 16:10:30 -0500

    chore: title case to bullets

--- src/app/communities/components/HeroComponentCommunities.tsx
+++ src/app/communities/components/HeroComponentCommunities.tsx
@@ -20,10 +20,10 @@ export const HeroCommunitiesComponent = ({
     subtitle={subtitle}
     items={
       items ?? [
-        'collective Badges are dynamic NFTs that represent your role and impact within the DAO.',
-        'whether you’re a Builder, Backer, or Community Contributor, your badge shows that you belong.',
-        'be part of something bigger, helping shape the future of Bitcoin.',
-        'these aren’t just collectibles. They are your passport to participation.',
+        'Sollective Badges are dynamic NFTs that represent your role and impact within the DAO.',
+        'Whether you’re a Builder, Backer, or Community Contributor, your badge shows that you belong.',
+        'Be part of something bigger, helping shape the future of Bitcoin.',
+        'These aren’t just collectibles. They are your passport to participation.',
       ]
     }
     button={shouldShowLearnMore && <CommunityItemButtonHandler color="black" data-testid="LearnMoreButton" />}
