d1ac4f425fc4f1dbd3c842117e625c47b5847569
Alexander Ivchenko <47615361+TravellerOnTheRun@users.noreply.github.com>
2025-10-23 19:36:49 +0400

    DAO-1554 [DAO-FE] Header should dissappear when scrolling, and if user scrolls up, it should appear (#1532)
    * feat: HeaderMobile sticky
    
    * refactor: make smooth exit of HeaderMobile

--- src/components/MainContainer/headers/HeaderMobile.tsx
+++ src/components/MainContainer/headers/HeaderMobile.tsx
@@ -1,5 +1,4 @@
 import { HTMLAttributes } from 'react'
-import Link from 'next/link'
 import { RootstockLogoIcon } from '@/components/Icons'
 // import { StarIcon, SunIcon } from '../icons'
 import { useLayoutContext } from '../LayoutProvider'
@@ -7,11 +6,27 @@ import { Hamburger } from '@/components/Hamburger'
 import { Tooltip } from '@/components/Tooltip'
 import { cn } from '@/lib/utils'
 import { UserConnectionManager } from '@/shared/walletConnection'
+import { useStickyHeader } from '@/shared/hooks'
 
 export function HeaderMobile({ className, ...props }: HTMLAttributes<HTMLDivElement>) {
-  const { isSidebarOpen, toggleSidebar, closeSidebar } = useLayoutContext()
+  const { isSidebarOpen, toggleSidebar } = useLayoutContext()
+  const { headerRef, isVisible } = useStickyHeader({
+    mode: 'direction-based',
+    style: {
+      backgroundColor: 'var(--l-black)',
+    },
+  })
+
   return (
-    <header {...props} className={cn('px-4 min-h-21 flex items-center', className)}>
+    <header
+      ref={headerRef}
+      {...props}
+      className={cn(
+        'px-4 min-h-21 flex items-center transition-transform duration-300 ease-in-out',
+        isVisible ? 'translate-y-0' : '-translate-y-full',
+        className,
+      )}
+    >
       <div className="flex-1/3">
         <Tooltip text={isSidebarOpen ? 'Close menu' : 'Open menu'}>
           <Hamburger isOpen={isSidebarOpen} onClick={toggleSidebar} />
