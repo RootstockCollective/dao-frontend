9327cbbed5602ebd82009713fcc49699efd1e218
Francisco Tobar <francisco.tobar@iovlabs.org>
2025-10-24 08:49:53 -0600

    feat(MetricBar): empty state

--- src/app/components/Metric/MetricBarSegment.tsx
+++ src/app/components/Metric/MetricBarSegment.tsx
@@ -3,9 +3,13 @@ import { cn } from '@/lib/utils'
 import { ReactElement } from 'react'
 import { MetricToken, TokenSymbol } from './types'
 
-export type MetricBarSegmentProps = BarSegmentProps & Pick<MetricToken, 'symbol'>
+export type MetricBarSegmentProps = BarSegmentProps &
+  Pick<MetricToken, 'symbol'> & {
+    fade?: boolean
+    gray?: boolean
+  }
 
-const RIF_CLASSES = 'bg-v3-rif-blue border-v3-rif-blue '
+const RIF_CLASSES = 'bg-v3-rif-blue border-v3-rif-blue'
 const USDRIF_CLASSES = RIF_CLASSES
 const RBTC_CLASSES = 'bg-v3-primary border-v3-primary'
 
@@ -16,6 +20,22 @@ const TOKEN_CLASSES: Record<TokenSymbol, string> = {
   tRBTC: RBTC_CLASSES,
 }
 
-export const MetricBarSegment = ({ className, symbol, ...barProps }: MetricBarSegmentProps): ReactElement => {
-  return <BarSegment className={cn(TOKEN_CLASSES[symbol], className)} {...barProps} />
+export const MetricBarSegment = ({
+  className,
+  symbol,
+  fade,
+  gray,
+  ...barProps
+}: MetricBarSegmentProps): ReactElement => {
+  return (
+    <BarSegment
+      className={cn(
+        TOKEN_CLASSES[symbol],
+        gray && 'bg-v3-bg-accent-20 border-v3-bg-accent-20',
+        fade && 'opacity-40 transition-opacity duration-200',
+        className,
+      )}
+      {...barProps}
+    />
+  )
 }
