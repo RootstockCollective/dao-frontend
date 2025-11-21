43be896e41f99048d13edf55841ced91b1c92edc
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2025-11-21 11:41:37 -0300

    Remove ProposalsFilterSideBar story and update FilterSideBar exports

--- src/components/FilterSideBar/index.ts
+++ src/components/FilterSideBar/index.ts
@@ -1,3 +1,3 @@
-export { FilterSideBar } from './FilterSideBar'
-export { FilterSection } from './FilterSection'
-export type { FilterGroup, FilterOption, ActiveFilter } from './types'
+export * from './FilterSideBar'
+export * from './FilterSection'
+export type * from './types'
