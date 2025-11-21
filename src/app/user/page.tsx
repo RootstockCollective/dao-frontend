adbb9db001225723970d8d9148e6817304e23ca9
Alexander Evchenko <alexivchenko9@gmail.com>
2025-11-21 10:28:15 +0400

    fix: DecorativePixels not showing

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -18,7 +18,7 @@ export default function User() {
   const { activeProposals, data: proposals } = useGetProposalsWithGraph()
 
   return (
-    <div className="flex flex-col gap-2 overflow-x-hidden">
+    <div className="flex flex-col gap-2">
       {searchParams.get('action') !== 'stake' && <IntroModal />}
 
       {isConnected ? (
