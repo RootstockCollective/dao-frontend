c93c94e2ceecbcaaa7ce672baf5ec5165db04827
DinizSa <diniz.araujosa@rootstocklabs.com>
2025-12-05 16:26:20 +0000

    feat: add synthetic yield tooltip

--- src/app/vault/components/VaultMetrics.tsx
+++ src/app/vault/components/VaultMetrics.tsx
@@ -2,13 +2,16 @@
 
 import { BalanceInfo } from '@/components/BalanceInfo'
 import { useVaultBalance } from '../hooks/useVaultBalance'
+import { useSubsidyPool } from '../hooks/useSubsidyPool'
 import { formatSymbol, formatApy } from '@/app/shared/formatter'
+import { Paragraph, Span } from '@/components/Typography'
 
 /**
  * Component displaying vault metrics including balance, lending parameters, and price per share
  */
 export const VaultMetrics = () => {
   const { totalAssets, estimatedApy, pricePerShare, isLoading } = useVaultBalance()
+  const { syntheticYield, isLoading: isLoadingSynthetic } = useSubsidyPool()
 
   return (
     <div className="flex flex-col gap-6 w-full">
@@ -24,6 +27,14 @@ export const VaultMetrics = () => {
           title="APY"
           amount={isLoading ? '...' : formatApy(estimatedApy)}
           symbol="%"
+          tooltipContent={
+            <div className="flex flex-col gap-2 text-wrap max-w-[35rem] text-xs p-2">
+              <Paragraph>
+                In addition to the strategies APYs listed below, there is an additional synthetic yield
+                {!isLoadingSynthetic && <Span> of {formatApy(syntheticYield)}</Span>}
+              </Paragraph>
+            </div>
+          }
         />
         <BalanceInfo
           className="max-w-[214px] min-w-[180px]"
