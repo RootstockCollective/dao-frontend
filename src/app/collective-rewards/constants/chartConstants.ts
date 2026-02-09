af2eda57243601f6c23a9c257319d359b873ad67
chris <christian.arevalo@rootstocklabs.com>
2026-02-08 19:12:30 -0500

    TOK-1089: show last 8 cycles instead of 10 (mobile) on the collective rewards chart (#1695)
    * chore: show 8 cycles instead of 10 on mobile
    
    * fix: mobile chart size

--- src/app/collective-rewards/constants/chartConstants.ts
+++ src/app/collective-rewards/constants/chartConstants.ts
@@ -1,5 +1,7 @@
 export const ONE_DAY_IN_SECONDS = 24 * 60 * 60
 export const ONE_DAY_IN_MS = 1000 * 60 * 60 * 24
+
+export const FOUR_MONTHS_IN_MS = 4 * 30 * 24 * 60 * 60 * 1000
 export const FIVE_MONTHS_IN_MS = 5 * 30 * 24 * 60 * 60 * 1000
 
 export const CHART_BUFFER_PERCENTAGE = 1.1
