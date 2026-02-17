93e4babb9306eb828b267fceb47819e94ccda39d
Francis Rodriguez <francisngg@gmail.com>
2026-02-17 01:05:55 -0400

    DAO-1922 Migrate BoosterContext and boost.utils to native fetch

--- src/app/providers/NFT/boost.utils.ts
+++ src/app/providers/NFT/boost.utils.ts
@@ -1,14 +1,18 @@
 import { NFT_BOOSTER_DATA_URL } from '@/lib/constants'
-import { axiosInstance } from './BoosterContext'
 
 export const fetchLatestFile = async (noCacheTime: number) => {
-  const { data } = await axiosInstance.get(`${NFT_BOOSTER_DATA_URL}/latest?nocache=${noCacheTime}`)
-
-  return data
+  const response = await fetch(`${NFT_BOOSTER_DATA_URL}/latest?nocache=${noCacheTime}`)
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`)
+  }
+  return response.text()
 }
 
 export const fetchBoostData = async (latestFile: string | undefined, noCacheTime: number) => {
-  const { data } = await axiosInstance.get(`${NFT_BOOSTER_DATA_URL}/${latestFile}?nocache=${noCacheTime}`)
-
+  const response = await fetch(`${NFT_BOOSTER_DATA_URL}/${latestFile}?nocache=${noCacheTime}`)
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`)
+  }
+  const data = await response.json()
   return { ...data, nftContractAddress: data.nftContractAddress.toLowerCase() }
 }
