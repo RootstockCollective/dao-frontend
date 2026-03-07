463fb8d0c62a92928fef3bfbb4cb1f42136db784
Rodrigo Santana Gonçalves <rodrigo.santana@iovlabs.org>
2026-03-07 20:25:21 -0300

    Overview dashboard - Row 2

--- src/app/fund-manager/components/RbtcVaultMetricsSection.tsx
+++ src/app/fund-manager/components/RbtcVaultMetricsSection.tsx
@@ -2,12 +2,24 @@
 
 import { LoadingSpinner } from '@/components/LoadingSpinner'
 import { Paragraph } from '@/components/Typography'
+import { RBTC } from '@/lib/constants'
 
+import { useRbtcAsyncVaultMetrics } from '../hooks/useRbtcAsyncVaultMetrics'
+import { RbtcVaultMetricCard } from './RbtcVaultMetricCard'
 import { RbtcVaultMetricsRow } from './RbtcVaultMetricsRow'
 
 export const RbtcVaultMetricsSection = () => {
-  const isLoading = false
-  const error = null
+  const {
+    currentNav,
+    deployedCapital,
+    unallocatedCapital,
+    manualBuffer,
+    isLoading: isLoadingAsyncVaultMetrics,
+    error: errorAsyncVaultMetrics,
+  } = useRbtcAsyncVaultMetrics()
+
+  const isLoading = isLoadingAsyncVaultMetrics
+  const error = errorAsyncVaultMetrics
 
   if (isLoading) {
     return (
@@ -36,7 +48,39 @@ export const RbtcVaultMetricsSection = () => {
 
       {/* Row 2 */}
       <RbtcVaultMetricsRow>
-        <></>
+        <RbtcVaultMetricCard
+          title="Current NAV"
+          tooltipContent=""
+          amount={currentNav.amount}
+          tokenSymbol={RBTC}
+          fiatAmount={currentNav.fiatAmount}
+          buttonLabel="Update NAV"
+          buttonVariant="primary"
+        />
+        <RbtcVaultMetricCard
+          title="Deployed Capital"
+          tooltipContent=""
+          amount={deployedCapital.amount}
+          tokenSymbol={RBTC}
+          fiatAmount={deployedCapital.fiatAmount}
+          buttonLabel="Deposit to Vault"
+        />
+        <RbtcVaultMetricCard
+          title="Unallocated Capital"
+          tooltipContent=""
+          amount={unallocatedCapital.amount}
+          tokenSymbol={RBTC}
+          fiatAmount={unallocatedCapital.fiatAmount}
+          buttonLabel="Transfer to Manager Wallet"
+        />
+        <RbtcVaultMetricCard
+          title="Manual Buffer"
+          tooltipContent=""
+          amount={manualBuffer.amount}
+          tokenSymbol={RBTC}
+          fiatAmount={manualBuffer.fiatAmount}
+          buttonLabel="Top Up Buffer"
+        />
       </RbtcVaultMetricsRow>
 
       {/* Row 3 */}
